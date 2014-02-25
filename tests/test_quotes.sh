b4ceac0cfd0eef91450e7d9feb9e1a2cd3c392fa
Vitaly Lipatov <lav@etersoft.ru>
2014-02-25 14:31:05 +0400

    remove quotes from commands

---
+++ tests/test_quotes.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SUDO=
+#sudofunc
+
+sudofunc()
+{
+    echo "arg1: $1"
+    echo "arg2: $2"
+    echo "arg3: $3"
+    echo "arg4: $4"
+}
+
+# fake
+showcmd()
+{
+	echo "$@"
+}
+
+# Print command line and run command line with SUDO
+sudocmd()
+{
+	showcmd "$SUDO $@"
+#FIXME
+	$SUDO $@
+}
+
+
+sudocmd "ls -l" "-a -a"
