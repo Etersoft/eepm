fb90eba7bf7cc9a53dbcc54aaa57236e2802f488
Vitaly Lipatov <lav@etersoft.ru>
2017-06-08 13:47:38 +0300

    make shellcheck more happy

--- check_code.sh
+++ check_code.sh
@@ -3,5 +3,15 @@
 # http://mywiki.wooledge.org/Bashism
 # https://wiki.ubuntu.com/DashAsBinSh
 
+EXCL=-eSC2086,SC2039,SC2034,SC2068,SC2155
+
+if [ -n "$1" ] ; then
+    shellcheck $EXCL "$1"
+    exit
+fi
+
 checkbashisms -f bin/*
 checkbashisms -f Makefile
+
+shellcheck $EXCL \
+	bin/epm bin/distr_info bin/epm-* bin/serv-* bin/tools_*
