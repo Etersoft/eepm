303842a5d61d39e2261695ae06383a5c9c422bde
Vitaly Lipatov <lav@etersoft.ru>
2017-07-22 13:53:08 +0300

    check_code.sh: skip global vars

--- check_code.sh
+++ check_code.sh
@@ -5,6 +5,11 @@
 
 EXCL=-eSC2086,SC2039,SC2034,SC2068,SC2155
 
+# TODO:
+# SC2154: pkg_filenames is referenced but not assigned.
+# SC2002: Useless cat.
+EXCL="$EXCL,SC2154,SC2002"
+
 if [ -n "$1" ] ; then
     shellcheck $EXCL "$1"
     exit
