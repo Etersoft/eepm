09fe35b2eca89a7e6fe788833a402511e6119e96
Vitaly Lipatov <lav@etersoft.ru>
2018-10-06 21:37:48 +0300

    add yandex-browser-beta.sh

---
+++ repack.d/yandex-browser-beta.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+[ -x $BUILDROOT/usr/bin/yandex-browser ] || ln -sv yandex-browser-beta $BUILDROOT/usr/bin/yandex-browser
