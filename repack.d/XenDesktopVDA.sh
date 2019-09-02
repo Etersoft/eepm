700440a5fa380a90b009d0eba8a4ad9df195c296
Vitaly Lipatov <lav@etersoft.ru>
2019-09-02 10:52:57 +0300

    add repack.d/XenDesktopVDA.sh (ALT bug 37167)

---
+++ repack.d/XenDesktopVDA.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+if [ "$(distr_info -a)" = "x86_64" ] ; then
+    # 32 bit
+    rm -rfv $BUILDROOT/opt/Citrix/VDA/lib32
+    subst "s|.*/libctxXrandrhook.so.||" $SPEC
+fi
+
+#REQUIRES=""
+subst "s|^\(Name: .*\)$|%filter_from_requires /AuthManagerDaemon/d\n\1|g" $SPEC
+
