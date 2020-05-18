826faae10873710fdba64127ab181ca6a553e9fa
Vitaly Lipatov <lav@etersoft.ru>
2020-05-18 22:36:43 +0300

    add zoom repack rule

---
+++ repack.d/zoom.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+
+SPEC="$2"
+
+test -f /lib64/ld-linux-x86-64.so.2 && return
+
+# drop x86_64 req from 32 bit binary
+sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
+
