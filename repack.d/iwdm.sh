6a62c262eb68d220ed8287a6b137efc32b90fbe7
Vitaly Lipatov <lav@etersoft.ru>
2020-08-25 00:18:51 +0300

    add repack scripts for InfoWatch products

---
+++ repack.d/iwdm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+# Infowatch product Device
+
+# remove kernel related script
+rm -fv $BUILDROOT/opt/iw/dmagent/etc/initramfs-tools/hooks/iwdm
+subst 's|"*/opt/iw/dmagent/etc/initramfs-tools/hooks/iwdm"*||' $SPEC
