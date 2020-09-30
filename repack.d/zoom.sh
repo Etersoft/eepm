25b100e0fdae50d85c14d7c8c3ca78c03823280c
Vitaly Lipatov <lav@etersoft.ru>
2020-09-30 17:13:33 +0300

    zoom.sh: s/return/exit (ALT bug 39014)

--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -4,7 +4,7 @@ BUILDROOT="$1"
 
 SPEC="$2"
 
-test -f /lib64/ld-linux-x86-64.so.2 && return
+test -f /lib64/ld-linux-x86-64.so.2 && exit
 
 # drop x86_64 req from 32 bit binary
 sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
