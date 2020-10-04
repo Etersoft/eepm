7d375cf6ad8a475b009f42ac6210dd533773ea9e
Vitaly Lipatov <lav@etersoft.ru>
2020-10-04 15:39:59 +0300

    add prescriptions for wine

---
+++ receipts.d/i586-wine-vanilla.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+MAIN=wine-vanilla
+
+# Устанавливаем wine
+epmi lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl
+
+# Модули, могут не быть в системе
+epmi i586-glibc-nss i586-glibc-gconv-modules
+
+# Если используется AD, потребуется
+epm installed sssd-client && epmi i586-sssd-client
