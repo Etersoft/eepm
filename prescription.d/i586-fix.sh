d69cea75d65d2b6b7824e186826f3b0194324d58
Vitaly Lipatov <lav@etersoft.ru>
2020-10-06 22:50:20 +0300

    add i586-fix prescription (installed missed i586- parts for installed rpm packages)

---
+++ prescription.d/i586-fix.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ "$1" != "--run" ] && echo "Fix missed 32 bit package modules on 64 bit system" && exit
+
+[ "$(distro_info -d)" != "ALTLinux" ] && echo "Only ALTLinux is supported" && exit 1
+[ "$(distro_info -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
+
+LIST=''
+
+# copied from
+
+echo
+echo "Checking for installed modules... "
+for i in glibc-nss glibc-gconv-modules \
+         sssd-client \
+         $(epmqp --short libnss | grep "^libnss-") \
+         $(epmqp --short xorg-dri | grep "^xorg-dri-")
+do
+    epm --quiet installed $i && LIST="$LIST i586-$i"
+done
+
+echo
+echo "Installing all appropiate i586-* packages ..."
+epmi $LIST
