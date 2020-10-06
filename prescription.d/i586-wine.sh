e48bd64e6d16aa65f7fa6e4d7dbbca9239ddd3aa
Vitaly Lipatov <lav@etersoft.ru>
2020-10-06 22:55:19 +0300

    i586-wine: use i586-fix prescription

--- prescription.d/i586-wine.sh
+++ prescription.d/i586-wine.sh
@@ -5,12 +5,10 @@ MAIN=wine
 [ "$1" != "--run" ] && echo "Install 32 bit $MAIN packages on 64 bit system" && exit
 
 [ "$(distro_info -d)" != "ALTLinux" ] && echo "Only ALTLinux is supported" && exit 1
+[ "$(distro_info -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
 # Устанавливаем wine
 epmi lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl
 
-# Модули, могут не быть в системе
-epmi i586-glibc-nss i586-glibc-gconv-modules
-
-# Если используется AD, потребуется
-epm installed sssd-client && epmi i586-sssd-client
+# Доставляем пропущенные модули (подпакеты) для установленных 64-битных
+epm prescription i586-fix
