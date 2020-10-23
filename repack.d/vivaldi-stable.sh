436e66e1847d8a9e73b5c262329cc25c5a18110b
Vitaly Lipatov <lav@etersoft.ru>
2020-10-24 01:32:39 +0300

    add repack for vivaldi-stable

---
+++ repack.d/vivaldi-stable.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=vivaldi
+PRODUCTDIR=/opt/vivaldi
+subst 's|%files|%files\n/usr/share/icons/hicolor/*x*/apps/*.png|' $SPEC
+
+for i in 16 22 24 32 48 64 128 256 ; do
+    mkdir -p $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/
+    cp $BUILDROOT/$PRODUCTDIR/product_logo_${i}.png $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/$PRODUCT.png
+done
+
+rm -f $BUILDROOT/etc/cron.daily/$PRODUCT
+subst "s|.*/etc/cron.daily/$PRODUCT.*||" $SPEC
