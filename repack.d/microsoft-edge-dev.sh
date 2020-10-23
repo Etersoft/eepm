e495e451774189c29053fd10c7811c74387c0d7d
Vitaly Lipatov <lav@etersoft.ru>
2020-10-23 18:22:09 +0300

    add repack for microsoft-edge-dev

---
+++ repack.d/microsoft-edge-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=microsoft-edge-dev
+PRODUCTDIR=/opt/microsoft/msedge-dev
+subst 's|%files|%files\n/usr/share/icons/hicolor/*x*/apps/*.png|' $SPEC
+
+for i in 16 22 24 32 48 64 128 256 ; do
+    mkdir -p $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/
+    cp $BUILDROOT/$PRODUCTDIR/product_logo_${i}_dev.png $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/$PRODUCT.png
+done
+
+rm -f $BUILDROOT/etc/cron.daily/$PRODUCT
+subst "s|.*/etc/cron.daily/$PRODUCT.*||" $SPEC
+
+# unsupported format
+rm -f $BUILDROOT/usr/share/menu/microsoft-edge-dev.menu
+subst "s|.*/usr/share/menu/microsoft-edge-dev.menu.*||" $SPEC
