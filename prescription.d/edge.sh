489efac73aceac2122538591c4c598965fc66db7
Vitaly Lipatov <lav@etersoft.ru>
2020-10-23 15:47:58 +0300

    prescriptions: add edge, zoom

---
+++ prescription.d/edge.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# TODO: common place
+fatal()
+{
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+[ "$1" != "--run" ] && echo "Install Microsoft Edge browser (dev) from the official site" && exit
+
+[ "$(distro_info -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
+
+# epm uses eget to download * names
+epm install "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-dev/microsoft-edge-*_amd64.deb"
---
+++ prescription.d/zoom.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# TODO: common place
+fatal()
+{
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+[ "$1" != "--run" ] && echo "Install Zoom client from the official site" && exit
+
+arch=$(distro_info --distro-arch)
+case $arch in
+    x86_64|amd64)
+        arch=$arch ;;
+    i586|i386)
+        arch=$arch ;;
+    *)
+        fatal "Unsupported arch $arch for $(distro_info -d)"
+esac
+
+if [ "$(distro_info -d)" = "ALTLinux" ] ; then
+    epm install https://zoom.us/client/latest/zoom_$arch.rpm
+    exit
+fi
+
+# TODO: there are more complex distro dependent url
+epm install https://zoom.us/client/latest/zoom_$arch."$(distro_info -p)"
