035ba2fcfcf9077f74579714e06a879213c64756
Vitaly Lipatov <lav@etersoft.ru>
2020-10-27 02:26:55 +0300

    repack: move skype to /opt, fix reqs

--- repack.d/skypeforlinux.sh
+++ repack.d/skypeforlinux.sh
@@ -4,10 +4,24 @@
 BUILDROOT="$1"
 SPEC="$2"
 
+PRODUCT=skype
 # move binaries from /usr/share/PKGNAME to _libdir/PKGNAME
-LIBDIR=$(rpmbuild --eval %_libdir 2>/dev/null)
-mkdir -p $BUILDROOT$LIBDIR/
-mv $BUILDROOT/usr/share/skypeforlinux/ $BUILDROOT$LIBDIR/
-subst "s|/usr/share/skypeforlinux|$LIBDIR/skypeforlinux|g" $SPEC
+#LIBDIR=$(rpmbuild --eval %_libdir 2>/dev/null)
+LIBDIR=/opt
 
-subst "s|^SKYPE_PATH=.*|SKYPE_PATH=$LIBDIR/skypeforlinux/skypeforlinux|" $BUILDROOT/usr/bin/skypeforlinux
+mkdir -p $BUILDROOT$LIBDIR/
+mv $BUILDROOT/usr/share/skypeforlinux/ $BUILDROOT$LIBDIR/$PRODUCT/
+subst "s|/usr/share/skypeforlinux|$LIBDIR/$PRODUCT|g" $SPEC
+
+subst "s|^SKYPE_PATH=.*|SKYPE_PATH=$LIBDIR/$PRODUCT/skypeforlinux|" $BUILDROOT/usr/bin/skypeforlinux
+
+subst '1iAutoProv:no' $SPEC
+
+# ignore embedded libs
+subst '1i%filter_from_requires /^libGLESv2.so().*/d' $SPEC
+subst '1i%filter_from_requires /^libEGL.so().*/d' $SPEC
+subst '1i%filter_from_requires /^libffmpeg.so().*/d' $SPEC
+
+mkdir -p $BUILDROOT/usr/bin/
+ln -s /usr/bin/skypeforlinux $BUILDROOT/usr/bin/skype
+subst 's|%files|%files\n/usr/bin/skype|' $SPEC
