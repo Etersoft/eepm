bc1ead9adbe1e10beab0bbd2a0ce6f4ce7d02476
Vitaly Lipatov <lav@etersoft.ru>
2020-11-11 00:34:55 +0300

    add prescription python2-remove

---
+++ prescription.d/python2-remove.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ "$1" != "--run" ] && echo "Remove all possible python2 packages" && exit
+
+[ "$($DISTRVENDOR -d)" != "ALTLinux" ] && echo "Only ALTLinux is supported" && exit 1
+
+PACKAGES="$(epm qp python-module)"
+
+[ -n "$PACKAGES" ] || { echo "All python2 packages are already removed" ; exit 0 ; }
+
+epm remove $PACKAGES
+
+epm remove python2-base
