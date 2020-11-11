44a68ea8a02240e729e04d73c333a7ab20fba7e7
Vitaly Lipatov <lav@etersoft.ru>
2020-11-11 20:16:47 +0300

    repack: add lightworks.sh

---
+++ repack.d/lightworks.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+#PRODUCT=lightworks
+
+# lib.req: ERROR: /tmp/.private/.../usr/lib/lightworks/libirng.so: no symbol bindings
+subst '1i%add_findreq_skiplist /usr/lib/lightworks/libirng.so /usr/lib/lightworks/libsvml.so' $SPEC
+
+subst '1iAutoProv:no' $SPEC
+
+# ignore embedded libs
+for i in libc++.so.1 libc++abi.so.1 libedit.so libportaudio.so.2 libportaudiocpp.so.0 ; do
+    subst "1i%filter_from_requires /^$i()(64bit).*/d" $SPEC
+done
+
+echo "Note: Download and install rpm package from https://developer.nvidia.com/cg-toolkit-download"
