5468b3f5dd87e01fc3aab479bbe53ff7b0ec6b3f
Vitaly Lipatov <lav@etersoft.ru>
2020-11-11 21:41:33 +0300

    improve anydesk repack

--- repack.d/anydesk.sh
+++ repack.d/anydesk.sh
@@ -6,8 +6,13 @@ SPEC="$2"
 
 PRODUCT=anydesk
 
-mkdir -p $BUILDROOT/etc/systemd/system
-ln -sf /usr/share/anydesk/files/systemd/anydesk.service $BUILDROOT/etc/systemd/system/anydesk.service
+mkdir -p $BUILDROOT/etc/systemd/system/
+mv -fv $BUILDROOT/usr/share/anydesk/files/systemd/anydesk.service $BUILDROOT/etc/systemd/system/anydesk.service
 subst "s|/usr/share/anydesk/files/systemd/anydesk.service|/etc/systemd/system/anydesk.service|g" $SPEC
+
+mkdir -p $BUILDROOT/etc/rc.d/init.d/
+mv -fv $BUILDROOT/usr/share/anydesk/files/init/anydesk $BUILDROOT/etc/rc.d/init.d/anydesk
+subst "s|.*/etc/default/NetworkManager.*||" $BUILDROOT/etc/rc.d/init.d/anydesk
+subst "s|/usr/share/anydesk/files/init/anydesk|/etc/rc.d/init.d/anydesk|" $SPEC
 
 #subst '1iAutoProv:no' $SPEC
