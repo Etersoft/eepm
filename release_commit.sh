b4fe5ef17ca8d6ededcf46b9dded883a812c7a07
Vitaly Lipatov <lav@etersoft.ru>
2020-11-25 19:47:29 +0300

    improve release_commit.sh script

--- release_commit.sh
+++ release_commit.sh
@@ -7,7 +7,9 @@ load_mod spec etersoft
 SPECNAME=eepm.spec
 version="$(get_version $SPECNAME)"
 
+./pack_in_onefile.sh
+
 git add packed
 git commit packed -m "commit packed $version"
-git tag -a "$version" -e -m "$(rpmlog --last-changelog)"
+git tag -a "$version" -e -s -m "$(rpmlog --last-changelog)"
 git push pub.github $version
