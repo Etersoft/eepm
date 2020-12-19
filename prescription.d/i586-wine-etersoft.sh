d76f8f6886a6fac87375cae7e249cd99452e701d
Vitaly Lipatov <lav@etersoft.ru>
2020-12-20 01:37:14 +0300

    i586-wine: return error on error

--- prescription.d/i586-wine-etersoft.sh
+++ prescription.d/i586-wine-etersoft.sh
@@ -8,7 +8,7 @@ MAIN=wine-etersoft
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
 # Устанавливаем wine
-epmi lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl
+epmi lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl || exit
 
 # Доставляем пропущенные модули (подпакеты) для установленных 64-битных
 epm prescription i586-fix
