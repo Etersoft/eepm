bee83a6a8016a73c4e954c99e5fa96101253cc87
Vitaly Lipatov <lav@etersoft.ru>
2021-01-06 18:46:05 +0300

    repack.d/tamtam-app.sh: set SUID for chromium-sandbox

--- repack.d/tamtam-app.sh
+++ repack.d/tamtam-app.sh
@@ -13,3 +13,9 @@ mkdir -p $BUILDROOT/usr/bin/
 ln -sf $LIBDIR/$PRODUCT/tamtam $BUILDROOT/usr/bin/tamtam
 
 subst "s|%files|%files\n%_bindir/tamtam|" $SPEC
+
+
+# Set SUID for chrome-sandbox if userns_clone is not supported
+userns_path='/proc/sys/kernel/unprivileged_userns_clone'
+userns_val="$(cat $userns_path 2>/dev/null)"
+[ "$userns_val" = '1' ] || chmod 4755 $BUILDROOT/$LIBDIR/$PRODUCT/chrome-sandbox
