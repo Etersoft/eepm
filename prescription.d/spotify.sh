eee16c0004cbfc8c1317e5f6624aaf900e9b4d5b
Vitaly Lipatov <lav@etersoft.ru>
2021-01-06 16:15:41 +0300

    epm play: add spotify install

---
+++ prescription.d/spotify.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# TODO: common place
+fatal()
+{
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+PKGNAME=spotify-client
+
+if [ "$1" = "--remove" ] ; then
+    epm remove $PKGNAME
+    exit
+fi
+
+[ "$1" != "--run" ] && echo "Install Spotify client for Linux from the official site" && exit
+
+[ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
+
+# epm uses eget to download * names
+epm install "https://repository-origin.spotify.com/pool/non-free/s/spotify-client/$(epm print constructname $PKGNAME "*" amd64 deb)"
---
+++ repack.d/spotify-client.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=spotify
+LIBDIR=/opt
+
+mkdir -p $BUILDROOT$LIBDIR/
+mv $BUILDROOT/usr/share/$PRODUCT/ $BUILDROOT$LIBDIR/$PRODUCT/
+subst "s|/usr/share/$PRODUCT|$LIBDIR/$PRODUCT|g" $SPEC
+
+subst '1iAutoProv:no' $SPEC
+
+# see https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=spotify
+mkdir -p $BUILDROOT/usr/bin/
+ln -sf $LIBDIR/$PRODUCT/$PRODUCT $BUILDROOT/usr/bin/$PRODUCT
+mkdir -p $BUILDROOT/usr/share/applications/
+ln -sf $LIBDIR/$PRODUCT/$PRODUCT.desktop $BUILDROOT/usr/share/applications/$PRODUCT.desktop
+subst "s|%files|%files\n/usr/share/applications/$PRODUCT.desktop|" $SPEC
+subst "s|%files|%files\n/usr/share/icons/hicolor/*/apps/$PRODUCT.png|" $SPEC
+
+for i in 16 22 24 32 48 64 128 256 ; do
+    mkdir -p $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/
+    cp $BUILDROOT/$LIBDIR/$PRODUCT/icons/spotify-linux-$i.png $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/$PRODUCT.png
+done
+
+REQUIRES="libcurl-gnutls"
+subst "s|^\(Name: .*\)$|# Converted from original package requires\nRequires:$REQUIRES\n\1|g" $SPEC
