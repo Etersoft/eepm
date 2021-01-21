19c89a15f384a75ee0e1f68d12dff5ef9e007ee5
Vitaly Lipatov <lav@etersoft.ru>
2021-01-21 12:00:50 +0300

    prescripion.d/i586-wine*: fix removing (ALT bug 39320)

--- prescription.d/i586-wine-etersoft.sh
+++ prescription.d/i586-wine-etersoft.sh
@@ -2,13 +2,20 @@
 
 MAIN=wine-etersoft
 
-[ "$1" != "--run" ] && echo "Install 32 bit $MAIN packages on 64 bit system" && exit
-
 [ "$($DISTRVENDOR -d)" != "ALTLinux" ] && echo "Only ALTLinux is supported" && exit 1
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
+PKGNAMES="lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl"
+
+if [ "$1" = "--remove" ] ; then
+    epm remove $PKGNAMES
+    exit
+fi
+
+[ "$1" != "--run" ] && echo "Install 32 bit $MAIN packages on 64 bit system" && exit
+
 # Устанавливаем wine
-epmi lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl || exit
+epm install $PKGNAMES || exit
 
 # Доставляем пропущенные модули (подпакеты) для установленных 64-битных
 epm prescription i586-fix
