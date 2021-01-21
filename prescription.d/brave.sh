f16c3a5df150a4f8c037601495ed3ac1d5a0d3c2
Vitaly Lipatov <lav@etersoft.ru>
2021-01-21 22:59:51 +0300

    add brave-browser install: prescription.d/brave.sh

---
+++ prescription.d/brave.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+# TODO: common place
+fatal()
+{
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+PKGNAME=brave-browser-nightly
+
+if [ "$1" = "--remove" ] ; then
+    epm remove $PKGNAME
+    exit
+fi
+
+[ "$1" != "--run" ] && echo "Install Brave browser from the official site" && exit
+
+[ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
+
+arch=x86_64
+pkgtype=rpm
+
+PKG=$($EGET --list --latest https://github.com/brave/brave-browser/releases "$PKGNAME-*.$arch.$pkgtype") || fatal "Can't get package URL"
+
+epm install "$PKG"
