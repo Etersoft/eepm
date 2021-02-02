b51391ef5216bfbc04053bceabc26d839c2a1883
Vitaly Lipatov <lav@etersoft.ru>
2021-02-02 14:37:28 +0300

    add glusterfs9 install

---
+++ prescription.d/glusterfs9.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ "$1" != "--run" ] && echo "Install glusterfs9 (or upgrade from glusterfs8)" && exit
+
+[ "$($DISTRVENDOR -d)" != "ALTLinux" ] && echo "Only ALTLinux is supported" && exit 1
+
+GFSOLD=glusterfs8
+GFSNEW=glusterfs9
+
+if epmqp --quiet ${GFSOLD}- ; then
+    # Upgrade if was installed
+    epmi $(epmqp --short $GFSOLD | grep -v rdma | sed -e "s|$GFSOLD|$GFSNEW|") ${GFSOLD}- ${GFSOLD}-client- python3-module-${GFSOLD}-
+    epm installed $GFSNEW-server && serv glusterd on
+else
+    # Install all packages
+    epmi ${GFSNEW}-cli ${GFSNEW}-client ${GFSNEW}
+
+    echo "You can install also '${GFSNEW}-server' if it is needed for this host"
+
+    epme $(epmqp ${GFSOLD})
+fi
