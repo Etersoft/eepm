b7ecb96467e0d04d110768ee092ebc100f72c2b2
Vitaly Lipatov <lav@etersoft.ru>
2021-02-05 04:00:16 +0300

    repack.d/code.sh: use upstream script to run code

--- repack.d/code.sh
+++ repack.d/code.sh
@@ -12,8 +12,8 @@ subst '1iAutoProv:no' $SPEC
 subst 's|\(.*/usr/share/code/code.*\)|"/usr/bin/code"\n"/usr/bin/vscode"\n\1|' $SPEC
 
 mkdir -p $BUILDROOT/usr/bin/
-ln -rs $BUILDROOT/usr/share/code/code $BUILDROOT/usr/bin/code
-ln -rs $BUILDROOT/usr/share/code/code $BUILDROOT/usr/bin/vscode
+ln -rs $BUILDROOT/usr/share/code/bin/code $BUILDROOT/usr/bin/code
+ln -rs $BUILDROOT/usr/share/code/bin/code $BUILDROOT/usr/bin/vscode
 
 # install all requires packages before packing (the list have got with rpmreqs package | xargs echo)
 epm install --skip-installed at-spi2-atk coreutils findutils firefox gawk glib2 libalsa libatk libat-spi2-core libcairo libdbus libdrm libexpat libgbm libgdk-pixbuf libgio libgtk+3 libnspr libnss libpango libsecret libX11 libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbfile libXrandr libXrender libXScrnSaver libXtst sed
