8ad08e9e87bdd9bc4421bbbac174654e7ee8b131
Vitaly Lipatov <lav@etersoft.ru>
2021-02-18 20:41:46 +0300

    add repack.d for trueconf-server (unsupported!)

---
+++ repack.d/trueconf-server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+#  trueconf-server: Требует: libcurl.so.4(CURL_OPENSSL_4)(64bit) но пакет не может быть установлен
+#                   Требует: liblber-2.4.so.2(OPENLDAP_2.4_2)(64bit) но пакет не может быть установлен
+#                   Требует: libldap_r-2.4.so.2(OPENLDAP_2.4_2)(64bit) но пакет не может быть установлен
+
+subst '1i%filter_from_requires /^libcurl.so.4(CURL_OPENSSL_.*/d' $SPEC
+subst '1i%filter_from_requires /^liblber-2.4.so.2(OPENLDAP_.*/d' $SPEC
+subst '1i%filter_from_requires /^libldap_r-2.4.so.2(OPENLDAP_.*/d' $SPEC
+
+#REQUIRES="libcurl libldap"
+#subst "1iRequires:$REQUIRES|" $SPEC
+
+# install all requires packages before packing (the list have got with rpmreqs anydesk)
+epm install --skip-installed coreutils gawk libapr1 libaprutil1 libcares libcrypt libcrypto1.1 libcurl liblame libldap libncurses libnghttp2 libnsl1 libpcre3 libpng16 libpq5 libreadline7 libspeex libssl1.1 libtinfo libxml2 mozldap systemd-utils veyon zlib
