1febfba16eacb68261b19d13e79bf8231b28db9d
Vitaly Lipatov <lav@etersoft.ru>
2021-03-09 23:25:20 +0300

    check_reqs.sh: add --detail

--- check_reqs.sh
+++ check_reqs.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+if [ "$1" = "--detail" ] ; then
+    for i in bin/epm-* ; do
+        echo
+        echo "==== $i:"
+        /usr/lib/rpm/shell.req $i
+    done
+    exit 0
+fi
+
 /usr/lib/rpm/shell.req bin/epm-* | sort -u | tee ./check_eepm.log
 git diff ./check_eepm.log
