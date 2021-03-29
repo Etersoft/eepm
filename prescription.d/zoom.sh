44a17135629ac70a102e5d4f5055d42a76d1615b
Vitaly Lipatov <lav@etersoft.ru>
2021-03-30 00:05:37 +0300

    fix install zoom on ALTLinux i586

--- prescription.d/zoom.sh
+++ prescription.d/zoom.sh
@@ -19,15 +19,19 @@ fi
 arch=$($DISTRVENDOR --distro-arch)
 case $arch in
     x86_64|amd64)
+        version=latest
+        repack=''
         arch=$arch ;;
     i586|i386)
-        arch=$arch ;;
+        version=5.4.53391.1108
+        repack='--repack'
+        arch=i686 ;;
     *)
         fatal "Unsupported arch $arch for $($DISTRVENDOR -d)"
 esac
 
 if [ "$($DISTRVENDOR -d)" = "ALTLinux" ] ; then
-    epm install https://zoom.us/client/latest/zoom_$arch.rpm
+    epm install $repack https://zoom.us/client/$version/zoom_$arch.rpm
     exit
 fi
 
--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -4,8 +4,12 @@ BUILDROOT="$1"
 
 SPEC="$2"
 
+# TODO: s/freetype/libfreetype/
+# see https://bugzilla.altlinux.org/show_bug.cgi?id=38892
+
 test -f /lib64/ld-linux-x86-64.so.2 && exit
 
 # drop x86_64 req from 32 bit binary
 sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
 
+subst '1i%filter_from_requires /^mesa-dri-drivers(x86-32)/d' $SPEC
