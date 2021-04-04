6dd8e5525af7a055be65c1cb99089c068f106070
Vitaly Lipatov <lav@etersoft.ru>
2021-04-04 16:08:59 +0300

    assistant.sh: upgrade to 4.2, preinstall fontconfig-disable-type1-font-for-assistant

--- prescription.d/assistant.sh
+++ prescription.d/assistant.sh
@@ -26,12 +26,12 @@ pkg="$($DISTRVENDOR -p)"
 # TODO: why wget/curl are not supported download-to?
 case $pkg in
     rpm)
-        URL="https://xn--80akicokc0aablc.xn--p1ai/%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C/Download/338"
-        OPKG=assistant-4.0-699.x86_64.rpm
+        URL="https://xn--80akicokc0aablc.xn--p1ai/%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C/Download/386"
+        OPKG=assistant-4.2-266.x86_64.rpm
         ;;
     deb)
-        URL="https://xn--80akicokc0aablc.xn--p1ai/%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C/Download/337"
-        OPKG=assistant_4.0-700_amd64.deb
+        URL="https://xn--80akicokc0aablc.xn--p1ai/%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C/Download/392"
+        OPKG=assistant_4.2-266_amd64.deb
         ;;
     *)
         fatal "$($DISTRVENDOR -e) is not supported (package type is $pkg)"
@@ -45,6 +45,7 @@ esac
 #repack=''
 #[ "$($DISTRVENDOR -p)" = "deb" ] || repack='--repack'
 
+[ "$($DISTRVENDOR -d)" = "ALTLinux" ] && epmi --skip-installed fontconfig-disable-type1-font-for-assistant
 
 OPKG=/tmp/$OPKG
 $EGET -O $OPKG $URL
