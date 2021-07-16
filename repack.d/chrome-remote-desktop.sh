9af5bb7f54b926c0a65aec4676e682c33a079a9a
Vitaly Lipatov <lav@altlinux.ru>
2021-07-16 08:22:45 +0300

    repack: fix packing chrome-remote-desktop, hide it from epm play list

--- repack.d/chrome-remote-desktop.sh
+++ repack.d/chrome-remote-desktop.sh
@@ -5,5 +5,8 @@ SPEC="$2"
 PKGNAME=chrome-remote-desktop
 
 rm -f $BUILDROOT/etc/cron.daily/$PKGNAME
-subst 's|.*/etc/cron.daily/$PKGNAME.*||' $SPEC
+subst "s|.*/etc/cron.daily/$PKGNAME.*||" $SPEC
+
+# install all requires packages before packing ($ rpmreqs chrome-remote-desktop  | xargs echo)
+epm install --skip-installed coreutils glib2 libcairo libdbus libdrm libexpat libgbm libgio libgtk+3 libnspr libnss libpango libX11 libxcb libXdamage libXext libXfixes libXrandr libXtst python3-base python3-module-psutil sudo python3
 
