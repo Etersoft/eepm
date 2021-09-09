0f27ec3ff88c6a7754ee4ae6bda09612f47a73ab
Vitaly Lipatov <lav@altlinux.ru>
2021-09-10 00:48:43 +0300

    test_all.sh: improve tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -5,12 +5,22 @@ fatal()
     exit 1
 }
 
+restore_epm()
+{
+	# in the far future: epm upgrade /usr/src/RPM/RPMS/noarch/*.rpm
+	epm --auto install /usr/src/RPM/RPMS/noarch/*.rpm
+}
+
 set -e -x
 set -o pipefail
 
 epm print info
 
 epm update
+
+epm upgrade /usr/src/RPM/RPMS/noarch/*.rpm
+
+epm downgrade /usr/src/RPM/RPMS/noarch/*.rpm
 
 epm --auto remove erc
 
@@ -34,6 +44,7 @@ epm cl erc | head
 
 # Sisyphus -> p10
 epm --auto --force --force-yes downgrade-release p10
+restore_epm
 
 # p10 -> p9
 epm --auto --force --force-yes downgrade-release
@@ -41,13 +52,22 @@ restore_epm
 
 # p9 -> p10
 epm --auto --force --force-yes upgrade-release
+restore_epm
 
 # try upgrade p10
 epm --auto --force --force-yes upgrade-release && fatal
+restore_epm
 
 # p10 -> Sisyphus
 epm --auto --force --force-yes upgrade-release Sisyphus
+restore_epm
 
 epm checkpkg eepm
+
+epm upgrade eepm
+restore_epm || :
+
+epm downgrade eepm
+restore_epm || :
 
 epm clean
