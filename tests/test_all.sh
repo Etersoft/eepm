4b8d95095a58ba95d2a8b55293e320f02cfa0f0b
Vitaly Lipatov <lav@altlinux.ru>
2021-09-09 22:56:18 +0300

    test_all.sh: improve

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,6 +1,17 @@
 #!/bin/sh -e
 
+fatal()
+{
+    exit 1
+}
+
+set -o pipefail
+
+epm print info
+
 epm update
+
+epm --auto remove erc
 
 epm --auto install erc
 
@@ -8,6 +19,30 @@ epm --auto remove erc
 
 epm --auto autoremove
 
+epm --auto autoremove --direct
+
+epm --auto autoorphans
+
 epm --auto upgrade
 
-epm --auto downgrade-release
+epmqf bash
+
+epm ql eepm | head
+
+epm cl erc | head
+
+epm --auto --force downgrade-release p10
+
+epm --auto --force downgrade-release
+
+epm --auto --force upgrade-release
+
+epm --auto --force upgrade-release
+
+epm --auto --force upgrade-release && fatal
+
+epm --auto --force upgrade-release Sisyphus
+
+epm checkpkg eepm
+
+epm clean
