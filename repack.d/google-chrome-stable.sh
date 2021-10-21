07ff2f76453cc5e4e1a93436575b8c15fb78356b
Vitaly Lipatov <lav@altlinux.ru>
2021-10-21 10:04:41 +0300

    google-chrome-stable.sh: fix icon packing

--- repack.d/google-chrome-stable.sh
+++ repack.d/google-chrome-stable.sh
@@ -3,14 +3,14 @@
 BUILDROOT="$1"
 SPEC="$2"
 
-subst 's|%dir "/opt/google/chrome"|%dir "/opt/google/chrome"\n/usr/share/icons/hicolor/*x*/apps/*.png|' $SPEC
+subst 's|%files|%files\n/usr/share/icons/hicolor/*x*/apps/*.png|' $SPEC
 
 # Make relative symlink
 rm -f $BUILDROOT/usr/bin/google-chrome-stable
 ln -s ../../opt/google/chrome/google-chrome $BUILDROOT/usr/bin/google-chrome-stable
 ln -s google-chrome-stable $BUILDROOT/usr/bin/google-chrome
 
-for i in 16 22 24 32 48 64 128 256 ; do
+for i in 16 24 32 48 64 128 256 ; do
     mkdir -p $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/
     cp $BUILDROOT/opt/google/chrome/product_logo_$i.png $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/google-chrome.png
 done
