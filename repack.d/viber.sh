85e0b7b6a3c04e5fdda5bc446960d8d88c344eb7
Vitaly Lipatov <lav@altlinux.ru>
2021-10-28 03:07:15 +0300

    epm play viber: remove versioned libtiff5 require (ALT bug 41224)

--- repack.d/viber.sh
+++ repack.d/viber.sh
@@ -7,3 +7,8 @@ mkdir -p $BUILDROOT/usr/bin/
 ln -s /opt/viber/Viber $BUILDROOT/usr/bin/viber
 ln -s /opt/viber/Viber $BUILDROOT/usr/bin/Viber
 subst 's|%files|%files\n/usr/bin/viber\n/usr/bin/Viber|' $SPEC
+
+subst '1i%filter_from_requires /^libtiff.so.5(LIBTIFF_.*/d' $SPEC
+
+#REQUIRES="libtiff5"
+#subst "1iRequires:$REQUIRES" $SPEC
