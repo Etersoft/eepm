5f14496a5762c15b5850cd64b20a9ef65b1b931b
Vitaly Lipatov <lav@altlinux.ru>
2021-12-09 22:37:25 +0300

    epm repack zoom: drop embedded libs from reqs

--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -7,9 +7,17 @@ SPEC="$2"
 # TODO: s/freetype/libfreetype/
 # see https://bugzilla.altlinux.org/show_bug.cgi?id=38892
 
-test -f /lib64/ld-linux-x86-64.so.2 && exit
+if [ ! -f /lib64/ld-linux-x86-64.so.2 ] ; then
+    # drop x86_64 req from 32 bit binary
+    sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
+fi
 
-# drop x86_64 req from 32 bit binary
-sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
+# TODO: add all subdirs
+subst 's|%files|%files\n%dir /opt/zoom|' $SPEC
 
 subst '1i%filter_from_requires /^mesa-dri-drivers(x86-32)/d' $SPEC
+
+# ignore embedded libs
+for i in libQt5 libav libfdkaac libmpg123 libquazip libturbojpeg libicu ; do
+    subst "1i%filter_from_requires /^$i.*/d" $SPEC
+done
