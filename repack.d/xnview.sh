7e1aa7c498fa460953ea0b555ea40bf4e5a86f49
Vitaly Lipatov <lav@altlinux.ru>
2021-12-26 21:16:10 +0300

    epm play xnview: remove libs embedded in Plugins

--- repack.d/xnview.sh
+++ repack.d/xnview.sh
@@ -17,5 +17,10 @@ for i in libQt5 libav libcrypto.so libdbus-1.so libicu liblibraw.so libssl.so li
     subst "1i%filter_from_requires /^$i.*/d" $SPEC
 done
 
-epm install --skip-installed bzlib fontconfig libalsa libcairo libcups libdrm libfreetype /usr/bin/perl zlib libXv glib2 libatk libcairo-gobject libEGL libgdk-pixbuf libgio libGL libgst-plugins1.0 libgstreamer1.0 libgtk+2 libgtk+3 libhalf12 libiex12 libilmthread12 libpango libpulseaudio libsqlite3 libwebp7 libX11 libxcb libxcb-render-util libXcomposite libXext libXfixes libxkbcommon libxkbcommon-x11 libXrender
+# ignore embedded libs for Plugins
+for i in libHalf.so libIex libIlmThread libwebp ; do
+    subst "1i%filter_from_requires /^$i.*/d" $SPEC
+done
+
+epm install --skip-installed bzlib fontconfig libalsa libcairo libcups libdrm libfreetype /usr/bin/perl zlib libXv glib2 libatk libcairo-gobject libEGL libgdk-pixbuf libgio libGL libgst-plugins1.0 libgstreamer1.0 libgtk+2 libgtk+3 libpango libpulseaudio libsqlite3 libX11 libxcb libxcb-render-util libXcomposite libXext libXfixes libxkbcommon libxkbcommon-x11 libXrender
 
