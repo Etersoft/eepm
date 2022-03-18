7d6a5cee52e7ccf1963fa7c8e8911b9f2da758cf
Vitaly Lipatov <lav@altlinux.ru>
2022-03-19 01:25:09 +0300

    epm play zoom: fix repack (ALT bug 41775) (ALT bug 41954)

--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -8,6 +8,7 @@ SPEC="$2"
 # see https://bugzilla.altlinux.org/show_bug.cgi?id=38892
 
 if [ ! -f /lib64/ld-linux-x86-64.so.2 ] ; then
+    # TODO: use patchelf
     # drop x86_64 req from 32 bit binary
     sed -E -i -e "s@/lib64/ld-linux-x86-64.so.2@/lib/ld-linux.so.2\x0________@" $BUILDROOT/opt/zoom/libQt5Core.so.*
 fi
@@ -15,9 +16,23 @@ fi
 # TODO: add all subdirs
 subst 's|%files|%files\n%dir /opt/zoom|' $SPEC
 
+# fix broken symlink
+rm -fv $BUILDROOT/usr/bin/zoom
+ln -sv /opt/zoom/ZoomLauncher $BUILDROOT/usr/bin/zoom
+
 subst '1i%filter_from_requires /^mesa-dri-drivers(x86-32)/d' $SPEC
 
 # ignore embedded libs
 for i in libQt5 libav libfdkaac libmpg123 libquazip libturbojpeg libicu ; do
     subst "1i%filter_from_requires /^$i.*/d" $SPEC
 done
+
+epm assure patchelf || exit
+for i in $BUILDROOT/opt/zoom/{libicui18n.so,libicui18n.so.56,libicuuc.so,libicuuc.so.56} ; do
+    a= patchelf --set-rpath '$ORIGIN/' $i
+done
+
+# qt5-3d libqt5-3dquickscene2d
+rm -v $BUILDROOT/opt/zoom/QtQuick/Scene2D/libqtquickscene2dplugin.so $BUILDROOT/opt/zoom/QtQuick/Scene3D/libqtquickscene3dplugin.so
+subst 's|.*/opt/zoom/QtQuick/Scene2D/libqtquickscene2dplugin.so.*||' $SPEC
+subst 's|.*/opt/zoom/QtQuick/Scene3D/libqtquickscene3dplugin.so.*||' $SPEC
