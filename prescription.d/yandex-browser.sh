5be0d68728c90e0cf7fdfa3bc734535c200042e4
Vitaly Lipatov <lav@altlinux.ru>
2022-03-18 14:44:18 +0300

    epm play yandex-browser: add install yandex-browser-beta-codecs-ffmpeg-extra (ALT bug 40113)

--- prescription.d/yandex-browser.sh
+++ prescription.d/yandex-browser.sh
@@ -23,9 +23,29 @@ fi
 # epm uses eget to download * names
 epm install "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-beta/$(epm print constructname $PKGNAME "*" amd64 deb)"
 
-echo
-echo '
-You can run
- # /opt/yandex/browser-beta/update-ffmpeg
-to download and install libffmpeg.so with proprietary codecs from chromium-codecs-ffmpeg-extra package
-'
+# used in update-ffmpeg
+epm install jq tar binutils
+
+# install appropriate ffmpeg extra codecs
+pack_ffmpeg() {
+  SOURCE="usr/lib/chromium-browser/libffmpeg.so"
+  DEST="/opt/yandex/browser-beta"
+  mkdir -p .$DEST
+  cp $SOURCE .$DEST
+  CNAME=$(echo "$(basename $SUITABLE_URLS)" | sed -e "s|chromium|$PKGNAME|" -e "s|-0ubuntu.*|-1.tar|")
+  tar cvf $CNAME ./$(dirname $DEST)
+  epm --repack install $CNAME
+}
+
+[ -s /opt/yandex/browser-beta/update-ffmpeg ] || fatal "/opt/yandex/browser-beta/update-ffmpeg is missed"
+SC=$(mktemp)
+sed -e 's|install_ffmpeg &&|pack_ffmpeg &&|' < /opt/yandex/browser-beta/update-ffmpeg > $SC
+. $SC
+rm -f $SC
+
+#echo
+#echo '
+#You can run
+# # /opt/yandex/browser-beta/update-ffmpeg
+#to download and install libffmpeg.so with proprietary codecs from chromium-codecs-ffmpeg-extra package
+#'
