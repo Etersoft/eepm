570ff3a50b14dd4ff18185195772e517cb2fb3cd
Vitaly Lipatov <lav@altlinux.ru>
2022-03-20 22:41:35 +0300

    epm play eagle: add downloading workaround via IPFS

--- prescription.d/eagle.sh
+++ prescription.d/eagle.sh
@@ -19,9 +19,16 @@ fi
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
 VERSION=9.6.2
+IPFSHASH=Qmd38jJnTnUMUeJuKSDBGesqXF3SxEahUVZc6NUPyMKgj1
 PKGURL="https://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_${VERSION}_English_Linux_64bit.tar.gz"
 
-epm install --repack "$PKGURL"
+if ! epm install --repack "$PKGURL" ; then
+    echo "It is possible you are blocked from USA, trying get from IPFS ..."
+    pkgname=$(basename $PKGURL)
+    $EGET -O $pkgname http://dhash.ru/ipfs/$IPFSHASH || fatal "Can't get $pkgname from IPFS."
+    epm install --repack $pkgname || exit
+    rm -fv $pkgname
+fi
 
 echo
 echo "
