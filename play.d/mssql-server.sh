022bfc13303e3d4b0fd508965a5303edd714249b
Vitaly Lipatov <lav@altlinux.ru>
2022-03-26 00:30:27 +0300

    epm play mssql-*: add AstraLinux support

--- play.d/mssql-server.sh
+++ play.d/mssql-server.sh
@@ -1,44 +1,47 @@
 #!/bin/sh
 
-# TODO: common place
-fatal()
-{
-    echo "FATAL: $*" >&2
-    exit 1
-}
-
 PKGNAME=mssql-server
+DESCRIPTION="MS SQL Server 2019 from the official site"
+
+DEBREPO="deb http://ftp.ru.debian.org/debian/ stretch main"
 
 if [ "$1" = "--remove" ] ; then
-    epm remove $PKGNAME mssql-tools msodbcsql17
+    epm remove $PKGNAME
     echo
     echo "Check data directory /var/opt/mssql if you need remove it too."
     exit
 fi
 
-[ "$1" != "--run" ] && echo "MS SQL Server 2019 from the official site" && exit
+. $(dirname $0)/common.sh
+
 
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
-if [ "$($DISTRVENDOR -d)" = "ALTLinux" ] ; then
-    serv mssql-server stop
+serv mssql-server stop
+
+case "$($DISTRVENDOR -d)" in
+  "AstraLinux")
+    # we have libc++1-9, but this package requires libc++1
+    epm ar $DEBREPO ; epm update
+    epm install libc++1
+    epm install https://packages.microsoft.com/ubuntu/20.04/mssql-server-2019/pool/main/m/mssql-server/mssql-server_1*_amd64.deb
+    epm rr $DEBREPO ; epm update
+    ;;
+  "ALTLinux")
     epm install --repack https://packages.microsoft.com/rhel/8/mssql-server-2019/mssql-server-1*.x86_64.rpm || fatal
-    epm install --repack https://packages.microsoft.com/rhel/8/prod/mssql-tools-1*.x86_64.rpm
-    epm install --repack https://packages.microsoft.com/rhel/8/prod/msodbcsql17-1*.x86_64.rpm
+    ;;
+  *)
+    fatal "$(DISTRVENDOR -d) is not supported yet."
+    ;;
+esac
 
-    sudo /opt/mssql/bin/mssql-conf setup accept-eula
+$SUDO /opt/mssql/bin/mssql-conf setup accept-eula
 
-    epm install unixODBC
-    sudo odbcinst -i -d -f /opt/microsoft/msodbcsql17/etc/odbcinst.ini
-    sudo odbcinst -q -d
+serv mssql-server on
 
-    serv mssql-server on
+
 cat <<EOF
 
 Use follow command to check the connection to the MS SQL server:
 $ /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -Q 'sp_databases'
 EOF
-    exit
-fi
-
-fatal "We support only ALTLinux for now"
--- play.d/mssql-tools.sh
+++ play.d/mssql-tools.sh
@@ -1,36 +1,43 @@
 #!/bin/sh
 
-# TODO: common place
-fatal()
-{
-    echo "FATAL: $*" >&2
-    exit 1
-}
-
 PKGNAME=mssql-tools
+DESCRIPTION="MS SQL Server sqlcmd and bcp from the official site"
 
 if [ "$1" = "--remove" ] ; then
-    epm remove $PKGNAME mssql-tools msodbcsql17
+    epm remove $PKGNAME msodbcsql17
     exit
 fi
+
+. $(dirname $0)/common.sh
 
 [ "$1" != "--run" ] && echo "MS SQL Server sqlcmd and bcp from the official site" && exit
 
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
 
-if [ "$($DISTRVENDOR -d)" = "ALTLinux" ] ; then
+case "$($DISTRVENDOR -d)" in
+  "AstraLinux")
+    epm install https://packages.microsoft.com/debian/8/prod/pool/main/m/msodbcsql17/msodbcsql17_1*_amd64.deb
+    epm install https://packages.microsoft.com/debian/8/prod/pool/main/m/mssql-tools/mssql-tools_1*_amd64.deb
+    #epm install https://packages.microsoft.com/ubuntu/16.04/prod/pool/main/m/mssql-tools/mssql-tools_1*_amd64.deb
+    #epm install https://packages.microsoft.com/ubuntu/16.04/prod/pool/main/m/msodbcsql17/msodbcsql17_1*_amd64.deb
+    epm install unixodbc
+    ;;
+  "ALTLinux")
     epm install --repack https://packages.microsoft.com/rhel/8/prod/mssql-tools-1*.x86_64.rpm
     epm install --repack https://packages.microsoft.com/rhel/8/prod/msodbcsql17-1*.x86_64.rpm
-
     epm install unixODBC
-    sudo odbcinst -i -d -f /opt/microsoft/msodbcsql17/etc/odbcinst.ini
-    sudo odbcinst -q -d
+    ;;
+  *)
+    fatal "$(DISTRVENDOR -d) is not supported yet."
+    ;;
+esac
+
+sudo odbcinst -i -d -f /opt/microsoft/msodbcsql17/etc/odbcinst.ini
+sudo odbcinst -q -d
+
+
 cat <<EOF
 
 Use follow command to check the connection to the MS SQL server:
 $ /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -Q 'sp_databases'
 EOF
-    exit
-fi
-
-fatal "We support only ALTLinux for now"
