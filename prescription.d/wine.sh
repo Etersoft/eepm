961865293b0e5a6f4588d4b37c0d10deba983ba7
Vitaly Lipatov <lav@etersoft.ru>
2022-04-02 18:13:02 +0300

    epm prescription wine: fix for 32-bit system

--- prescription.d/wine.sh
+++ prescription.d/wine.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 MAIN=wine
-[ -n "$2" ] && MAIN="$2"
 
 vendor="$($DISTRVENDOR -s)" ; [ "$vendor" = "alt" ] || { echo "Only ALT distros is supported for now" ; exit 1 ; }
-
 arch="$($DISTRVENDOR -a)"
 
 PKGCOMMON="wine-mono wine-gecko winetricks"
@@ -15,24 +13,33 @@ if [ "$1" = "--remove" ] ; then
     exit
 fi
 
-[ "$1" != "--run" ] && echo "Install $MAIN packages (add vanilla or etersoft if you need these packages)" && exit
+
+[ "$1" != "--run" ] && echo "Install $MAIN packages (add wine-vanilla if you need these packages)" && exit
+
+ONLY32=''
+[ "$2" == "--only-i586" ] && ONLY32=1 && shift
+[ -n "$2" ] && MAIN="$2"
 
 # do some magic: if winetricks more than 20210206, we have new wine package naming
 epm install winetricks || exit 1
 WTVER="$(epm print version for package winetricks)"
 if [ "$(epm print compare package version "$WTVER" "20210206")" = "1" ] ; then
-    PKGNAMES="$MAIN-full $MAIN-twain $PKGCOMMON"
+    PKGNAMES="$MAIN-full $MAIN-twain"
     PKGNAMES32="i586-$MAIN i586-$MAIN-gl i586-$MAIN-twain"
 else
     # old naming scheme
-    PKGNAMES="$MAIN lib$MAIN lib$MAIN-gl lib$MAIN-twain $PKGCOMMON"
-    PKGNAMES32=''
+    PKGNAMES="$MAIN lib$MAIN lib$MAIN-gl lib$MAIN-twain"
+    PKGNAMES32="i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl i586-lib$MAIN-twain"
     echo "We recommend ask about more new wine from your vendor."
 fi
 
 case $arch in
     x86_64)
-        PKGNAMES="$PKGNAMES $PKGNAMES32"
+        PKGNAMES="$PKGNAMES $PKGNAMES32 $PKGCOMMON"
+        [ -n "$ONLY32" ] && PKGNAMES="$PKGNAMES32 $PKGCOMMON"
+        ;;
+    x86)
+        PKGNAMES="$PKGNAMES $PKGCOMMON"
         ;;
     *)
         echo "Arch $arch is not yet supported" && exit 1
