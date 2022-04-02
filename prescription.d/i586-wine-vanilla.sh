f57014fcf8f31f63daadd47163204d0c6f9de0b8
Vitaly Lipatov <lav@etersoft.ru>
2022-04-02 18:17:46 +0300

    epm play: drop obsoleted i586-wine-vanilla, i586-wine-etersoft, i586-wine

--- prescription.d/i586-wine-vanilla.sh
+++ prescription.d/i586-wine-vanilla.sh
@@ -1,23 +1,4 @@
 #!/bin/sh
 
-MAIN=wine-vanilla
-
-distro="$($DISTRVENDOR -d)" ; [ "$distro" = "ALTLinux" ] || [ "$distro" = "ALTServer" ] || { echo "Only ALTLinux is supported" ; exit 1 ; }
-[ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
-
-PKGNAMES="lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl"
-
-if [ "$1" = "--remove" ] ; then
-    epm remove $PKGNAMES
-    exit
-fi
-
-[ "$1" != "--run" ] && echo "Install 32 bit $MAIN packages on 64 bit system" && exit
-
-# Устанавливаем wine
-epm install $PKGNAMES || exit
-
-# Доставляем пропущенные модули (подпакеты) для установленных 64-битных
-epm prescription i586-fix || exit
-
-echo "See '# epm play wine wine-vanilla' command to get best result."
+echo "This is obsoleted script. Use # epm play wine wine-vanilla" >&2
+exit 1
--- prescription.d/i586-wine.sh
+++ prescription.d/i586-wine.sh
@@ -1,23 +1,4 @@
 #!/bin/sh
 
-MAIN=wine
-
-distro="$($DISTRVENDOR -d)" ; [ "$distro" = "ALTLinux" ] || [ "$distro" = "ALTServer" ] || { echo "Only ALTLinux is supported" ; exit 1 ; }
-[ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
-
-PKGNAMES="lib$MAIN i586-$MAIN i586-lib$MAIN i586-lib$MAIN-gl"
-
-if [ "$1" = "--remove" ] ; then
-    epm remove $PKGNAMES
-    exit
-fi
-
-[ "$1" != "--run" ] && echo "Install 32 bit $MAIN packages on 64 bit system" && exit
-
-# Устанавливаем wine
-epm install $PKGNAMES || exit
-
-# Доставляем пропущенные модули (подпакеты) для установленных 64-битных
-epm prescription i586-fix || exit
-
-echo "See '# epm play wine' command to get best result."
+echo "This is obsoleted script. Use # epm play wine" >&2
+exit 1
