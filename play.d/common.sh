4b40a3a5bf8cdc91fdf290fc7a5017b114b28dff
Vitaly Lipatov <lav@etersoft.ru>
2022-05-26 18:02:36 +0300

    play.d/common.sh: major clean up

--- play.d/common.sh
+++ play.d/common.sh
@@ -6,33 +6,34 @@ fatal()
     exit 1
 }
 
-if [ "$1" = "--remove" ] ; then
-    epm remove $PKGNAME
-    exit
-fi
-
-if [ "$1" = "--package" ] ; then
-    echo "$PKGNAME"
-    exit
-fi
-
-if [ "$1" = "--installed" ] ; then
-    epm installed $PKGNAME
-    exit
-fi
-
-if [ "$1" = "--description" ] ; then
-     echo "$DESCRIPTION"
-     exit
-fi
-
-
-[ "$1" != "--run" ] && [ "$1" != "--update" ] && fatal "Unknown command $1"
-
-if [ "$1" = "--update" ] ; then
-     if ! epm installed $PKGNAME ; then
-         echo "Skipping update of $PKGNAME (package is not installed)"
-         exit
-     fi
-fi
+case "$1" in
+    "--remove")
+        epm remove $PKGNAME
+        exit
+        ;;
+    "--package")
+        echo "$PKGNAME"
+        exit
+        ;;
+    "--installed")
+        epm installed $PKGNAME
+        exit
+        ;;
+    "--description")
+        echo "$DESCRIPTION"
+        exit
+        ;;
+    "--update")
+        if ! epm installed $PKGNAME ; then
+            echo "Skipping update of $PKGNAME (package is not installed)"
+            exit
+        fi
+        ;;
+    "--run")
+        # just pass
+        ;;
+    *)
+        fatal "Unknown command $1"
+        ;;
+esac
 
