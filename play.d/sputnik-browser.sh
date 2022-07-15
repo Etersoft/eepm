f5584f26a63de4ce75c80d42ad2225683bd42f05
Vitaly Lipatov <lav@etersoft.ru>
2022-07-16 02:41:16 +0300

    play.d/sputnik-browser.sh: use url from b2c-distrs channel

--- play.d/sputnik-browser.sh
+++ play.d/sputnik-browser.sh
@@ -8,36 +8,49 @@ DESCRIPTION="Sputnik browser from the official site"
 
 [ "$($DISTRVENDOR -a)" != "x86_64" ] && echo "Only x86_64 is supported by vendor" && exit 1
 
+url_by_id_content()
+{
+    local id_content="$1"
+    epm tool eget -q -O- https://sputnik-lab.com/api-updates/updates/apps/meta?channel=b2c-distrs-on-site | grep -A6 "$id_content" | tail -n1 | sed -e 's|.*"url": "||' -e 's|".*||'
+}
 
 case "$($DISTRVENDOR -e)" in
     Ubuntu/20.04)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/f02e89f4-a256-4a8f-964e-047e3a44eaff/sputnik-browser-stable_5.6.6306.0-1_amd64.deb
+        id_content='browser-b2c-ubuntu20-id'
         ;;
     Ubuntu/16.04)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/a5be3569-9c9b-4d91-ae7e-a44c7e96e907/sputnik-browser-stable_5.6.6312.0-1_amd64.deb
+        id_content='browser-b2c-ubuntu-id'
         ;;
     Ubuntu/*)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/f02e89f4-a256-4a8f-964e-047e3a44eaff/sputnik-browser-stable_5.6.6306.0-1_amd64.deb
+        id_content='browser-b2c-ubuntu20-id'
         ;;
     ALTLinux/c8)
-        epm --repack install https://upd.sputnik-lab.com/api-updates/updates/download/94d70495-75ec-4ad7-831d-8008d0525d90/sputnik-browser-stable-5.6.6322.0-1.x86_64.rpm
+        id_content='browser-b2c-alt-id'
         ;;
     ALTLinux/*|ALTServer/*)
-        epm --repack install https://upd.sputnik-lab.com/api-updates/updates/download/49734e35-cfe1-493c-bfae-8fa83f2a4365/sputnik-browser-stable-5.6.6324.0-1.x86_64.rpm
+        id_content='browser-b2c-alt9-id'
+        ;;
+    AstraLinux/orel)
+        id_content='browser-b2c-astrase-id'
         ;;
     AstraLinux/*)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/3fb587b6-19fa-421b-b757-7f232ed46d74/sputnik-browser-stable_5.6.6316.0-1_amd64.deb
+        id_content='browser-b2c-astrace-id'
         ;;
     RedOS/*)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/569b5726-5899-4bb1-b064-6b1b00a5bc15/sputnik-browser-stable-5.6.6325.0-1.x86_64.rpm
+        id_content='browser-b2c-redos-id'
         ;;
     RosaLinux/*)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/0618f239-abfb-44f8-a771-1622a6336eb6/sputnik-browser-stable-5.6.6319.0-1.x86_64.rpm
+        id_content='browser-b2c-rosa-id'
         ;;
     Windows/*)
-        epm install https://upd.sputnik-lab.com/api-updates/updates/download/c8b0945b-4d89-4a72-82ec-b7ed1b259384/SputnikOfflineInstaller-free-5.6.6282.0.exe
+        id_content='browser-b2c-win-id'
+        ;;
+    *)
+        fatal "Unsupported system $($DISTRVENDOR -e)"
         ;;
 esac
+
+epm install $(url_by_id_content $id_content)
 
 ERR=$?
 if [ "$ERR" = 0 ] ; then
