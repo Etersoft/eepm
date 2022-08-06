0f6b5aecaee2572a7b2104ab4c8d56691bab1737
Vitaly Lipatov <lav@etersoft.ru>
2022-08-06 15:16:07 +0300

    epm play: add signal messenger support (thanks, george@)

---
+++ repack.d/signal-desktop.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=signal-desktop
+PRODUCTCUR=Signal
+PRODUCTDIR=/opt/$PRODUCTCUR
+
+. $(dirname $0)/common-chromium-browser.sh
+
+install_deps
+
+subst '1iAutoProv:no' $SPEC
+
+subst "s|.*/etc/apt.*||" $SPEC
+subst '/linux-arm64/d' $SPEC
+rm -rf $BUILDROOT/opt/Signal/resources/app.asar.unpacked/node_modules/ffi-napi/node_modules/ref-napi/prebuilds/linux-arm64
+
+fix_chrome_sandbox
+
+add_bin_link_command
