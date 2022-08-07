008ba4ce0c0d9594cf7a53d980bf722b4daad6ba
Vitaly Lipatov <lav@etersoft.ru>
2022-08-07 07:59:40 +0300

    epm play: add gitlab-runner install

---
+++ play.d/gitlab-runner.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PKGNAME=gitlab-runner
+SUPPORTEDARCHES="armhf aarch64 x86 x86_64 ppc64le"
+DESCRIPTION='Gitlab runner'
+
+. $(dirname $0)/common.sh
+
+arch="$($DISTRVENDOR -a)"
+pkg="$($DISTRVENDOR -p)"
+case "$arch" in
+    x86_64)
+        arch=amd64
+        ;;
+    aarch64)
+        arch=arm64
+        ;;
+    armhf)
+        arch=armhf
+        ;;
+    x86)
+        arch=i686
+        [ "$pkg" = "deb" ] && arch=i386
+        ;;
+esac
+
+# https://docs.gitlab.com/runner/install/linux-manually.html
+# https://gitlab-runner-downloads.s3.amazonaws.com/latest/index.html
+epm install "https://gitlab-runner-downloads.s3.amazonaws.com/latest/rpm/gitlab-runner_${arch}.$pkg"
