1819b4cb03f48143e88722c10b398a62f245c90b
Vitaly Lipatov <lav@etersoft.ru>
2022-08-07 14:11:19 +0300

    epm play: add MyOffice support

---
+++ play.d/myoffice.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PKGNAME=myoffice-standard-home-edition
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="MyOffice for Linux from the official site"
+
+. $(dirname $0)/common.sh
+
+arch=$($DISTRVENDOR --distro-arch)
+pkgtype=$($DISTRVENDOR -p)
+
+# https://preset.myoffice-app.ru/myoffice-standard-home-edition_2022.01-1.28.0.4_amd64
+PKGMASK="$(epm print constructname $PKGNAME "*" $arch)"
+PKG="$(epm tool eget --list --latest https://myoffice.ru/products/standard-home-edition/ $PKGMASK)" || fatal "Can't get package URL"
+
+# /var/lib/dpkg/info/myoffice-standard-home-edition.postinst: line 62: xdg-desktop-menu: command not found
+epm assure xdg-utils
+
+epm --scripts install "$PKG"
