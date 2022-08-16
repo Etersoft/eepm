c32b74d35bfb7b6722070b7fb180c8c3e36b48fc
Vitaly Lipatov <lav@etersoft.ru>
2022-08-16 04:52:10 +0300

    fix quotes in format strings

--- tests/test_sudo.sh
+++ tests/test_sudo.sh
@@ -11,4 +11,8 @@ sudorun()
 }
 
 #run "EDITOR=vim crontab -e"
-sudorun EDITOR=vim crontab -e
+#sudorun EDITOR=vim crontab -e
+dorun rpm -q --queryformat "%{size}@%{name}-%{version}-%{release}\\\n" mc
+dorun rpm -q --queryformat '%{size}@%{name}-%{version}-%{release}\\n' mc
+#dorun dpkg-query -W --showformat="\\\${Package}-\\\${Version}\\\n" -- mc
+#dorun dpkg-query -W --showformat='\${Package}-\${Version}\\n' -- mc
