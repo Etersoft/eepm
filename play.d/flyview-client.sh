a358450433f37abdf1b6a3ddc2d521f1577b873e
Vitaly Lipatov <lav@etersoft.ru>
2022-09-03 01:14:34 +0300

    epm play: add FlyView client

---
+++ play.d/flyview-client.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PKGNAME=flyview-client
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="FlyView Client from the official site"
+
+. $(dirname $0)/common.sh
+
+PKG="$(epm tool eget --list --latest https://flyviewvms.ru/downloads/ "flyview-client*linux64.deb")"
+
+epm install $PKG
