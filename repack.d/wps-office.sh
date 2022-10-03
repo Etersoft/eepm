f0975f10ed0670b728e12954756d421c63947f5e
Vitaly Lipatov <lav@etersoft.ru>
2022-10-03 08:02:03 +0300

    epm repack wps-office: drop /usr/share/desktop-directories/wps-office.directory (ALT bug 43751)

--- repack.d/wps-office.sh
+++ repack.d/wps-office.sh
@@ -17,5 +17,8 @@ remove_dir /etc/cron.d
 remove_dir /etc/logrotate.d
 remove_dir /etc/xdg/menus/applications-merged
 
+# ALT bug 43751
+remove_file /usr/share/desktop-directories/wps-office.directory
+
 #epm install --skip-installed bzlib fontconfig libalsa libcairo libcups libdrm libfreetype /usr/bin/perl zlib libXv glib2 libatk libcairo-gobject libEGL libgdk-pixbuf libgio libGL libgst-plugins1.0 libgstreamer1.0 libgtk+2 libgtk+3 libpango libpulseaudio libsqlite3 libX11 libxcb libxcb-render-util libXcomposite libXext libXfixes libxkbcommon libxkbcommon-x11 libXrender
 
