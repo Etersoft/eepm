e10f02229944a51597ce966fea56dd1b55ed6b5a
Vitaly Lipatov <lav@etersoft.ru>
2022-10-03 08:17:01 +0300

    commit packed 3.27.1

--- packed/epm.sh
+++ packed/epm.sh
@@ -11312,7 +11312,7 @@ Examples:
 
 print_version()
 {
-        echo "EPM package manager version 3.27.0  https://wiki.etersoft.ru/Epm"
+        echo "EPM package manager version 3.27.1  https://wiki.etersoft.ru/Epm"
         echo "Running on $($DISTRVENDOR -e) ('$PMTYPE' package manager uses '$PKGFORMAT' package format)"
         echo "Copyright (c) Etersoft 2012-2022"
         echo "This program may be freely redistributed under the terms of the GNU AGPLv3."
@@ -11322,7 +11322,7 @@ print_version()
 Usage="Usage: epm [options] <command> [package name(s), package files]..."
 Descr="epm - EPM package manager"
 
-EPMVERSION=3.27.0
+EPMVERSION=3.27.1
 verbose=$EPM_VERBOSE
 quiet=
 nodeps=
