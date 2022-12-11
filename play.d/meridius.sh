b1ea7bbd6ffe4241a489eef9b1bc01445d3f7f58
Vitaly Lipatov <lav@etersoft.ru>
2022-12-11 23:41:15 +0300

    epm play meridus: add support for install a given version

--- play.d/meridius.sh
+++ play.d/meridius.sh
@@ -6,5 +6,12 @@ DESCRIPTION="Meridius — music player for VK"
 
 . $(dirname $0)/common.sh
 
-URL=$(epm tool eget --list --latest https://github.com/PurpleHorrorRus/Meridius/releases "$PKGNAME-*.tar.gz") || fatal "Can't get package URL"
-epm install $URL
+VERSION="$2"
+
+if [ -n "$VERSION" ] ; then
+    URL="https://github.com/PurpleHorrorRus/Meridius/releases/download/v$VERSION/meridius-$VERSION.tar.gz"
+else
+    URL=$(epm tool eget --list --latest https://github.com/PurpleHorrorRus/Meridius/releases "$PKGNAME-*.tar.gz") || fatal "Can't get package URL"
+fi
+
+epm install "$URL"
