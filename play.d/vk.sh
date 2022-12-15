357469726f97f4bb35178c648b3ae69f4bc22e2d
Vitaly Lipatov <lav@etersoft.ru>
2022-12-15 23:38:18 +0300

    epm play: add VK messenger

---
+++ play.d/vk.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PKGNAME=vk
+SUPPORTEDARCHES="x86_64 x86"
+DESCRIPTION="VK Messenger from the official site"
+
+. $(dirname $0)/common.sh
+
+arch="$($DISTRVENDOR -a)"
+case "$arch" in
+    x86)
+        arch="i686"
+        ;;
+esac
+
+URL=$(eget --list --latest https://desktop.userapi.com/rpm/master/ "*.$arch.rpm")
+
+epm install $URL
