367bfa403a3b93da0324523bd42220f50ffba372
Vitaly Lipatov <lav@etersoft.ru>
2022-12-16 09:19:07 +0300

    epm repack rstudio: fix packing

--- repack.d/rstudio.sh
+++ repack.d/rstudio.sh
@@ -9,7 +9,10 @@ PRODUCTDIR=/usr/lib/$PRODUCT
 
 . $(dirname $0)/common.sh
 
-add_bin_exec_command $PRODUCT /usr/lib/rstudio/bin/rstudio
+add_bin_exec_command $PRODUCT
+
+# fix bug in upstream
+subst 's|/usr/lib/rstudio/bin/rstudio|$PRODUCTDIR/$PRODUCT|' $BUILDROOT$PRODUCTDIR/resources/app/bin/rstudio-backtrace.sh
 
 # https://bugzilla.altlinux.org/43794
 subst '1iAutoReq:yes,nopython,nopython3,nomono,nomonolib' $SPEC
