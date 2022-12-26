1e25e2c2b58388a74a259a51f0c00dd08f6b1f84
Vitaly Lipatov <lav@etersoft.ru>
2022-12-26 23:08:44 +0300

    epm repack trueconf: fix repack on Sisyphus (remove libtbbbind requires missed libhwloc.so.5)

--- repack.d/trueconf.sh
+++ repack.d/trueconf.sh
@@ -13,10 +13,17 @@ chmod a+x $BUILDROOT/opt/trueconf/trueconf
 chmod a+x $BUILDROOT/opt/trueconf/trueconf-autostart
 
 epm assure patchelf || exit
-for i in lib/libboost*.so  ; do
+for i in lib/lib*.so  ; do
     a= patchelf --set-rpath '$ORIGIN' $BUILDROOT$PRODUCTDIR/$i
 done
 
 for i in TrueConf ; do
     a= patchelf --set-rpath '$ORIGIN/lib' $BUILDROOT$PRODUCTDIR/$i
 done
+
+# libhwloc.so.5 => not found (we have only libhwloc.so.15)
+remove_file $PRODUCTDIR/lib/libtbbbind.so
+remove_file $PRODUCTDIR/lib/libtbbbind.so.2
+
+epm install --skip-installed pulseaudio libalsa libcrypto1.1 libcurl libdbus libGL libicu69 libidn libgs libprotobuf27 libarchive13  libXScrnSaver libspeex libspeexdsp libudev1 libv4l libX11 libxcb libXrandr liblame libatomic1 coreutils
+epm install --skip-installed libqt5-core libqt5-dbus libqt5-gui libqt5-multimedia libqt5-network libqt5-opengl libqt5-sql libqt5-svg libqt5-webkit libqt5-webkitwidgets libqt5-widgets libqt5-webengine libqt5-concurrent qt5-graphicaleffects qt5-imageformats qt5-qtquickcontrols
