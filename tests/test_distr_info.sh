bb9bd2298e000716797d0f8eeae8fe2d9d1fb907
Vitaly Lipatov <lav@etersoft.ru>
2022-12-28 12:58:19 +0300

    tests: add os-release tests

--- tests/test_distr_info.sh
+++ tests/test_distr_info.sh
@@ -6,15 +6,17 @@ WRITE=''
 for ROOTDIR in $(ls -1d etc/*) ; do
     export ROOTDIR
     e=$(../bin/distr_info -e)
+    s=$(../bin/distr_info -s)
+    idstr="$e $s"
     if [ -n "$WRITE" ] ; then
-        echo "$e" > $ROOTDIR/etalon.txt
+        echo "$idstr" > $ROOTDIR/etalon.txt
         continue
     fi
     le="$(cat $ROOTDIR/etalon.txt 2>/dev/null)"
-    if [ "$le" = "$e" ] ; then
-        printf "%23s -> %20s : %s\n" "$(basename $ROOTDIR)" "$e" "OK"
+    if [ "$le" = "$idstr" ] ; then
+        printf "%23s -> %20s : %s\n" "$(basename $ROOTDIR)" "$idstr" "OK"
     else
-        printf "%23s -> %20s : %s\n" "$(basename $ROOTDIR)" "$e" "FAIL (expect $le)"
+        printf "%23s -> %20s : %s\n" "$(basename $ROOTDIR)" "$idstr" "FAIL (expect $le)"
     fi
 done
 
