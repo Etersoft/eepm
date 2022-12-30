36202bc63ea1a6b5a8aa22759f4158981e9ca4a6
Vitaly Lipatov <lav@etersoft.ru>
2022-12-30 15:56:31 +0300

    epm play kyodialog: fix erc req

--- play.d/kyodialog.sh
+++ play.d/kyodialog.sh
@@ -16,8 +16,8 @@ PKGDIR="$(mktemp -d)"
 trap "rm -fr $PKGDIR" EXIT
 cd $PKGDIR || fatal
 epm tool eget "$URL" || fatal
-erc Linux_Universal_Driver.zip || fatal
-erc KyoceraLinuxPackages-*.tar.gz || fatal
+a= erc Linux_Universal_Driver.zip || fatal
+a= erc KyoceraLinuxPackages-*.tar.gz || fatal
 cd KyoceraLinuxPackages-*.tar || fatal
 cd Fedora/Global/kyodialog_x86_64 || fatal
 epm --repack install kyodialog-9.*.x86_64.rpm
