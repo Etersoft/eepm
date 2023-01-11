0a52f41f48d4dcf7bc2e8c7b85029f7f2c725ebb
Vitaly Lipatov <lav@etersoft.ru>
2023-01-11 17:01:31 +0300

    epm repack generic.sh: fix typo

--- repack.d/generic.sh
+++ repack.d/generic.sh
@@ -33,4 +33,4 @@ if [ -n "$flag_python3" ] ; then
 fi
 
 # Set high Epoche to override repository package
-subst "s|^\(Name: .*\)$|# Override reposity package\nEpoch: 100\n\1|g" $SPEC
+subst "s|^\(Name: .*\)$|# Override repository package\nEpoch: 100\n\1|g" $SPEC
