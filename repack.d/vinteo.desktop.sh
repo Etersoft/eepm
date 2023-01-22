20978488c75fbbb125be964819cccc9bde41ca08
Vitaly Lipatov <lav@etersoft.ru>
2023-01-22 16:15:07 +0300

    epm repack vinteo.desktop: cleanup

--- repack.d/vinteo.desktop.sh
+++ repack.d/vinteo.desktop.sh
@@ -5,11 +5,11 @@ BUILDROOT="$1"
 SPEC="$2"
 
 PRODUCT=vinteo.desktop
-PRODUCTDIR=/opt/VinteoDesktop/
+PRODUCTDIR=/opt/VinteoDesktop
 
 . $(dirname $0)/common-chromium-browser.sh
 
 add_bin_link_command vinteo.desktop
-add_bin_link_command $PRODUCT $PRODUCTDIR/vinteo.desktop
+subst 's|/opt/VinteoDesktop/||' $BUILDROOT/usr/share/applications/vinteo.desktop.desktop
 
 fix_chrome_sandbox
