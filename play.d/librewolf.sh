25569be55bb55b28eac90fe14555850ddbc202a9
Vitaly Lipatov <lav@etersoft.ru>
2023-01-23 20:34:05 +0300

    epm play librewolf: fix download URL

--- play.d/librewolf.sh
+++ play.d/librewolf.sh
@@ -6,13 +6,15 @@ DESCRIPTION="LibreWolf - a custom version of Firefox, focused on privacy, securi
 
 . $(dirname $0)/common.sh
 
+arch=x86_64
+
 pkgtype=$(epm print info -p)
 case $pkgtype in
     rpm)
         PKG="https://rpm.librewolf.net/pool/librewolf*.rpm"
         ;;
     deb)
-        PKG="https://deb.librewolf.net/pool/focal/main/libr/librewolf/librewolf_*all.deb"
+        PKG="https://deb.librewolf.net/pool/focal/librewolf-*$arch.deb"
         ;;
     *)
         fatal "Package target $pkgtype is not supported yet"
@@ -22,7 +24,7 @@ esac
 case "$(epm print info -s)" in
   alt)
       # uses old glibc needed for ALT p10
-      PKG="https://deb.librewolf.net/pool/focal/main/libr/librewolf/librewolf_*all.deb"
+      PKG="https://deb.librewolf.net/pool/focal/librewolf-*$arch.deb"
       epm install --repack $PKG
       exit
       ;;
