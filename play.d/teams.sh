0e8ed2bde878b41494ab66018b291eb942fc44c1
Vitaly Lipatov <lav@etersoft.ru>
2023-02-01 22:23:40 +0300

    epm play teams: fix for other rpm based distro

--- play.d/teams.sh
+++ play.d/teams.sh
@@ -6,9 +6,24 @@ DESCRIPTION="Microsoft Teams for Linux from the official site"
 
 . $(dirname $0)/common.sh
 
+repack=''
+pkgtype="$($DISTRVENDOR -p)"
+
+if [ "$pkgtype" = "deb" ] ; then
+    URL="https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams"
+    arch=amd64
+else
+    URL="https://packages.microsoft.com/yumrepos/ms-teams/Packages/t"
+    arch=x86_64
+    pkgtype=rpm
+fi
+
+if [ "$($DISTRVENDOR -s)" = "alt" ] ; then
+    repack="--repack"
+fi
 
 # rpm and deb contains the same binaries
-#https://packages.microsoft.com/yumrepos/ms-teams/teams-1.3.00.25560-1.x86_64.rpm
+# $ diff -ru teams-1.5.00.23861-1.x86_64 teams_1.5.00.23861_amd64
 
 # epm uses eget to download * names
-epm install "https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/$(epm print constructname teams "*" amd64 deb)"
+epm install $repack "$URL/$(epm print constructname teams "[0-9]*" $arch $pkgtype)"
