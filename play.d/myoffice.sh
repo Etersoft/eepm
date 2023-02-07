9f60786b14fee51562af50abae97d4d4db4ddc5b
Vitaly Lipatov <lav@etersoft.ru>
2023-02-08 02:24:26 +0300

    epm play myoffice: improve xdg-utils checking

--- play.d/myoffice.sh
+++ play.d/myoffice.sh
@@ -2,7 +2,7 @@
 
 PKGNAME=myoffice-standard-home-edition
 SUPPORTEDARCHES="x86_64"
-DESCRIPTION="MyOffice for Linux from the official site"
+DESCRIPTION="MyOffice Standart Home Edition for Linux from the official site"
 
 if [ "$1" = "--remove" ] ; then
     # Allow scripts: MyOffice reclaims their rpm package supports ALT
@@ -16,11 +16,12 @@ fi
 arch=$($DISTRVENDOR --distro-arch)
 pkgtype=$($DISTRVENDOR -p)
 
+# /var/lib/dpkg/info/myoffice-standard-home-edition.postinst: line 62: xdg-desktop-menu: command not found
+epm assure xdg-desktop-menu xdg-utils
+
 # https://preset.myoffice-app.ru/myoffice-standard-home-edition_2022.01-1.28.0.4_amd64
 PKGMASK="$(epm print constructname $PKGNAME "*" $arch)"
 PKG="$(epm tool eget --list --latest https://myoffice.ru/products/standard-home-edition/ $PKGMASK)" || fatal "Can't get package URL"
 
-# /var/lib/dpkg/info/myoffice-standard-home-edition.postinst: line 62: xdg-desktop-menu: command not found
-epm assure xdg-utils
 
 epm --scripts install "$PKG"
