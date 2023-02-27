43ce96ba4d563d9baebe113103b930d6fe89cf8f
Vitaly Lipatov <lav@etersoft.ru>
2023-02-27 23:15:00 +0300

    epm repack: add FlashPrint5 support

---
+++ repack.d/flashprint5.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+#PRODUCT=flashprint5
+#PRODUCTDIR=/usr/lib/$PRODUCT
+
+. $(dirname $0)/common.sh
+
+move_file /usr/lib/libOCCTWrapper.so.1 /usr/lib64/libOCCTWrapper.so.1
+
+epm install --skip-installed libGL libGLU libqt5-core libqt5-gui libqt5-network libqt5-opengl libqt5-widgets libudev1
+
