d6c36cfce1c429bce304a7ef8383973b08f13019
Vitaly Lipatov <lav@etersoft.ru>
2023-02-28 11:46:35 +0300

    epm repack synology-drive: fix install on ALT p10 (try 2)

--- repack.d/synology-drive.sh
+++ repack.d/synology-drive.sh
@@ -19,10 +19,10 @@ for i in lib/lib*.so.* package/cloudstation/lib/lib*.so.* ; do
     a= patchelf --set-rpath '$ORIGIN' $i
 done
 
-# TODO
-#for i in package/cloudstation/lib/plugins/lib*.so ; do
-#    a= patchelf --set-rpath '$ORIGIN' $i
-#done
+# /opt/Synology/SynologyDrive/package/cloudstation/lib/plugins/designer/libqquickwidget.so
+for i in package/cloudstation/lib/plugins/designer/lib*.so ; do
+    a= patchelf --set-rpath '$ORIGIN../../' $i
+done
 
 for i in bin/launcher package/cloudstation/bin/cloud-drive-* ; do
     a= patchelf --set-rpath '$ORIGIN/../lib' $i
@@ -32,16 +32,20 @@ done
 # ignore embedded libs
 filter_from_requires libQt5
 
-epm install --skip-installed coreutils glib2 libdbus libfontconfig1 libfreetype libgtk+3 libICE libpango libSM libX11 libxcb libxkbcommon libXrender
+epm install --skip-installed coreutils glib2 libdbus libgtk+3 libICE libpango libSM libX11 libxcb libxkbcommon libXrender
+# libfontconfig1 libfreetype 
 # Suggests
 # epm install --skip-installed libnautilus libnautilus-extension-compat
 filter_from_requires libnautilus
 
-# fix ALT p10 install
+remove_file /usr/lib/nautilus/extensions-4/libnautilus-drive-extension-4.so
+
+# fix ALT p10 install (TODO: restore for Sisyphus)
 #   synology-drive: Depends: libc.so.6(GLIBC_2.33)(64bit) but it is not installable
 #                  Depends: libc.so.6(GLIBC_2.34)(64bit) but it is not installable
 #                  Depends: libstdc++.so.6(GLIBCXX_3.4.29)(64bit) but it is not installable
 remove_file $PRODUCTDIR/package/cloudstation/icon-overlay/14/lib/plugin-cb-4.so
-
+# skip nautilus support
+remove_file $PRODUCTDIR/package/cloudstation/icon-overlay/14/lib/plugin-cb.so
 # TODO: parse
 # https://www.synology.com/api/support/findDownloadInfo?lang=ru-ru&product=DS2411%2B&major=6&minor=2
