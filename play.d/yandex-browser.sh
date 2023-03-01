eec3a11cba07dca3e825e66d9519b10387469003
Vitaly Lipatov <lav@etersoft.ru>
2023-03-01 18:51:18 +0300

    epm play yandex-browser: allow install native rpm for other rpm systems than ALT

--- play.d/yandex-browser.sh
+++ play.d/yandex-browser.sh
@@ -16,11 +16,11 @@ SUPPORTEDARCHES="x86_64"
 
 . $(dirname $0)/common.sh
 
-if epm installed yandex-browser-stable && [ "$(get_pkgvendor yandex-browser-stable)" = "YANDEX LLC" ] ; then
-    if [ "$(epm print field Vendor for package yandex-browser-stable)" = "Yandex Browser Team <browser@support.yandex.ru>" ] ; then
-        echo "Package yandex-browser-stable is already installed manually from https://browser.yandex.ru/."
+if epm installed $PKGNAME && [ "$(get_pkgvendor $PKGNAME)" = "YANDEX LLC" ] ; then
+    if [ "$(epm print field Vendor for package $PKGNAME)" = "Yandex Browser Team <browser@support.yandex.ru>" ] ; then
+        echo "Package $PKGNAME is already installed manually from https://browser.yandex.ru/."
     else
-        echo "Package yandex-browser-stable is already installed from ALT repository."
+        echo "Package $PKGNAME is already installed from ALT repository."
     fi
     exit 0
 fi
@@ -31,7 +31,12 @@ fi
 URL="https://repo.yandex.ru/yandex-browser"
 update_url_if_need_mirrored || update_url_if_need_mirrored https://download.etersoft.ru/pub/download/yandex-browser
 
-# epm uses eget to download * names
-epm install "$URL/deb/pool/main/y/$PKGNAME/$(epm print constructname $PKGNAME "*" amd64 deb)" || exit
+if [ "$(epm print info -s)" = "alt" ] || [ "$(epm print info -p)" != "rpm" ] ; then
+    # epm uses eget to download * names
+    epm install "$URL/deb/pool/main/y/$PKGNAME/$(epm print constructname $PKGNAME "*" amd64 deb)" || exit
+else
+    # https://repo.yandex.ru/yandex-browser/rpm/stable/x86_64/yandex-browser-stable-23.1.1.1114-1.x86_64.rpm
+    epm install "$URL/rpm/stable/x86_64/$(epm print constructname $PKGNAME "*" x86_64 rpm)" || exit
+fi
 
 epm play yandex-browser-codecs-ffmpeg-extra $BRANCH
