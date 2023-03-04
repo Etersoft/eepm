cbb25894b3f1a30eea0c2f7162f9f7e4103f38f9
Vitaly Lipatov <lav@etersoft.ru>
2023-03-04 11:04:57 +0300

    epm repack viber: rewrite

--- repack.d/viber.sh
+++ repack.d/viber.sh
@@ -3,12 +3,19 @@
 BUILDROOT="$1"
 SPEC="$2"
 
-mkdir -p $BUILDROOT/usr/bin/
-ln -s /opt/viber/Viber $BUILDROOT/usr/bin/viber
-ln -s /opt/viber/Viber $BUILDROOT/usr/bin/Viber
-subst 's|%files|%files\n/usr/bin/viber\n/usr/bin/Viber|' $SPEC
+PRODUCT=Viber
+PRODUCTCUR=viber
+PRODUCTDIR=/opt/viber
+
+. $(dirname $0)/common.sh
+
+add_bin_link_command
+add_bin_link_command $PRODUCTCUR $PRODUCT
 
 subst '1i%filter_from_requires /^libtiff.so.5(LIBTIFF_.*/d' $SPEC
 
-#REQUIRES="libtiff5"
-#subst "1iRequires:$REQUIRES" $SPEC
+subst "s|$PRODUCTDIR/$PRODUCT|$PRODUCTCUR|" $BUILDROOT/usr/share/applications/viber.desktop
+
+epm install --skip-installed glib2 gst-plugins-bad1.0 libalsa libbrotlidec libcups libdbus libdrm libEGL libexpat libfreetype libGL libGLX libgomp1 libgst-plugins1.0 libgstreamer1.0 libharfbuzz libICE libkrb5 liblcms2 libmng libmtdev libnspr libnss libOpenGL libopus libSM libsnappy libtiff5 libts0 libudev1 libwayland-client libwayland-cursor libwayland-egl libwayland-server libwebp7 libX11 libxcb libxcb-render-util libxcbutil-icccm libxcbutil-image libxcbutil-keysyms libXext libXfixes libxkbcommon libxkbcommon-x11 libxkbfile libxml2 libXrandr libXScrnSaver libxshmfence libxslt libXtst libzstd zlib
+# libfontconfig1 
+# seamonkey thunderbird 
