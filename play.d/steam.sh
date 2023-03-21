35bf691a7d4fd2940660485b88bffd17f68b0c20
Vitaly Lipatov <lav@etersoft.ru>
2023-03-21 15:29:57 +0300

    epm play steam: add install Steam from the repo

--- play.d/steam.sh
+++ play.d/steam.sh
@@ -2,8 +2,21 @@
 
 PKGNAME=steam-launcher
 SUPPORTEDARCHES="x86_64"
-DESCRIPTION=''
+DESCRIPTION='Steam Launcher (from the repository if the package is there, or from the official site)'
+
+[ "$(epm print info -s)" = "alt" ] && REPOPKGNAME=i586-steam || REPOPKGNAME=steam
+
+epm installed $REPOPKGNAME && PKGNAME=$REPOPKGNAME
 
 . $(dirname $0)/common.sh
 
-epm install "https://cdn.akamai.steamstatic.com/client/installer/steam.deb"
+res=0
+if ! epm install $REPOPKGNAME ; then
+    PKGURL="https://cdn.akamai.steamstatic.com/client/installer/steam.deb"
+    epm install $PKGURL
+    res=$?
+fi
+
+epm play i586-fix
+
+exit $res
