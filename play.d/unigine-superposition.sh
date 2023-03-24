0f6a00dc16e8ba63aeabb363e9aaa304f1e4180d
Vitaly Lipatov <lav@etersoft.ru>
2023-03-24 08:49:06 +0300

    epm play unigine-superposition: move unpacking to pack.d

--- play.d/unigine-superposition.sh
+++ play.d/unigine-superposition.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 
 PKGNAME=unigine-superposition
-OPKGNAME=Unigine_Superposition
 SUPPORTEDARCHES="x86_64"
 DESCRIPTION="Unigine Superposition 2017 (Unigine Benchmark) from the official site"
 
 . $(dirname $0)/common.sh
 
-convert_makeself_to_tar()
-{
-    offset=`head -n 402 "$1" | wc -c | tr -d " "`
-    dd if="$1" ibs=$offset skip=1 obs=1024 conv=sync | gzip -cd > "$(basename "$1" .run).tar"
-}
+cd_to_temp_dir
 
-PKGDIR=$(mktemp -d)
-trap "rm -fr $PKGDIR" EXIT
-
-cd $PKGDIR || fatal
 # https://assets.unigine.com/d/Unigine_superposition-4.0.run
-epm tool eget --latest https://benchmark.unigine.com/superposition "$OPKGNAME*.run"
+epm tool eget --latest https://benchmark.unigine.com/superposition "Unigine_Superposition*.run"
 
-mv $OPKGNAME*.run $(echo $OPKGNAME*.run | tr "[A-Z_]" "[a-z-]")
-
-convert_makeself_to_tar $PKGNAME*.run
-
-epm install $PKGNAME*.tar
+epm pack --install $PKGNAME *.run
