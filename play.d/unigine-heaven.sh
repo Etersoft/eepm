0a04c941cb4165c91c70860f4f6e044d2a13e98e
Vitaly Lipatov <lav@etersoft.ru>
2023-03-24 08:49:21 +0300

    epm play unigine-heaven: cleanup

--- play.d/unigine-heaven.sh
+++ play.d/unigine-heaven.sh
@@ -6,11 +6,9 @@ DESCRIPTION="Unigine Heaven 2009 (Unigine Benchmark) from the official site"
 
 . $(dirname $0)/common.sh
 
-PKGDIR=$(mktemp -d)
-trap "rm -fr $PKGDIR" EXIT
-cd $PKGDIR || fatal
+cd_to_temp_dir
 
 # https://assets.unigine.com/d/Unigine_Heaven-4.0.run
 epm tool eget --latest https://benchmark.unigine.com/heaven "Unigine_Heaven*.run"
 
-epm pack --install $PKGNAME Unigine_Heaven*.run
+epm pack --install $PKGNAME *.run
