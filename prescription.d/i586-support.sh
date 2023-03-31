332a9e164201a80a4e7f3b154b8d4c2fc9063de6
Vitaly Lipatov <lav@etersoft.ru>
2023-03-31 06:13:21 +0300

    epm prescription: add i586-support

---
+++ prescription.d/i586-support.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+[ "$1" != "--run" ] && echo "Add 32 bit support on 64 bit system" && exit
+
+[ "$(epm print info -a)" != "x86_64" ] && echo "Only x86_64 is supported" && exit 1
+
+
+vendor="$(epm print info -s)"
+
+case "$vendor" in
+    "alt")
+        if epm --quiet repo list | grep -q "x86_64-i586 classic" ; then
+            #[ -n "$verbose" ] && info "This system is ready to install 32bit packages"
+            exit 0
+        else
+            epm repo add "$(epm --quiet repo list | grep "x86_64 classic" | sed -e 's|x86_64 |x86_64-i586|')"
+            epm update
+        fi
+        ;;
+    "deb")
+        if a= dpkg --print-foreign-architectures | grep -q "i386" ; then
+            #[ -n "$verbose" ] && info "This system is ready to install 32bit packages"
+            exit 0
+        else
+            a= dpkg --add-architecture i386
+            epm update
+        fi
+        ;;
+    *)
+        info "Unsupported $(epm print info -e) system. Just skipping the operation."
+        exit
+        ;;
+esac
