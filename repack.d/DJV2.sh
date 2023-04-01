b311842ecce860ca8bdb009614064091ecedee5d
Vitaly Lipatov <lav@etersoft.ru>
2023-04-01 13:40:01 +0300

    epm repack DJV2: fix requires

--- repack.d/DJV2.sh
+++ repack.d/DJV2.sh
@@ -30,5 +30,12 @@ for i in $BUILDROOT$PRODUCTDIR/bin/{djv,djv_*} ; do
     a= patchelf --set-rpath '$ORIGIN/../lib' $i
 done
 
+for i in $BUILDROOT$PRODUCTDIR/lib/lib*.so* ; do
+    a= patchelf --set-rpath '$ORIGIN' $i
+done
+
+filter_from_requires libav libswresample libswscale
+
 # install all requires packages before packing (the list have got with rpmreqs package | xargs echo)
-#epm install --skip-installed at-spi2-atk coreutils findutils gawk glib2 libalsa libatk libat-spi2-core libcairo libdbus libdrm libexpat libgbm libgdk-pixbuf libgio libgtk+3 libnspr libnss libpango libsecret libX11 libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbfile libXrandr libXrender libXScrnSaver libXtst sed
+epm install --skip-installed libalsa libGLX libOpenGL libX11 libxcb libXext zlib
+
