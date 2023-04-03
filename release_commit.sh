aeca4a9b562e7adc0a774e173f6f0f27835bff6d
Vitaly Lipatov <lav@etersoft.ru>
2023-04-03 21:54:10 +0300

    improve release_commit.sh

--- release_commit.sh
+++ release_commit.sh
@@ -4,6 +4,19 @@
 . /usr/share/eterbuild/eterbuild
 load_mod spec etersoft
 
+push_tag()
+{
+    local version="$1"
+    git tag -a "$version" -e -s -m "$(rpmlog -q --last-changelog | sed -e 's|^* ||')"
+    git push pub.github $version
+}
+
+if [ -n "$1" ] ; then
+    [ "$(git tag -l "$1")" = "$1" ] || fatal "Can't find tag '$1' in the repo."
+    push_tag $1
+    exit
+fi
+
 SPECNAME=eepm.spec
 version="$(get_version $SPECNAME)"
 
@@ -11,8 +24,8 @@ version="$(get_version $SPECNAME)"
 
 git add packed
 git commit packed -m "commit packed $version"
-git tag -a "$version" -e -s -m "$(rpmlog -q --last-changelog | sed -e 's|^* ||')"
-git push pub.github $version
+
+push_tag $version
 gpush pub.github
 
 rpmpub /var/ftp/pub/Korinf/sources
