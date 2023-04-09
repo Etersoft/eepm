3b75196c5e43f5ddeb8a1428c9e69c3080ee1c55
Vitaly Lipatov <lav@etersoft.ru>
2023-04-10 02:13:35 +0300

    epm play sweethome3d: support for latest version, rewrite repack

--- play.d/sweethome3d.sh
+++ play.d/sweethome3d.sh
@@ -21,7 +21,11 @@ esac
 
 # TODO: get url from https://sourceforge.net/projects/sweethome3d/best_release.json (is it client system dependend??)
 # see get_github_urls in eget
-VERSION=6.6
-PKG="http://download.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-$VERSION/SweetHome3D-$VERSION-linux-$arch.tgz"
+#VERSION=6.6
+#PKGURL="http://download.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-$VERSION/SweetHome3D-$VERSION-linux-$arch.tgz"
 
-epm --repack install "$PKG"
+# http://sourceforge.net/projects/sweethome3d/files/SweetHome3D/SweetHome3D-7.1/SweetHome3D-7.1-linux-x86.tgz/download
+URL="$(eget -4 --list --latest https://www.sweethome3d.com/download.jsp SweetHome3D-.*-linux-$arch.tgz/download)"
+PKGURL="$(echo "$URL" | sed -e "s|http://sourceforge.net/projects/sweethome3d/files|http://download.sourceforge.net/project/sweethome3d|" -e 's|/download$||' )"
+
+epm install "$PKGURL"
--- repack.d/SweetHome3D.sh
+++ repack.d/SweetHome3D.sh
@@ -5,6 +5,11 @@ BUILDROOT="$1"
 SPEC="$2"
 
 PRODUCT=SweetHome3D
+PRODUCTCUR=sweethome3d
+PRODUCTDIR=/opt/$PRODUCT
+
+. $(dirname $0)/common.sh
+
 
 subst '1iConflicts:sweethome3d' $SPEC
 
@@ -12,11 +17,11 @@ subst "s|^License: unknown$|License: GPLv2|" $SPEC
 subst "s|^URL:.*|URL: http://www.sweethome3d.com|" $SPEC
 subst "s|^Summary:.*|Summary: An interior design application to draw house plans & arrange furniture|" $SPEC
 
-# move package to /opt
 ROOTDIR=$(basename $(find $BUILDROOT -mindepth 1 -maxdepth 1 -type d))
-mkdir $BUILDROOT/opt
-mv $BUILDROOT/$ROOTDIR $BUILDROOT/opt/$PRODUCT
-subst "s|\"/$ROOTDIR/|\"/opt/$PRODUCT/|" $SPEC
+move_to_opt /$ROOTDIR
+
+add_bin_exec_command
+add_bin_link_command $PRODUCTCUR $PRODUCT
 
 # create desktop file
 mkdir -p $BUILDROOT/usr/share/applications/
@@ -34,8 +39,8 @@ Comment=Design Application
 Comment[fr]=Application de conception d'intérieur en 3D
 Comment[pt]=Aplicativo de design de interiores
 Comment[ru]=Программа проектирования домашнего интерьера в 3D
-Exec=/opt/$PRODUCT/SweetHome3D
-Icon=sweethome3d
+Exec=$PRODUCT
+Icon=$PRODUCT
 Terminal=false
 Type=Application
 StartupNotify=true
@@ -43,4 +48,30 @@ StartupWMClass=com-eteks-sweethome3d-SweetHome3D
 Categories=Graphics;2DGraphics;3DGraphics;
 MimeType=application/vnd.sh3d;
 EOF
-subst "s|%files|%files\n/usr/share/applications/$PRODUCT.desktop|" $SPEC
+pack_file /usr/share/applications/$PRODUCT.desktop
+
+install_file $PRODUCTDIR/SweetHome3DIcon.png /usr/share/pixmaps/$PRODUCT.png
+
+cd $BUILDROOT$PRODUCTDIR/ || fatal
+epm assure patchelf || exit
+for i in runtime/lib/amd64/lib*.so ; do
+    a= patchelf --set-rpath '$ORIGIN:$ORIGIN/server:$ORIGIN/jli' $i
+done
+
+for i in lib/lib*.so ; do
+    [ -r "$i" ] || continue
+    a= patchelf --set-rpath '$ORIGIN/../runtime/lib/amd64' $i
+done
+
+for i in lib/java3d-1.6/lib*.so lib/yafaray/lib*.so  ; do
+    [ -r "$i" ] || continue
+    a= patchelf --set-rpath '$ORIGIN:$ORIGIN/../../runtime/lib/amd64' $i
+done
+
+chmod -v a+x runtime/bin/*
+
+add_findreq_skiplist "$PRODUCTDIR/runtime/lib/amd64/libav*.so"
+
+subst '1iAutoProv:no' $SPEC
+
+epm install --skip-installed coreutils glib2 libalsa libatk libcairo libcairo-gobject fontconfig libfreetype libgdk-pixbuf libgio libGL libgtk+2 libgtk+3 libnsl1 libpango libX11 libXext libXi libXrender libXtst
