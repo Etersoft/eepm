d2a5bfbbbd1ae4deeabd8c5b8ecbd84fe057e773
Vitaly Lipatov <lav@etersoft.ru>
2023-04-12 03:20:38 +0300

    epm play: fix unigine-valley url

--- play.d/unigine-valley.sh
+++ play.d/unigine-valley.sh
@@ -7,6 +7,6 @@ DESCRIPTION="Unigine Valley 2013 (Unigine Benchmark) from the official site"
 . $(dirname $0)/common.sh
 
 # https://assets.unigine.com/d/Unigine_valley-4.0.run
-PKGURL=$(eget --latest https://benchmark.unigine.com/valley "Unigine_Valley*.run")
+PKGURL=$(eget --list --latest https://benchmark.unigine.com/valley "Unigine_Valley*.run")
 
 epm pack --install $PKGNAME $PKGURL
