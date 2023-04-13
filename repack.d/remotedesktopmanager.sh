c40b5a3f039a75d42ac7b0c8d04620b05f82db6a
Vitaly Lipatov <lav@etersoft.ru>
2023-04-13 23:53:06 +0300

    epm repack: add support for remotedesktopmanager

---
+++ repack.d/remotedesktopmanager.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+. $(dirname $0)/common-chromium-browser.sh
+
+PRODUCT=remotedesktopmanager
+PRODUCTDIR=/usr/lib/devolutions/RemoteDesktopManager
+
+add_findreq_skiplist "$PRODUCTDIR/runtimes/*"
