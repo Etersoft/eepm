f47acd7089661cf392b4a520c2133d90f494ee87
Vitaly Lipatov <lav@etersoft.ru>
2023-04-19 08:15:55 +0300

    epm play cascadeur: get build-id and version from the site

--- play.d/cascadeur.sh
+++ play.d/cascadeur.sh
@@ -6,5 +6,9 @@ DESCRIPTION="Cascadeur - a physics‑based 3D animation software"
 
 . $(dirname $0)/common.sh
 
+page="$(eget -O- https://cascadeur.com/ru/download)"
+BUILDID=$(echo "$page" | grep 'data-platform="linux"' | grep 'data-build-id=' | sed -e 's|.*data-build-id="\(.*\)" data-modal.*|\1|') #"
+VERSION=$(echo "$page" | grep 'main-download__info-version' | sed -e 's|.*<div class="main-download__info-version">\(.*\)</div>.*|\1|') #"
+
 # TODO: ask license, get version
-epm pack --install $PKGNAME "https://cdn.cascadeur.com/builds/linux/59/cascadeur-linux.tgz" "2022.3.1"
+epm pack --install $PKGNAME "https://cdn.cascadeur.com/builds/linux/$BUILDID/cascadeur-linux.tgz" "$VERSION"
