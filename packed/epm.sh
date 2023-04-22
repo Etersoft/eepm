aa8146e5bd1acf6da59058c26f789c9c10d2521a
Vitaly Lipatov <lav@etersoft.ru>
2023-04-23 01:12:02 +0300

    commit packed 3.52.6

--- packed/epm.sh
+++ packed/epm.sh
@@ -33,7 +33,7 @@ SHAREDIR=$PROGDIR
 # will replaced with /etc/eepm during install
 CONFIGDIR=$PROGDIR/../etc
 
-EPMVERSION="3.52.5"
+EPMVERSION="3.52.6"
 
 # package, single (file), pipe, git
 EPMMODE="package"
