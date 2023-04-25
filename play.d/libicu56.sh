2e8b57d21a3b4d215da4e29c07c929a6e323b134
Vitaly Lipatov <lav@etersoft.ru>
2023-04-25 22:51:22 +0300

    epm play: add libicu56 (for ALT only)

---
+++ play.d/libicu56.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PKGNAME=libicu56
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION=""
+
+. $(dirname $0)/common.sh
+
+if [ "$(epm print info -s)" = "alt" ] ; then
+    PKGURL="http://ftp.basealt.ru/pub/distributions/archive/p8/date/2018/01/04/x86_64/RPMS.classic/libicu56-5.6.1-alt1.1.x86_64.rpm"
+    epm install $PKGURL
+else
+    epm install libicu56
+fi
+
