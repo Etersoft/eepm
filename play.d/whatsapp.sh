ea73c7be014b2744c9863a36c9636a7d905e497b
Vitaly Lipatov <lav@etersoft.ru>
2023-04-28 01:49:59 +0300

    epm play: enable whatsapp support for other distro

--- play.d/whatsapp.sh
+++ play.d/whatsapp.sh
@@ -3,10 +3,15 @@
 PKGNAME=whatsapp-for-linux
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
-DESCRIPTION='' #"An unofficial WhatsApp desktop application for Linux"
+DESCRIPTION='An unofficial WhatsApp desktop application (from the repository if the package is there, or from the official site)'
 
 . $(dirname $0)/common.sh
 
+if epm install $PKGNAME ; then
+    return
+fi
+
+[ "$(epm print info -s)" = "alt" ] && fatal "ALT is not supports $PKGNAME AppImage for now."
 
 #case "$(epm print info -d)" in
 #    ALTLinux|ALTServer)
@@ -14,6 +19,7 @@ DESCRIPTION='' #"An unofficial WhatsApp desktop application for Linux"
 #        ;;
 #esac
 arch=x86_64
+# sh: symbol lookup error: /tmp/.private/lav/.mount_whatsaxhRMDh/opt/libc/lib/x86_64-linux-gnu/libc.so.6: undefined symbol: __libc_enable_secure, version GLIBC_PRIVATE
 URL=$(epm tool eget --list --latest https://github.com/eneshecan/whatsapp-for-linux/releases "$PKGNAME-$VERSION-$arch.AppImage") || fatal "Can't get package URL"
 epm install $URL
 
