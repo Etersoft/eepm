adb088c976fd940ecff5939f4fc911e93465e485
Vitaly Lipatov <lav@etersoft.ru>
2023-05-01 01:03:03 +0300

    epm repack trueconf-server: improve preinstall packages

--- repack.d/trueconf-server.sh
+++ repack.d/trueconf-server.sh
@@ -3,6 +3,10 @@
 BUILDROOT="$1"
 SPEC="$2"
 
+PREINSTALL_PACKAGES="coreutils gawk libapr1 libaprutil1 libcares libcrypt libcrypto1.1 libcurl liblame libldap libncurses libnghttp2 libnsl1 libpcre3 libpng16 libpq5 libreadline7 libspeex libssl1.1 libtinfo libxml2 systemd-utils zlib"
+
+. $(dirname $0)/common.sh
+
 #  trueconf-server: Требует: libcurl.so.4(CURL_OPENSSL_4)(64bit) но пакет не может быть установлен
 #                   Требует: liblber-2.4.so.2(OPENLDAP_2.4_2)(64bit) но пакет не может быть установлен
 #                   Требует: libldap_r-2.4.so.2(OPENLDAP_2.4_2)(64bit) но пакет не может быть установлен
@@ -14,7 +18,3 @@ subst '1i%filter_from_requires /^libldap_r-2.4.so.2(OPENLDAP_.*/d' $SPEC
 #REQUIRES="libcurl libldap"
 #subst "1iRequires:$REQUIRES|" $SPEC
 
-if [ "$(epm print info -s)" = "alt" ] ; then
-    # install all requires packages before packing (the list have got with rpmreqs anydesk)
-    epm install --skip-installed coreutils gawk libapr1 libaprutil1 libcares libcrypt libcrypto1.1 libcurl liblame libldap libncurses libnghttp2 libnsl1 libpcre3 libpng16 libpq5 libreadline7 libspeex libssl1.1 libtinfo libxml2 mozldap systemd-utils veyon zlib
-fi
