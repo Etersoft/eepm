8fbb51f39cf52783c58ab9cc382c5d1e2840c498
Vitaly Lipatov <lav@etersoft.ru>
2023-05-02 03:26:33 +0300

    epm play: add joplin

---
+++ play.d/joplin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PKGNAME=Joplin
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Joplin - an open source note taking and to-do application with synchronisation capabilities"
+URL="https://joplinapp.org/"
+
+. $(dirname $0)/common.sh
+
+PKGURL="$(eget --list --latest https://github.com/laurent22/joplin/releases/ "Joplin-$VERSION.AppImage")"
+
+epm install $PKGURL
