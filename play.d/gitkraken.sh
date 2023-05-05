847936638508bbf19e8bf0c92918f8df1e95245d
Andrei Tropichev <52948263+ATropichev@users.noreply.github.com>
2023-05-05 21:51:17 +1000

    Create gitkraken.sh

---
+++ play.d/gitkraken.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PKGNAME=gitkraken
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="GitKraken Client from the official site"
+URL="https://www.gitkraken.com/"
+
+. $(dirname $0)/common.sh
+
+pkgtype=$(epm print info -p)
+case $pkgtype in
+    rpm)
+        PKGURL="https://release.gitkraken.com/linux/gitkraken-amd64.rpm"
+        ;;
+    *)
+        PKGURL="https://release.gitkraken.com/linux/gitkraken-amd64.deb"
+        ;;
+esac
+
+if [ "$(epm print info -s)" = "alt" ] ; then
+    repack="--repack"
+fi
+
+epm install $repack "$PKGURL"
