49a0351ec765d962fbb5055b94c452eb3dbb6218
Vitaly Lipatov <lav@etersoft.ru>
2023-05-05 15:11:42 +0300

    epm play: add r7-office-organizer

---
+++ play.d/r7-office-organizer.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+PKGNAME=r7-office-organizer
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="R7 Office Organizer for Linux from the official site"
+URL="https://support.r7-office.ru/category/organizer/install_organizer/"
+
+. $(dirname $0)/common.sh
+
+if [ "$VERSION" = "*" ] ; then
+    VERSION="68.5.0"
+fi
+
+# hack with release part
+[ "$VERSION" = "68.5.0" ] && VERSION="$VERSION-5"
+
+case $(epm print info -p) in
+    rpm)
+        PKGURL="https://download.r7-office.ru/centos/r7-office-organizer-$VERSION-centos.ru.x86_64.rpm"
+        ;;
+    *)
+        PKGURL="https://download.r7-office.ru/ubuntu/r7-office-organizer-$VERSION.ru.x86_64.deb"
+        ;;
+esac
+
+case $(epm print info -e) in
+    ROSA/2021.1)
+        PKGURL="https://download.r7-office.ru/rosa/r7-office-organizer-$VERSION-rosa.ru.x86_64.rpm"
+        ;;
+    ALTLinux/*|ALTServer/*)
+        PKGURL="https://download.r7-office.ru/altlinux/r7-office-organizer-$VERSION-altlinux.ru.x86_64.rpm"
+        ;;
+esac
+
+# install with scripts (need for install icons and associations)
+# see /etc/eepm/pkgallowscripts.list
+# TODO: pack it into the package
+epm install "$PKGURL"
