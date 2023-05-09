e9a59af74746edcc34c2f2fdec7b7290d854a0d2
Vitaly Lipatov <lav@etersoft.ru>
2023-05-09 05:11:40 +0300

    epm play zoom: rewrite with VERSION using

--- play.d/zoom.sh
+++ play.d/zoom.sh
@@ -2,28 +2,27 @@
 
 PKGNAME=zoom
 SUPPORTEDARCHES="x86_64 x86"
+VERSION="$2"
 DESCRIPTION="Zoom client from the official site"
 
 . $(dirname $0)/common.sh
 
+[ "$VERSION" = "*" ] && VERSION="latest"
 
 arch=$(epm print info --distro-arch)
 case $arch in
     x86_64|amd64)
-        version=latest
         arch=$arch ;;
     i586|i386)
         # latest does not work
-        version=5.4.53391.1108
+        VERSION=5.4.53391.1108
         arch=i686 ;;
     *)
         fatal "Unsupported arch $arch for $(epm print info -d)"
 esac
 
-if [ "$(epm print info -s)" = "alt" ] ; then
-    epm install --repack https://zoom.us/client/$version/zoom_$arch.rpm
-    exit
-fi
+repack=''
+[ "$(epm print info -s)" = "alt" ] && repack="--repack"
 
 # TODO: there are more complex distro dependent url
-epm install "https://zoom.us/client/latest/zoom_$arch.$(epm print info -p)"
+epm install $repack "https://zoom.us/client/$VERSION/zoom_$arch.$(epm print info -p)"
