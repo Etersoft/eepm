5fa5c5c1258e3f8853ad508601776242c87989ee
Vitaly Lipatov <lav@etersoft.ru>
2023-05-13 16:35:22 +0300

    epm pack.d: initial implement of generic-appimage.sh

---
+++ pack.d/generic-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+TAR="$1"
+RETURNTARNAME="$2"
+VERSION="$3"
+
+. $(dirname $0)/common.sh
+
+if ! rhas "$TAR" "\.AppImage$" ; then
+    fatal "No idea how to handle $TAR"
+fi
+
+alpkg=$(basename $TAR)
+
+if [ -n "$VERSION" ] ; then
+    PRODUCT="$(basename $alpkg .AppImage)"
+else
+    # AppImage version
+    # hack for ktalk2.4.2 -> ktalk 2.4.2
+    VERSION="$(echo "$alpkg" | grep -o -P "[-_.a-zA-Z]([0-9])([0-9])*([.]*[0-9])*" | head -n1 | sed -e 's|^[-_.a-zA-Z]||' -e 's|--|-|g' )"  #"
+    [ -n "$VERSION" ] && PRODUCT="$(echo "$alpkg" | sed -e "s|[-_.]$VERSION.*||")" || fatal "Can't get version from $TAR."
+fi
+
+PKGNAME=$PRODUCT-$VERSION.tar
+
+[ -x "$TAR" ] || chmod u+x $verbose "$TAR"
+$TAR --appimage-extract || fatal
+erc pack $PKGNAME squashfs-root
+
+return_tar $PKGNAME
