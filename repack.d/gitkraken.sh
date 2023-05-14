897fe7554ed9e929058b7cc02141a4b5770d39e7
Vitaly Lipatov <lav@etersoft.ru>
2023-05-14 13:41:58 +0300

    epm repack gitkraken: disable perl requires (todo: remove embedded git?)

--- repack.d/gitkraken.sh
+++ repack.d/gitkraken.sh
@@ -24,3 +24,7 @@ add_findreq_skiplist "$PRODUCTDIR/resources/app.asar.unpacked/node_modules/@axos
 add_findreq_skiplist "$PRODUCTDIR/resources/app.asar.unpacked/node_modules/@msgpackr-extract/msgpackr-extract-linux-x64/*.node"
 
 add_requires libXScrnSaver
+
+# TODO: replace gitkraken/resources/app.asar.unpacked/git with system's one optionally?
+subst '1iAutoReq:yes,noperl' $SPEC
+subst '1iAutoProv:no' $SPEC
