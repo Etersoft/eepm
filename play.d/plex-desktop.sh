112caf312ab6c62e3d01cfc23796db2080fe8609
Vitaly Lipatov <lav@etersoft.ru>
2023-05-14 05:15:12 +0300

    epm play: add plex-desktop

---
+++ play.d/plex-desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PKGNAME=plex-desktop
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Plex Desktop from the official site"
+
+. $(dirname $0)/common.sh
+
+# https://api.snapcraft.io/api/v1/snaps/download/qc6MFRM433ZhI1XjVzErdHivhSOhlpf0_37.snap
+PKGURL="$(eget -O- -H Snap-Device-Series:16 https://api.snapcraft.io/v2/snaps/info/plex-desktop | epm --inscript tool json -b | grep '\["channel-map",0,"download","url"\]' | head -n1 | sed -e 's|.*"\(.*\)"$|\1|' )" || fatal "Can't get URL"
+
+epm install "$PKGURL"
---
+++ repack.d/plex-desktop.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+
+SPEC="$2"
+
+. $(dirname $0)/common.sh
+
+add_requires glib2 glxinfo libalsa libdbus libdrm libEGL libexpat fontconfig libfreetype libgbm libGLX libharfbuzz libjpeg8 liblcms2 libminizip libnspr libnss libOpenGL libopus libpci libpulseaudio libsnappy libtiff5 libudev1 libva libwayland-client libwayland-cursor libwayland-egl libwayland-server libX11 libxcb libxcb-render-util libxcbutil-icccm libxcbutil-image libxcbutil-keysyms libXcomposite libXdamage libXext libXfixes libXinerama libxkbcommon libxkbcommon-x11 libxkbfile libxml2 libXrandr libXrender libXScrnSaver libxshmfence libxslt libXtst which zlib
+
+subst '1iAutoReq:no' $SPEC
+subst '1iAutoProv:no' $SPEC
+
+add_bin_link_command $PRODUCT $PRODUCTDIR/Plex.sh
+
+cd .$PRODUCTDIR || fatal
+
+for i in usr/lib/x86_64-linux-gnu/libwebp* ; do
+    move_file $PRODUCTDIR/$i $PRODUCTDIR/lib/$(basename $i)
+done
+
+for i in etc meta snap lib/dri usr/bin usr/lib usr/include usr/share/X11 usr/share/misc usr/share/doc usr/share/fonts ; do
+    remove_dir $PRODUCTDIR/$i
+done
+
+for i in libEGL.so.1 libdrm.so.2 libdrm_amdgpu.so.1 libva-drm.so.2 libva-x11.so.2 libva.so.2 ; do
+    remove_file $PRODUCTDIR/lib/$i
+done
+
+if epm assure patchelf ; then
+for i in bin/Plex "bin/Plex Transcoder" ; do
+    a= patchelf --set-rpath '$ORIGIN/../lib' "$i" || continue
+done
+for i in lib/lib*.so* ; do
+    a= patchelf --set-rpath '$ORIGIN' "$i" || continue
+done
+fi
+
+subst '1i%filter_from_requires /^libtiff.so.5(LIBTIFF_.*/d' $SPEC
+
+exit 0
