94f1e3bdf2bdd1238868ce07bab77e871e2c01b4
Vitaly Lipatov <lav@etersoft.ru>
2023-05-23 02:43:16 +0300

    epm play cascadeur: revert to 2022.3.1 version

--- play.d/cascadeur.sh
+++ play.d/cascadeur.sh
@@ -3,12 +3,24 @@
 PKGNAME=cascadeur
 SUPPORTEDARCHES="x86_64"
 DESCRIPTION="Cascadeur - a physics‑based 3D animation software"
+URL="https://cascadeur.com/download"
 
 . $(dirname $0)/common.sh
 
+# keep worked version
+VERSION="2022.3.1"
+PKGURL="ipfs://Qma8WF8iPwgKNPM6UdZHWse4q1cTnPAvjMRkGsbbWYi18w?filename=cascadeur-linux.tgz"
+# TODO: ask license
+epm pack --install $PKGNAME "$PKGURL" "$VERSION"
+exit
+
+# (liblapack.so.3)
 page="$(eget -O- https://cascadeur.com/ru/download)"
 BUILDID=$(echo "$page" | grep 'data-platform="linux"' | grep 'data-build-id=' | sed -e 's|.*data-build-id="\(.*\)" data-modal.*|\1|') #"
-VERSION=$(echo "$page" | grep 'main-download__info-version' | sed -e 's|.*<div class="main-download__info-version">\(.*\)</div>.*|\1|') #"
+VERSION=$(echo "$page" | grep 'main-download__info-version' | sed -e 's|.*<div class="main-download__info-version">\(.*\)</div>.*|\1|') #'
+# https://cdn.cascadeur.com/builds/linux/62/cascadeur-linux_2023.1.tgz
+PKGURL="https://cdn.cascadeur.com/builds/linux/$BUILDID/cascadeur-linux_$VERSION.tgz"
 
-# TODO: ask license, get version
-epm pack --install $PKGNAME "https://cdn.cascadeur.com/builds/linux/$BUILDID/cascadeur-linux.tgz" "$VERSION"
+# TODO: ask license
+epm pack --install $PKGNAME "$PKGURL" "$VERSION"
+
