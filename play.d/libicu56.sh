c5b1a151d1ee95ec4b123ab1606dfa99cadc883d
Vitaly Lipatov <lav@etersoft.ru>
2023-07-02 22:07:56 +0300

    epm play libicu56: use http (ALT bug 46508)

--- play.d/libicu56.sh
+++ play.d/libicu56.sh
@@ -8,7 +8,9 @@ DESCRIPTION=""
 . $(dirname $0)/common.sh
 
 if [ "$(epm print info -s)" = "alt" ] ; then
-    PKGURL="https://ftp.basealt.ru/pub/distributions/archive/p8/date/2018/01/04/x86_64/RPMS.classic/libicu56-5.6.1-alt1.1.x86_64.rpm"
+    # https://bugzilla.altlinux.org/46508
+    #PKGURL="https://ftp.basealt.ru/pub/distributions/archive/p8/date/2018/01/04/x86_64/RPMS.classic/libicu56-5.6.1-alt1.1.x86_64.rpm"
+    PKGURL="http://ftp.basealt.ru/pub/distributions/archive/p8/date/2018/01/04/x86_64/RPMS.classic/libicu56-5.6.1-alt1.1.x86_64.rpm"
     epm install $PKGURL
 else
     epm install libicu56
