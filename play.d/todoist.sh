d142ce102c2509acbbd8f62852881d76290993aa
Vitaly Lipatov <lav@etersoft.ru>
2023-07-04 01:41:04 +0300

    epm play todoist: fix package name

--- play.d/todoist.sh
+++ play.d/todoist.sh
@@ -6,6 +6,12 @@ DESCRIPTION='Todoist client application from the official site'
 
 . $(dirname $0)/common.sh
 
+PKGURL="https://todoist.com/linux_app/appimage"
 
-epm install "https://todoist.com/linux_app/appimage"
+# TODO: rename AppImages on the fly (in pack.d/generic-appimage.sh)
+cd_to_temp_dir
+epm tool eget "$PKGURL" || fatal
+newname="$(echo *.AppImage | sed -e "s|^Todoist-linux-x86_64-|$PKGNAME-|" )"
+mv -v *.AppImage $newname || exit
 
+epm install $newname
