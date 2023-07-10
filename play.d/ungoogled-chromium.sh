b2eaf6e685ad58cf79e7ad723da765934a28032c
Vitaly Lipatov <lav@etersoft.ru>
2023-07-11 01:56:34 +0300

    epm play ungoogled-chromium: keep old version due libc.so.6(GLIBC_2.33)

--- play.d/ungoogled-chromium.sh
+++ play.d/ungoogled-chromium.sh
@@ -7,6 +7,13 @@ DESCRIPTION="Google Chromium, sans integration with Google from the official sit
 
 . $(dirname $0)/common.sh
 
+# keep old version due libc.so.6(GLIBC_2.33)(64bit)
+if [ "$VERSION" = "*" ] && [ "$(epm print info -s)" = "alt" ] && [ "$(epm print info -r)" = "p10" ] ; then
+    VERSION="113.0.5672.127"
+fi
+
+[ "$VERSION" != "*" ] && VERSION="$VERSION-1.1"
+
 PKG=$(epm tool eget --list --latest https://github.com/clickot/ungoogled-chromium-binaries/releases ungoogled-chromium_${VERSION}_linux.tar.xz) || fatal "Can't get package URL"
 
 epm install "$PKG"
