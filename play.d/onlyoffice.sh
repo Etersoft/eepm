bd2be4c6f1a9f6d37160dde17699fae51d0c0290
Vitaly Lipatov <lav@etersoft.ru>
2023-07-14 00:56:34 +0300

    epm play onlyoffice: all get any version from github releases

--- play.d/onlyoffice.sh
+++ play.d/onlyoffice.sh
@@ -2,15 +2,40 @@
 
 PKGNAME=onlyoffice-desktopeditors
 SUPPORTEDARCHES="x86_64"
+VERSION="$2"
 DESCRIPTION="ONLYOFFICE for Linux from the official site"
 
 . $(dirname $0)/common.sh
 
-#arch=$(epm print info --distro-arch)
-arch=amd64
-#pkgtype=$(epm print info -p)
-pkgtype=deb
+arch=$(epm print info --distro-arch)
+case "$(epm print info -p)" in
+  rpm)
+      file=onlyoffice-desktopeditors.x86_64.rpm
+      pkgtype=rpm
+      ;;
+  *)
+      file=onlyoffice-desktopeditors_amd64.deb
+      pkgtype=deb
+      ;;
+esac
 
-PKG="https://download.onlyoffice.com/install/desktop/editors/linux/$(epm print constructname $PKGNAME "" $arch deb)"
+case "$(epm print info -s)" in
+  alt)
+      arch=amd64
+      file=onlyoffice-desktopeditors_amd64.deb
+      pkgtype=deb
+      ;;
+esac
 
-epm install "$PKG"
+if [ "$VERSION" = "*" ] ; then
+    PKGURL="$(eget --list --latest https://github.com/ONLYOFFICE/DesktopEditors/releases $file)"
+else
+    PKGURL="https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v$VERSION/$file"
+fi
+
+#https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.4.0/onlyoffice-desktopeditors.x86_64.rpm
+#https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.4.0/onlyoffice-desktopeditors_amd64.deb
+#PKGURL="https://download.onlyoffice.com/install/desktop/editors/linux/"
+#PKGURL="$(eget --list --latest https://github.com/ONLYOFFICE/DesktopEditors/releases $(epm print constructname $PKGNAME "$VERSION" $arch $pkgtype))"
+
+epm install "$PKGURL"
