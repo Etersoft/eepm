4097011d71d3aca25169ba8e71e3c93c9e0e5d80
Vitaly Lipatov <lav@etersoft.ru>
2023-07-15 02:14:10 +0300

    epm repack anydesk: switch to UNIREQUIRES

--- repack.d/anydesk.sh
+++ repack.d/anydesk.sh
@@ -7,7 +7,13 @@ SPEC="$2"
 PRODUCT=anydesk
 
 # install all requires packages before packing (the list have got with rpmreqs anydesk)
-PREINSTALL_PACKAGES="fontconfig glib2 libatk libcairo libfreetype libgdk-pixbuf libgio libGL libGLU libgtk+2 libICE libpango libpolkit libSM libX11 libxcb libXdamage libXext libXfixes libXi libxkbfile libXmu libXrandr libXrender libXt libXtst polkit libminizip libgtkglext libpangox-1.0.so.0()(64bit)"
+#PREINSTALL_PACKAGES="fontconfig glib2 libatk libcairo libfreetype libgdk-pixbuf libgio libGL libGLU libgtk+2 libICE libpango libpolkit libSM libX11 libxcb libXdamage libXext libXfixes libXi libxkbfile libXmu libXrandr libXrender libXt libXtst polkit libminizip libgtkglext libpangox-1.0.so.0"
+UNIREQUIRES="libGL.so.1 libGLU.so.1 libX11-xcb.so.1 libX11.so.6 libXdamage.so.1 libXext.so.6 libXfixes.so.3 libXi.so.6 libXrandr.so.2 libXrender.so.1 libXtst.so.6 libatk-1.0.so.0
+libcairo.so.2 libfontconfig.so.1 libfreetype.so.6
+libgdk-x11-2.0.so.0 libgdk_pixbuf-2.0.so.0 libgio-2.0.so.0 libglib-2.0.so.0 libgobject-2.0.so.0 libgtk-x11-2.0.so.0 libpango-1.0.so.0 libpangocairo-1.0.so.0 libpangoft2-1.0.so.0 libpolkit-gobject-1.so.0
+libstdc++.so.6
+libxcb-shm.so.0 libxcb.so.1 libxkbfile.so.1
+polkit"
 
 . $(dirname $0)/common.sh
 
@@ -20,8 +26,6 @@ PREINSTALL_PACKAGES="fontconfig glib2 libatk libcairo libfreetype libgdk-pixbuf 
 #subst "s|.*/etc/default/NetworkManager.*||" $BUILDROOT/etc/rc.d/init.d/anydesk
 #subst "s|/usr/share/anydesk/files/init/anydesk|/etc/rc.d/init.d/anydesk|" $SPEC
 
-set_autoreq 'yes'
-
 remove_file /usr/share/anydesk/files/init/anydesk
 
 # put service file to the normal place
@@ -30,6 +34,10 @@ cp $BUILDROOT/usr/share/anydesk/files/systemd/anydesk.service $BUILDROOT/etc/sys
 remove_file /usr/share/anydesk/files/systemd/anydesk.service
 pack_file /etc/systemd/system/anydesk.service
 
+fix_desktop_file /usr/bin/$PRODUCT
+
+# autoreq is disabled, skip elf patching due requires
+exit
 
 LIBDIR=/usr/lib64
 [ -d $BUILDROOT$LIBDIR ] || LIBDIR=/usr/lib
@@ -51,6 +59,5 @@ fi
 # preloaded from /usr/lib64/anydesk/, drop external requires
 filter_from_requires libpangox-1.0.so.0 libgdkglext-x11-1.0.so.0 libgtkglext-x11-1.0.so.0
 
-fix_desktop_file /usr/bin/$PRODUCT
-
-add_requires $PREINSTALL_PACKAGES
+#add_requires $PREINSTALL_PACKAGES
+#set_autoreq 'yes'
