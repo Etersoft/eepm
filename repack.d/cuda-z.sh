04471e12368d367206b6c78b763f0221a4fbd0bd
Vitaly Lipatov <lav@etersoft.ru>
2023-07-15 01:16:48 +0300

    epm repack cuda-z: use UNIREQUIRES

--- repack.d/cuda-z.sh
+++ repack.d/cuda-z.sh
@@ -8,7 +8,10 @@ PRODUCT=cuda-z
 # static linked
 # strace -f cuda-z 2>&1 | grep \.so | grep lib64/lib | grep fstat | sed -e 's|.*<||' -e 's|>.*||' | sort -u | epm --quiet --short qf | sort -u | xargs -n100
 # glibc-core glibc-pthread libgcc1 libstdc++6 libX11 libXau libxcb libXcursor libXdmcp libXext libXfixes libXrender
-PREINSTALL_PACKAGES="glibc-core glibc-pthread libgcc1 libstdc++6 libX11 libXau libxcb libXcursor libXdmcp libXext libXfixes libXrender"
+#PREINSTALL_PACKAGES="glibc-core glibc-pthread libgcc1 libstdc++6 libX11 libXau libxcb libXcursor libXdmcp libXext libXfixes libXrender"
+# 
+UNIREQUIRES="libstdc++.so.6 libX11-xcb.so.1 libX11.so.6 libXau.so.6 libxcb.so.1 libXcursor.so.1 libXdmcp.so.6 libXext.so.6 libXfixes.so.3 libXrender.so.1"
+
 
 . $(dirname $0)/common.sh
 
@@ -38,5 +41,3 @@ install_file "https://cuda-z.sourceforge.net/img/web-download-detect.png" /usr/s
 # Running 32 bit cuda-z on Ubuntu
 # libc6:i386 libstdc++6:i386 zlib1g:i386 libx11-6:i386 libxext6:i386 libxrender1:i386
 # https://blog.redscorp.net/?p=94
-
-set_autoreq 'yes'
