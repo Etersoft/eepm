1ad998a0f8c11d6f296c00edcb595ccdb5b979cd
Vitaly Lipatov <lav@etersoft.ru>
2023-07-14 23:28:05 +0300

    epm repack: fix cnrdrvcups-ufr2-uk

--- repack.d/cnrdrvcups-ufr2-uk.sh
+++ repack.d/cnrdrvcups-ufr2-uk.sh
@@ -4,7 +4,9 @@ BUILDROOT="$1"
 
 SPEC="$2"
 
-PREINSTALL_PACKAGES="glib2 libatk libcairo libcairo-gobject libcups libgdk-pixbuf libgio libgtk+3 libpango libxml2 libjbig"
+. $(dirname $0)/common.sh
+
+add_requires "glib2 libatk libcairo libcairo-gobject libcups libgdk-pixbuf libgio libgtk+3 libpango libxml2 libjbig"
 
 # fix hang up when can't find libjbig.so libjbig.so.2.1 libjbig.so.2.0 libjbig.so.0
 # source code: linux-UFRII-drv-v570-m17n/Sources/cnrdrvcups-lb-5.70-1.11.tar.xz/utar://cnrdrvcups-common-5.70/cnjbig/cnjbig.c
@@ -12,6 +14,3 @@ if [ -f /usr/lib64/libjbig.so.1.6 ] ; then
     subst 's|libjbig.so.2.0|libjbig.so.1.6|' $BUILDROOT/usr/bin/cnjbigufr2
 fi
 
-. $(dirname $0)/common.sh
-
-set_autoreq 'yes'
