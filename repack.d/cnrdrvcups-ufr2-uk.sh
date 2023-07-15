224916f38fc2b72e543edadfa2e9a57b41455089
Vitaly Lipatov <lav@etersoft.ru>
2023-07-15 03:23:48 +0300

    epm repack cnrdrvcups-ufr2-uk: use UNIREQUIRES

--- repack.d/cnrdrvcups-ufr2-uk.sh
+++ repack.d/cnrdrvcups-ufr2-uk.sh
@@ -4,9 +4,11 @@ BUILDROOT="$1"
 
 SPEC="$2"
 
-. $(dirname $0)/common.sh
+UNIREQUIRES="libglib-2.0.so.0 libgmodule-2.0.so.0 libgobject-2.0.so.0 libgthread-2.0.so.0 libatk-1.0.so.0 libcairo.so.2 libcairo-gobject.so.2
+libcups.so.2 libcupsimage.so.2 libgdk_pixbuf-2.0.so.0 libgio-2.0.so.0 libgdk-3.so.0 libgtk-3.so.0
+libpango-1.0.so.0 libpangocairo-1.0.so.0 libpangoft2-1.0.so.0 libpangoxft-1.0.so.0 libxml2.so.2 libjbig.so.1.6"
 
-add_requires "glib2 libatk libcairo libcairo-gobject libcups libgdk-pixbuf libgio libgtk+3 libpango libxml2 libjbig"
+. $(dirname $0)/common.sh
 
 # fix hang up when can't find libjbig.so libjbig.so.2.1 libjbig.so.2.0 libjbig.so.0
 # source code: linux-UFRII-drv-v570-m17n/Sources/cnrdrvcups-lb-5.70-1.11.tar.xz/utar://cnrdrvcups-common-5.70/cnjbig/cnjbig.c
