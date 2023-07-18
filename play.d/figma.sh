060dc123087b6b21df135035d2b9001ec4a89ffc
Vitaly Lipatov <lav@etersoft.ru>
2023-07-18 23:24:52 +0300

    epm play: add figma

---
+++ play.d/figma.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PKGNAME=figma-linux
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Figma-linux - an unofficial Electron-based Figma desktop app for Linux"
+URL="https://github.com/Figma-Linux/figma-linux"
+
+. $(dirname $0)/common.sh
+
+[ "$VERSION" = "*" ] && VERSION="[0-9]*"
+
+pkgtype="$(epm print info -p)"
+
+# https://github.com/Figma-Linux/figma-linux/releases/download/v0.10.0/figma-linux_0.10.0_linux_x86_64.rpm
+# https://github.com/Figma-Linux/figma-linux/releases/download/v0.10.0/figma-linux_0.10.0_linux_amd64.deb
+
+case "$pkgtype" in
+    rpm)
+        file="${PKGNAME}_${VERSION}_linux_x86_64.$pkgtype"
+        ;;
+    deb)
+        file="${PKGNAME}_${VERSION}_linux_amd64.$pkgtype"
+        ;;
+    *)
+        pkgtype="deb"
+        file="${PKGNAME}_${VERSION}_linux_amd64.$pkgtype"
+        ;;
+esac
+
+PKGURL=$(epm tool eget --list --latest https://github.com/Figma-Linux/figma-linux/releases $file) || fatal "Can't get package URL"
+
+repack=''
+[ "$(epm print info -s)" = "alt" ] && repack='--repack'
+
+epm install $repack "$PKGURL" || exit
+
