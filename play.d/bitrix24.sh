34ee4b9c664c1250dd9c1ff126204ef253c05ec8
Vitaly Lipatov <lav@etersoft.ru>
2023-07-18 20:15:02 +0300

    epm play: add bitrix24

---
+++ play.d/bitrix24.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PKGNAME=bitrix24
+SUPPORTEDARCHES="x86_64"
+#VERSION="$2"
+DESCRIPTION="Bitrix24 desktop client from the official site"
+URL="https://www.bitrix24.ru/features/desktop.php"
+
+. $(dirname $0)/common.sh
+
+pkgtype=$(epm print info -p)
+case $pkgtype in
+    rpm)
+        PKGURL="https://dl.bitrix24.com/b24/bitrix24_desktop.rpm"
+        ;;
+    deb)
+        PKGURL="https://dl.bitrix24.com/b24/bitrix24_desktop.deb"
+        ;;
+esac
+
+repack=''
+#[ "$(epm print info -s)" = "alt" ] && repack='--repack'
+
+epm install $repack "$PKGURL"
