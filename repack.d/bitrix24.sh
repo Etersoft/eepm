eb320035d9cf803d77a67ea78907307b11fbdb6e
Vitaly Lipatov <lav@etersoft.ru>
2023-07-21 01:50:13 +0300

    epm repack bitrix24: add cli commands

--- repack.d/bitrix24.sh
+++ repack.d/bitrix24.sh
@@ -5,13 +5,18 @@ BUILDROOT="$1"
 SPEC="$2"
 
 PRODUCT=bitrix24
+PRODUCTCUR=Bitrix24
+PRODUCTDIR=/opt/$PRODUCTCUR
 
 UNIREQUIRES='libnotify.so.4'
 
 . $(dirname $0)/common.sh
 
-PRODUCTDIR=/opt/$PRODUCTCUR
-
 remove_dir /etc/apt/sources.list.d
 remove_dir /etc/yum.repos.d
 remove_dir /etc/apt/trusted.gpg.d
+
+add_bin_exec_command $PRODUCTCUR $PRODUCTDIR/$PRODUCTCUR
+add_bin_exec_command $PRODUCTCUR-web $PRODUCTDIR/$PRODUCTCUR-web
+add_bin_link_command $PRODUCT $PRODUCTCUR
+add_bin_link_command $PRODUCT-web $PRODUCTCUR-web
