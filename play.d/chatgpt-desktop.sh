3a866926190f9397b09109490d805a37aadeba1e
Vitaly Lipatov <lav@etersoft.ru>
2023-07-26 00:00:46 +0300

    epm play: add chatgpg-desktop

---
+++ play.d/chatgpt-desktop.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PKGNAME=chat-gpt
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION='Unofficial ChatGPT Desktop Application from the official site'
+URL="https://github.com/lencx/ChatGPT"
+
+. $(dirname $0)/common.sh
+
+https://github.com/lencx/ChatGPT/releases/download/v1.0.0/ChatGPT_1.0.0_linux_x86_64.deb
+PKGURL=$(epm tool eget --list --latest https://github.com/lencx/ChatGPT/releases/ "ChatGPT_${VERSION}_linux_x86_64.deb") || fatal "Can't get package URL"
+
+epm install "$PKGURL"
