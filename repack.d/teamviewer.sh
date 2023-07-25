87cd3441eaebd49506b22f6587457759e2736e6d
Vitaly Lipatov <lav@etersoft.ru>
2023-07-25 23:06:03 +0300

    epm repack teamviewer: rewrite

--- repack.d/teamviewer.sh
+++ repack.d/teamviewer.sh
@@ -6,25 +6,10 @@ SPEC="$2"
 
 PRODUCTDIR=/opt/teamviewer
 
+UNIREQUIRES="xdg-utils"
+# libdbus libqt5-core libqt5-dbus libqt5-gui libqt5-network libqt5-qml libqt5-quick libqt5-webkit libqt5-webkitwidgets libqt5-widgets libqt5-x11extras libminizip
+
 . $(dirname $0)/common.sh
-
-if [ "$(epm print info -a)" = "x86_64" ] ; then
-    # 32 bit
-    rm -fv $BUILDROOT/opt/teamviewer/tv_bin/script/libdepend
-    subst "s|.*script/libdepend.*||" $SPEC
-fi
-
-# commented out libexo (we have libexo-gtk3 only now)
-add_requires xdg-utils libdbus libqt5-core libqt5-dbus libqt5-gui libqt5-network libqt5-qml libqt5-quick libqt5-webkit libqt5-webkitwidgets libqt5-widgets libqt5-x11extras libminizip
-
-# TODO: check if we missed something from it
-rm -rf $BUILDROOT/opt/teamviewer/tv_bin/script/teamviewer_setup
-
-put_link()
-{
-    mkdir -p "$BUILDROOT$1"
-    ln -sr "$BUILDROOT/opt/teamviewer/tv_bin/script/$2" "$BUILDROOT$1/$2"
-}
 
 TV_DBUS_FILE_GUI='com.teamviewer.TeamViewer.service'
 TV_DBUS_FILE_DESKTOP='com.teamviewer.TeamViewer.Desktop.service'
@@ -32,39 +17,10 @@ TV_POLKIT_FILE='com.teamviewer.TeamViewer.policy'
 #put_link /usr/share/dbus-1/services $TV_DBUS_FILE_GUI
 #put_link /usr/share/dbus-1/services $TV_DBUS_FILE_DESKTOP
 #put_link /usr/share/polkit-1/actions $TV_POLKIT_FILE
-put_link /lib/systemd/system teamviewerd.service
-
-subst "s|\"/opt/teamviewer/tv_bin/script/teamviewer_setup\"|\n\
-/lib/systemd/system/teamviewerd.service\n\
-|" $SPEC
-
-#subst "s|\"/opt/teamviewer/tv_bin/script/teamviewer_setup\"|\n\
-#/usr/share/dbus-1/services/$TV_DBUS_FILE_GUI\n\
-#/usr/share/dbus-1/services/$TV_DBUS_FILE_DESKTOP\n\
-#/usr/share/polkit-1/actions/$TV_POLKIT_FILE\n\
-#/lib/systemd/system/teamviewerd.service\n\
-#|" $SPEC
+install_file $PRODUCTDIR/tv_bin/script/teamviewerd.service /lib/systemd/system/teamviewerd.service
 
 # don't use packed xdg-utils
-rm -rfv $BUILDROOT/opt/teamviewer/tv_bin/xdg-utils
-subst "s|.*/opt/teamviewer/tv_bin/xdg-utils.*||" $SPEC
+remove_dir /opt/teamviewer/tv_bin/xdg-utils
 
-rm -rfv $BUILDROOT/opt/teamviewer/tv_bin/script/teamviewerd.sysv
-subst "s|.*/opt/teamviewer/tv_bin/script/teamviewerd.sysv.*||" $SPEC
+remove_file /opt/teamviewer/tv_bin/script/teamviewerd.sysv
 
-# see https://bugzilla.altlinux.org/show_bug.cgi?id=39891
-filter_from_requires '\\/bin\\/ip'
-
-# ignore embedded libs
-filter_from_requires libQt5
-
-if epm assure patchelf ; then
-for i in $BUILDROOT$PRODUCTDIR/tv_bin/RTlib/{libicui18n.so.*,libicuuc.so.*} ; do
-    a= patchelf --set-rpath '$ORIGIN/' $i
-done
-fi
-
-remove_file $PRODUCTDIR/tv_bin/RTlib/qt/qml/QtWebEngine/libqtwebengineplugin.so.debug
-remove_file $PRODUCTDIR/tv_bin/RTlib/qt/qml/QtWebChannel/libdeclarative_webchannel.so.debug
-
-set_autoreq 'yes'
