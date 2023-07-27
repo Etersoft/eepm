911f45ea91a25d9f0574fffb9fe3d07e1ec12a03
Vitaly Lipatov <lav@etersoft.ru>
2023-07-27 18:36:34 +0300

    epm repack bitwarden: use electron deps

--- repack.d/bitwarden.sh
+++ repack.d/bitwarden.sh
@@ -7,10 +7,9 @@ SPEC="$2"
 PRODUCT=bitwarden
 PRODUCTDIR=/opt/Bitwarden
 
-# install all requires packages before packing (the list have got with rpmreqs package | xargs echo)
-PREINSTALL_PACKAGES="at-spi2-atk coreutils findutils gawk glib2 libalsa libatk libat-spi2-core libcairo libdbus libdrm libexpat libgbm libgdk-pixbuf libgio libgtk+3 libnspr libnss libpango libsecret libX11 libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbfile libXrandr libXrender libXScrnSaver libXtst sed"
-
 . $(dirname $0)/common.sh
+
+add_electron_deps
 
 add_bin_link_command
 add_bin_link_command $PRODUCTCUR $PRODUCT
@@ -18,5 +17,3 @@ add_bin_link_command $PRODUCTCUR $PRODUCT
 fix_chrome_sandbox
 
 fix_desktop_file
-
-set_autoreq 'yes'
