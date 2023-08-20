003d02fb65d4c9a4cfcb69ed4d8a0ca65617852e
Vitaly Lipatov <lav@etersoft.ru>
2023-08-20 23:45:47 +0300

    epm play: add balena-etcher

---
+++ play.d/balena-etcher.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PKGNAME=balena-etcher
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Flash OS images to SD cards & USB drives, safely and easily"
+URL="https://etcher.io/"
+
+. $(dirname $0)/common.sh
+
+pkgtype=$(epm print info -p)
+case $pkgtype in
+    rpm)
+        mask="balena-etcher-${VERSION}.x86_64.rpm"
+        ;;
+    deb)
+        mask="balena-etcher_${VERSION}_amd64.deb"
+        ;;
+esac
+
+PKGURL=$(epm tool eget --list --latest https://github.com/balena-io/etcher/releases $mask) || fatal "Can't get package URL"
+
+repack=''
+[ "$(epm print info -s)" = "alt" ] && repack='--repack'
+
+epm install $repack "$PKGURL"
---
+++ repack.d/balena-etcher.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=balena-etcher
+PRODUCTDIR=/opt/balenaEtcher
+
+. $(dirname $0)/common-chromium-browser.sh
+
+add_bin_link_command
+fix_desktop_file
+
+fix_chrome_sandbox
+add_electron_deps
