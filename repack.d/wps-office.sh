b5965b1316b9db9fcfdb9b629e9948255b714330
Vitaly Lipatov <lav@etersoft.ru>
2023-08-31 20:18:11 +0300

    epm repack wps-office: fix broken deps

--- repack.d/wps-office.sh
+++ repack.d/wps-office.sh
@@ -3,9 +3,9 @@
 # It will be run with two args: buildroot spec
 BUILDROOT="$1"
 SPEC="$2"
+PRODUCTDIR=/opt/kingsoft/wps-office
 
 . $(dirname $0)/common.sh
-
 
 #REQUIRES="fonts-ttf-liberation, fonts-ttf-dejavu"
 #subst "s|^\(Name: .*\)$|# Converted from original package requires\nRequires:$REQUIRES\n\1|g" $SPEC
@@ -18,6 +18,12 @@ remove_dir /etc/xdg/menus/applications-merged
 remove_file /usr/share/desktop-directories/wps-office.directory
 
 # ALT bug 45683
-remove_file /opt/kingsoft/wps-office/office6/wpscloudsvr
+remove_file $PRODUCTDIR/office6/wpscloudsvr
+
+# linked with missed libkappessframework.so()(64bit)
+remove_file $PRODUCTDIR/office6/addons/pdfbatchcompression/libpdfbatchcompressionapp.so
+
+# hack to fix bug somewhere in linking
+ignore_lib_requires "libc++.so"
 
 add_libs_requires
