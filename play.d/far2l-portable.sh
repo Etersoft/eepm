2cad885f41088554bcd2bb6cad9af9dd8403f2f9
Mikhail Tergoev <fidel@altlinux.org>
2023-09-20 17:07:35 +0300

    epm play far2l-portable: move to AppImage

--- play.d/far2l-portable.sh
+++ play.d/far2l-portable.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
-PKGNAME=far2l-portable
-SUPPORTEDARCHES="x86_64 x86 aarch64"
+PKGNAME="far2l-portable"
+SUPPORTEDARCHES="x86_64"
 DESCRIPTION="FAR2L Portable from the official site"
 
 . $(dirname $0)/common.sh
 
-arch=$(epm print info -a)
-case $arch in
-    x86_64)
-        arch="amd64" ;;
-    x86)
-        arch="i386" ;;
-    aarch64)
-        arch=$arch ;;
-    *)
-        fatal "Unsupported arch $arch for $(epm print info -d)"
-esac
+PKGURL=$(epm tool eget --list --latest https://github.com/spvkgn/far2l-portable/releases "far2l_x86_64.AppImage.tar")
+[ -n "$PKGURL" ] || fatal "Can't get package URL"
 
-VERSION="2.4"
-PKGURL="https://github.com/unxed/far2l-deb/raw/master/portable/far2l_portable_$arch.tar.gz"
-epm pack --install $PKGNAME "$PKGURL" "$VERSION"
+epm --install pack $PKGNAME "$PKGURL"
