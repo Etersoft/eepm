71e22b9687714d01f2722c48c92cc2d4a7b442a8
Mikhail Tergoev <fidel@altlinux.org>
2023-09-20 17:07:35 +0300

    epm play far2l-portable: move to AppImage

--- pack.d/far2l-portable.sh
+++ pack.d/far2l-portable.sh
@@ -2,33 +2,16 @@
 
 TAR="$1"
 RETURNTARNAME="$2"
-VERSION="$3"
-
-PRODUCTCUR=far2l
+# PRODUCT="far2l"
 
 . $(dirname $0)/common.sh
 
-[ -n "$VERSION" ] || fatal "Missed archive version"
-
-PKGNAME=$PRODUCT-$VERSION.tar
-
-if echo "$TAR" | grep -q "far2l_portable.*.tar.gz" ; then
-    erc $TAR || fatal
-    RUNFILE="$(echo far2l*.run)"
-elif echo "$TAR" | grep -q "far2l_portable.*.run" ; then
-    RUNFILE="$TAR"
-fi
-
-[ -s "$RUNFILE" ] || fatal "Missed $RUNFILE"
-
-mkdir -p opt/$PRODUCT/
-
-sh $RUNFILE --nox11 --noexec --target opt/$PRODUCT || fatal
-
-#mkdir -p $tdir/usr/bin/
-#ln -s /opt/$PRODUCT/$PRODUCTCUR $tdir/usr/bin/$PRODUCTCUR
-
-#erc pack $PKGNAME opt/$PRODUCT usr/bin/ || fatal
-erc pack $PKGNAME opt/$PRODUCT || fatal
-
+erc $TAR || fatal
+PKGNAME="$(ls | grep -e "_x86_64.AppImage$")"
 return_tar "$PKGNAME"
+
+# TODO: return original version from generic-appimage.sh
+# VERSION="$(basename "$PKGNAME" .AppImage | awk -F_ '{print $2}')"
+# [ -n "$VERSION" ] || fatal "Can't get version $TAR."
+# mv "$PKGNAME" "$PRODUCT-$VERSION.AppImage"
+# return_tar "$PRODUCT-$VERSION.AppImage"
