49e10dff51bde0b4184475e0ae3d9a450876551e
Mikhail Tergoev <fidel@altlinux.org>
2023-09-20 12:32:10 +0300

    epm play duplicati: replaced set_autoreq by add_libs_requires

--- repack.d/duplicati.sh
+++ repack.d/duplicati.sh
@@ -14,7 +14,8 @@ add_requires mono-full libgtk-sharp2
 subst '1iBuildRequires: rpm-build-python3' $SPEC
 subst '1i%add_python3_path /usr/lib/duplicati' $SPEC
 
-set_autoreq 'yes,nomonolib,nomono'
+# set_autoreq 'yes,nomonolib,nomono'
+add_libs_requires
 
 subst 's|env python.*|env python3|' $BUILDROOT/usr/lib/duplicati/utility-scripts/DuplicatiVerify.py
 subst 's|/usr/bin/bash|/bin/bash|' $BUILDROOT/usr/lib/duplicati/{lvm-scripts/*.sh,run-script-example.sh} $BUILDROOT/usr/bin/{duplicati-server,duplicati-cli,duplicati}
