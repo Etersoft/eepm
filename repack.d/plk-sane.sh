c3f58e59326b873814cb8cdde41792e532e21ef7
Andrey Cherepanov <cas@altlinux.org>
2023-09-22 11:55:27 +0300

    epm repack: add support for plk-sane

---
+++ repack.d/plk-sane.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+# Remove LIBJPEG version
+subst '1i%filter_from_requires /LIBJPEG.*_6.2/d' $SPEC
+
+set_autoreq 'yes'
