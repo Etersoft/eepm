b294e394c040d148f22dcb14f09ade51424f3fc3
Vitaly Lipatov <lav@etersoft.ru>
2023-10-08 00:16:23 +0300

    epm pack k3s: add symlinks

--- pack.d/k3s.sh
+++ pack.d/k3s.sh
@@ -9,7 +9,9 @@ VERSION="$3"
 PKGNAME=$PRODUCT-$VERSION
 
 install -D $TAR usr/bin/$PRODUCT || fatal
-ln -s $PRODUCT usr/bin/kubectl
-erc pack $PKGNAME.tar usr/bin/$PRODUCT usr/bin/kubectl
+for i in kubectl crictl ctr k3s ; do
+    ln -s $PRODUCT usr/bin/$i
+done
+erc pack $PKGNAME.tar usr/bin
 
 return_tar $PKGNAME.tar
