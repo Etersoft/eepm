d8bb020500c8eea806e9701179d0fffe0c743827
Vitaly Lipatov <lav@etersoft.ru>
2023-12-23 04:51:09 +0300

    epm play viber: use only latest Viber version

--- play.d/viber.sh
+++ play.d/viber.sh
@@ -2,10 +2,12 @@
 
 PKGNAME=viber
 SUPPORTEDARCHES="x86_64"
+VERSION="$2"
 DESCRIPTION="Viber for Linux from the official site"
 
 . $(dirname $0)/common.sh
 
+[ "$VERSION" = "*" ] || fatal "Only latest Viber version is allowed"
 
 # the same binaries in deb and rpm
 PKG="https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb"
