06a41477671b15ec81103bb88b5a07ba4a779005
Vitaly Lipatov <lav@etersoft.ru>
2024-01-31 12:09:14 +0300

    epm repack librewolf: fix dependencies

--- repack.d/librewolf.sh
+++ repack.d/librewolf.sh
@@ -5,15 +5,12 @@ SPEC="$2"
 
 PRODUCT=librewolf
 
-PREINSTALL_PACKAGES="fontconfig glib2 libalsa libatk libcairo libcairo-gobject libdbus libdbus-glib libfreetype libgdk-pixbuf libgio libgtk+3 libharfbuzz libpango libX11 libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libXrandr libXrender libXtst"
-
-
 . $(dirname $0)/common.sh
-
-set_autoreq 'yes,noshell,nomonolib,nomono,nopython'
 
 move_to_opt
 fix_desktop_file "/usr/share/$PRODUCT/$PRODUCT"
+
+add_libs_requires
 
 rm -f $BUILDROOT/usr/bin/librewolf
 add_bin_link_command
