22a2232fabc552a671f6a97ea49b83f857511620
Vitaly Lipatov <lav@etersoft.ru>
2024-02-26 01:47:22 +0300

    epm repack plex-desktop: fix repack with eepm-rpm-build

--- play.d/plex-desktop.sh
+++ play.d/plex-desktop.sh
@@ -4,6 +4,7 @@ PKGNAME=plex-desktop
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION="Plex Desktop from the official site"
+URL="https://www.plex.tv/"
 
 . $(dirname $0)/common.sh
 
--- repack.d/plex-desktop.sh
+++ repack.d/plex-desktop.sh
@@ -6,9 +6,8 @@ SPEC="$2"
 
 . $(dirname $0)/common.sh
 
-add_requires glib2 glxinfo libalsa libdbus libdrm libEGL libexpat fontconfig libfreetype libgbm libGLX libharfbuzz libjpeg8 liblcms2 libminizip libnspr libnss libOpenGL libopus libpci libpulseaudio libsnappy libtiff5 libudev1 libva libwayland-client libwayland-cursor libwayland-egl libwayland-server libX11 libxcb libxcb-render-util libxcbutil-icccm libxcbutil-image libxcbutil-keysyms libXcomposite libXdamage libXext libXfixes libXinerama libxkbcommon libxkbcommon-x11 libxkbfile libxml2 libXrandr libXrender libXScrnSaver libxshmfence libxslt libXtst which zlib
-
 add_bin_link_command $PRODUCT $PRODUCTDIR/Plex.sh
+
 
 cd .$PRODUCTDIR || fatal
 
@@ -23,6 +22,14 @@ done
 for i in libEGL.so.1 libdrm.so.2 libdrm_amdgpu.so.1 libva-drm.so.2 libva-x11.so.2 libva.so.2 ; do
     remove_file $PRODUCTDIR/lib/$i
 done
+
+cd >/dev/null
+
+add_libs_requires
+
+exit
+
+add_requires glib2 glxinfo libalsa libdbus libdrm libEGL libexpat fontconfig libfreetype libgbm libGLX libharfbuzz libjpeg8 liblcms2 libminizip libnspr libnss libOpenGL libopus libpci libpulseaudio libsnappy libtiff5 libudev1 libva libwayland-client libwayland-cursor libwayland-egl libwayland-server libX11 libxcb libxcb-render-util libxcbutil-icccm libxcbutil-image libxcbutil-keysyms libXcomposite libXdamage libXext libXfixes libXinerama libxkbcommon libxkbcommon-x11 libxkbfile libxml2 libXrandr libXrender libXScrnSaver libxshmfence libxslt libXtst which zlib
 
 if epm assure patchelf ; then
 for i in bin/Plex "bin/Plex Transcoder" ; do
