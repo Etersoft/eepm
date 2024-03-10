44b07cc2fce10444a86e2b9bfae6e60894b6871f
Vitaly Lipatov <lav@etersoft.ru>
2024-03-11 02:35:50 +0300

    epm play: add singularityapp (fix github issue 49)

---
+++ play.d/singularityapp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME=singularityapp
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="SingularityApp from the official site"
+URL="https://snapcraft.io/singularityapp"
+
+. $(dirname $0)/common.sh
+
+SNAPNAME="singularityapp"
+# https://api.snapcraft.io/api/v1/snaps/download/qc6MFRM433ZhI1XjVzErdHivhSOhlpf0_37.snap
+PKGURL="$(eget -O- -H Snap-Device-Series:16 https://api.snapcraft.io/v2/snaps/info/$SNAPNAME | epm --inscript tool json -b | grep '\["channel-map",0,"download","url"\]' | head -n1 | sed -e 's|.*"\(.*\)"$|\1|' )" || fatal "Can't get URL"
+
+epm install "$PKGURL"
