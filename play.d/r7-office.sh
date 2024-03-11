e55ac122d1ba09344d0c1d79437cf983092ef2e0
Vitaly Lipatov <lav@etersoft.ru>
2024-03-11 06:52:33 +0300

    epm play r7-office: update for support new versions

--- play.d/r7-office.sh
+++ play.d/r7-office.sh
@@ -4,34 +4,44 @@ PKGNAME=r7-office
 SKIPREPACK=1
 SUPPORTEDARCHES="x86_64"
 DESCRIPTION="R7 Office for Linux from the official site"
+URL="https://r7-office.ru/"
 
 . $(dirname $0)/common.sh
 
-# https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=r7-office
+URLBASE="https://r7-office.ru/download_editor"
+
+#[ "$VERSION" = "*" ]
+
+# $ eget -U --list "https://r7-office.ru/download_editor" "*.rpm"
+# $ eget -U --list "https://r7-office.ru/download_editor" "*.deb"
 
 case $(epm print info -e) in
     AstraLinux*|Debian/*)
-        PKG="https://download.r7-office.ru/astra/r7-office.deb"
+        mask="astra/*.deb"
         ;;
     Ubuntu/*)
-        PKG="https://download.r7-office.ru/ubuntu/r7-office.deb"
+        mask="ubuntu/*.deb"
         ;;
     RedOS/*|AlterOS/*)
-        PKG="https://download.r7-office.ru/redos/r7-office.rpm"
+        mask="centos/*.rpm"
         ;;
-    AlterOS/*|CentOS/*)
-        PKG="https://download.r7-office.ru/centos/r7-office.rpm"
+    ROSA/7.9)
+        mask="rosa/cobalt/$PKGNAME-*.rpm"
+        ;;
+    ROSA/*)
+        mask="rosa/$PKGNAME-*.rpm"
         ;;
     ALTLinux/*)
-        PKG="https://download.r7-office.ru/altlinux/r7-office.rpm"
-        epm install --skip-installed fonts-ttf-dejavu fonts-ttf-google-crosextra-carlito fonts-ttf-liberation gst-libav gst-plugins-ugly1.0 libX11 libXScrnSaver libcairo libgcc1 libgtk+2 libgtkglext
+        mask="altlinux/*.rpm"
         ;;
     *)
         fatal "Unsupported distro $(epm print info -e). Ask application vendor for a support."
         ;;
 esac
 
+PKGURL="$(epm tool eget --list --latest -U  "$URLBASE" "$mask")"
+
 # install with scripts (need for install icons and associations)
 # see /etc/eepm/pkgallowscripts.list
 # TODO: pack it into the package
-epm install "$PKG"
+epm install "$PKGURL"
