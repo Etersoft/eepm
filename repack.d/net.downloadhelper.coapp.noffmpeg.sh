97c67f45f50b2cc93e4d8eb02081c0ae664c60c8
Vitaly Lipatov <lav@etersoft.ru>
2024-03-11 06:26:53 +0300

    epm repack: add net.downloadhelper.coapp.noffmpeg

---
+++ repack.d/net.downloadhelper.coapp.noffmpeg.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+PRODUCTDIR=/opt/vdhcoapp
+
+. $(dirname $0)/common.sh
+
+# use xdg-open from the system
+rm -v .$PRODUCTDIR/xdg-open
+ln -s /usr/bin/xdg-open .$PRODUCTDIR/xdg-open
+add_unirequires /usr/bin/xdg-open
+add_unirequires /usr/bin/ffmpeg /usr/bin/ffplay /usr/bin/ffprobe
+
+cat <<EOF > net.downloadhelper.coapp.json
+{
+  "type": "stdio",
+  "allowed_extensions": [
+    "weh-native-test@downloadhelper.net",
+    "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}"
+  ],
+  "name": "net.downloadhelper.coapp",
+  "description": "Video DownloadHelper companion app",
+  "path": "$PRODUCTDIR/vdhcoapp"
+}
+EOF
+install_file net.downloadhelper.coapp.json /usr/lib64/mozilla/native-messaging-hosts/net.downloadhelper.coapp.json
+
+cat <<EOF > net.downloadhelper.coapp.json
+{
+  "type": "stdio",
+  "allowed_origins": [
+    "chrome-extension://lmjnegcaeklhafolokijcfjliaokphfk/",
+    "chrome-extension://pfoiagbblcbmognbkekfpodpidedkmcc/",
+    "chrome-extension://jmkaglaafmhbcpleggkmaliipiilhldn/",
+    "chrome-extension://fojefjolbhfidomcaelhceoldmmpcaga/"
+  ],
+  "name": "net.downloadhelper.coapp",
+  "description": "Video DownloadHelper companion app",
+  "path": "$PRODUCTDIR/vdhcoapp"
+}
+EOF
+install_file net.downloadhelper.coapp.json /etc/chromium/native-messaging-hosts/net.downloadhelper.coapp.json
