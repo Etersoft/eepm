585cc47404f22f42e291fc6b761c777599b8fd31
Vitaly Lipatov <lav@etersoft.ru>
2024-03-11 20:00:53 +0300

    epm play: add neovide

---
+++ play.d/neovide.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PKGNAME=neovide
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="No Nonsense Neovim Client in Rust from the official site"
+URL="https://neovide.dev/"
+
+. $(dirname $0)/common.sh
+
+PKGURL=$(epm tool eget --list --latest https://github.com/neovide/neovide/releases $PKGNAME.AppImage) || fatal "Can't get package URL"
+
+epm install "$PKGURL"
+
