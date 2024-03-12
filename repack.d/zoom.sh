f10c1542200b3dced02c734a1e39567418aaa647
Vitaly Lipatov <lav@etersoft.ru>
2024-03-12 09:28:42 +0300

    epm repack zoom: add bug link

--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -17,7 +17,9 @@ fix_chrome_sandbox $PRODUCTDIR/cef/chrome-sandbox
 
 fix_desktop_file /usr/bin/zoom
 
+# https://bugzilla.altlinux.org/47427
 remove_file /opt/zoom/Qt/qml/Qt/labs/lottieqt/liblottieqtplugin.so
+
 add_libs_requires
 # autoreq is disabled: don't patch elf due requires
 exit
