9b28124b7eccbdfaa3921056724dca69b6f28395
Vitaly Lipatov <lav@etersoft.ru>
2024-03-21 01:11:53 +0300

    epm play todoist: cleanup for new AppImage naming scheme

--- play.d/todoist.sh
+++ play.d/todoist.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-PKGNAME=Todoist
+PKGNAME=Todoist-linux
 SUPPORTEDARCHES="x86_64"
 DESCRIPTION='Todoist client application from the official site'
+URL="https://todoist.com/"
 
 . $(dirname $0)/common.sh
 
 PKGURL="https://todoist.com/linux_app/appimage"
 
-# TODO: rename AppImages on the fly (in pack.d/generic-appimage.sh)
-cd_to_temp_dir
-epm tool eget "$PKGURL" || fatal
-newname="$(echo *.AppImage | sed -e "s|^Todoist-linux-x86_64-|$PKGNAME-|" )"
-mv -v *.AppImage $newname || exit
-
-epm install $newname
+epm install $PKGURL
