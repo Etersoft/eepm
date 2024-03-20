21dcc1330bf063bc19dc325ee0c55df1ff2a6df5
Vitaly Lipatov <lav@etersoft.ru>
2024-03-21 02:50:39 +0300

    epm play geogebra: restore install via IPFS store

--- play.d/geogebra.sh
+++ play.d/geogebra.sh
@@ -4,6 +4,7 @@ PKGNAME=geogebra-classic
 SUPPORTEDARCHES="x86_64 x86"
 VERSION="$2"
 DESCRIPTION="Geogebra 6 from the official site"
+URL="https://wiki.geogebra.org/en/Reference:GeoGebra_Installation"
 
 . $(dirname $0)/common.sh
 
@@ -23,11 +24,13 @@ repack=''
 [ "$(epm print info -s)" = "alt" ] && repack='--repack'
 
 case $pkgtype in
-    deb)
-        epm install "https://www.geogebra.net/linux/pool/main/g/geogebra-classic/$(epm print constructname $PKGNAME "$VERSION" $arch)"
-        ;;
+    #deb)
+    #    epm install "https://www.geogebra.net/linux/pool/main/g/geogebra-classic/$(epm print constructname $PKGNAME "$VERSION" $arch)"
+    #    ;;
     rpm)
-        epm $repack install "https://www.geogebra.net/linux/rpm/$arch/$(epm print constructname $PKGNAME "$VERSION" $arch)"
+        #epm $repack install "https://www.geogebra.net/linux/rpm/$arch/$(epm print constructname $PKGNAME "$VERSION" $arch)"
+        #http://www.geogebra.net/linux/rpm/x86_64/geogebra-classic-6.0.666.0-202109211234.x86_64.rpm
+        epm $repack install "ipfs://Qmc6KzDcverDKJptZd6ffJMqyYbKKcuwdHitB6EAEw2C6n?filename=geogebra-classic-6.0.666.0-202109211234.x86_64.rpm"
         ;;
     *)
         fatal "Unsupported $pkgtype"
