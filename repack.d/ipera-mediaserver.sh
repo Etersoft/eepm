7fdd248b76f8f4b8ae331b744da1ad48e541ded1
Vitaly Lipatov <lav@etersoft.ru>
2024-03-21 03:00:09 +0300

    epm repack ipera-mediaserver: fix requires

--- repack.d/ipera-mediaserver.sh
+++ repack.d/ipera-mediaserver.sh
@@ -9,6 +9,13 @@ PRODUCTDIR=/opt/ipera/mediaserver
 
 remove_dir /etc/init
 
+# see https://bugzilla.altlinux.org/47890
+# hack due broken provides in libcurl-gnutls-compat
+ignore_lib_requires "libcurl-gnutls.so.4"
+add_requires "libcurl-gnutls.so.4(64bit)"
+
+ignore_lib_requires "libnx_clusterdb_engine.so"
+
 add_libs_requires
 
 mkdir -p var/lib/ipera
@@ -16,20 +23,3 @@ pack_dir /var/lib/ipera
 ln -s /var/lib/ipera .$PRODUCTDIR/var
 pack_file $PRODUCTDIR/var
 
-exit
-
-if epm assure patchelf ; then
-cd $BUILDROOT$PRODUCTDIR || exit
-for i in lib/lib*.so.* ; do
-    a= patchelf --set-rpath '$ORIGIN' $i
-done
-
-for i in bin/plugins_optional/lib*.so bin/plugins/lib*.so ; do
-    a= patchelf --set-rpath '$ORIGIN/../../lib' $i
-done
-fi
-
-filter_from_requires "libldap_r-2.4.so.2(OPENLDAP_2.*)(64bit)" "liblber-2.4.so.2(OPENLDAP_2.*)(64bit)" "ld-linux-.*(GLIBC_PRIVATE)"
-filter_from_requires libQt5 libGL libicu
-
-set_autoreq 'yes'
