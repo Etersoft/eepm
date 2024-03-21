803e6871d7693ad0b668283eea288f5eac4444c9
Vitaly Lipatov <lav@etersoft.ru>
2024-03-21 12:20:19 +0300

    epm pack far2l-portable: fix package name as far2l-portable

--- pack.d/far2l-portable.sh
+++ pack.d/far2l-portable.sh
@@ -7,11 +7,7 @@ RETURNTARNAME="$2"
 . $(dirname $0)/common.sh
 
 erc $TAR || fatal
-PKGNAME="$(ls | grep -e "_x86_64.AppImage$")"
+pkg="$(ls | grep -e "_x86_64.AppImage$")"
+PKGNAME="${pkg/far2l/far2l-portable}"
+mv $pkg $PKGNAME
 return_tar "$PKGNAME"
-
-# TODO: return original version from generic-appimage.sh
-# VERSION="$(basename "$PKGNAME" .AppImage | awk -F_ '{print $2}')"
-# [ -n "$VERSION" ] || fatal "Can't get version $TAR."
-# mv "$PKGNAME" "$PRODUCT-$VERSION.AppImage"
-# return_tar "$PRODUCT-$VERSION.AppImage"
