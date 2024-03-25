40473c6ce2bfe6f6a73916ce2ddab4395170213e
Vitaly Lipatov <lav@etersoft.ru>
2024-03-26 00:50:09 +0300

    commit packed 3.60.12

--- packed/serv.sh
+++ packed/serv.sh
@@ -33,7 +33,7 @@ SHAREDIR=$PROGDIR
 # will replaced with /etc/eepm during install
 CONFIGDIR=$PROGDIR/../etc
 
-EPMVERSION="3.60.11"
+EPMVERSION="3.60.12"
 
 # package, single (file), pipe, git
 EPMMODE="package"
