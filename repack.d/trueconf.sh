f058962184c3b1046f754641a86c3a5319dff970
Vitaly Lipatov <lav@etersoft.ru>
2024-03-28 02:44:50 +0300

    epm repack trueconf: fix for trueconf client since 8.4.0.1957

--- repack.d/trueconf.sh
+++ repack.d/trueconf.sh
@@ -5,7 +5,7 @@ SPEC="$2"
 ORIGINPACKAGE="$4"
 
 PRODUCT=trueconf
-PRODUCTDIR=/opt/trueconf
+PRODUCTDIR=/opt/trueconf/client
 
 . $(dirname $0)/common.sh
 
@@ -13,25 +13,23 @@ PRODUCTDIR=/opt/trueconf
 reqs="$(epm requires "$ORIGINPACKAGE")"
 [ -n "$reqs" ] && add_requires $reqs
 
+# for old trueconf (before 8.4.0.1957)
+[ -d .$PRODUCTDIR ] || PRODUCTDIR=/opt/$PRODUCT
+
 add_bin_link_command
 
-chmod a+x $BUILDROOT/opt/trueconf/trueconf
-chmod a+x $BUILDROOT/opt/trueconf/trueconf-autostart
+chmod a+x .$PRODUCTDIR/trueconf
+chmod a+x .$PRODUCTDIR/trueconf-autostart
+
+if [ -e .$PRODUCTDIR/QtWebEngineProcess ]; then
+    chmod a+x .$PRODUCTDIR/QtWebEngineProcess
+fi
 
 # TODO: report the bug:
 # libhwloc.so.5 => not found (we have only libhwloc.so.15)
-remove_file $PRODUCTDIR/lib/libtbbbind.so
-remove_file $PRODUCTDIR/lib/libtbbbind.so.2
+#remove_file $PRODUCTDIR/lib/libtbbbind.so
+#remove_file $PRODUCTDIR/lib/libtbbbind.so.2
+# or
+#filter_from_requires libhwloc.so.5
 
-exit
-
-if epm assure patchelf ; then
-for i in lib/lib*.so  ; do
-    a= patchelf --set-rpath '$ORIGIN' $BUILDROOT$PRODUCTDIR/$i
-done
-
-for i in TrueConf ; do
-    a= patchelf --set-rpath '$ORIGIN/lib' $BUILDROOT$PRODUCTDIR/$i
-done
-fi
-
+# (requires is disabled by default now)
