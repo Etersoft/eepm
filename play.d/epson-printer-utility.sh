9ff8743548c0cd9037ebc05d3bb734fc97c62d41
Ivan Mazhukin <vanomj@etersoft.ru>
2024-03-27 19:05:41 +0300

    epm play: add epson-printer-utility

---
+++ play.d/epson-printer-utility.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+PKGNAME=epson-printer-utility
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="Epson Printer Utility - Linux Epson Printer Utility from the official site"
+URL="http://support.epson.net/linux/Printer/LSB_distribution_pages/en/utility.php"
+
+# TODO: remove repo too
+case "$1" in
+    "--remove")
+        epm remove $(epm qp $PKGNAME-)
+        exit
+        ;;
+esac
+
+
+. $(dirname $0)/common.sh
+
+pkgtype=$(epm print info -p)
+arch="$(epm print info -a)"
+case "$pkgtype-$arch" in
+    rpm-x86_64)
+        PKGURL="https://download.ebz.epson.net/dsc/du/02/DriverDownloadInfo.do?LG2=JA&CN2=US&CTI=177&PRN=Linux%20rpm%2064bit%20package&OSC=LX&DL"
+        ;;
+    *-x86_64)
+        PKGURL="https://download.ebz.epson.net/dsc/du/02/DriverDownloadInfo.do?LG2=JA&CN2=US&CTI=177&PRN=Linux%20deb%2064bit%20package&OSC=LX&DL"
+        ;;
+    *)
+        fatal "$arch arch is not supported"
+        ;;
+esac
+
+repack=''
+[ "$(epm print info -s)" = "alt" ] && repack='--repack'
+
+epm install $repack "$PKGURL"
+
+echo
+echo "Note: run
+# serv ecbd on
+to enable needed epson-printer-utility system service
+"
