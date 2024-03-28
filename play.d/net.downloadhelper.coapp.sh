a13f56adfe7af77808fd55354315224d45a006fa
Vitaly Lipatov <lav@etersoft.ru>
2024-03-28 07:02:55 +0300

    epm play vdhcoapp: update to new filename

--- play.d/net.downloadhelper.coapp.sh
+++ play.d/net.downloadhelper.coapp.sh
@@ -3,14 +3,14 @@
 PKGNAME=net.downloadhelper.coapp
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
-DESCRIPTION="Video DownloadHelper Companion App"
+DESCRIPTION="Video DownloadHelper Companion App (obsoleted)"
 URL="https://www.downloadhelper.net/install-coapp"
 
 . $(dirname $0)/common.sh
 
 mask="$PKGNAME-${VERSION}_amd64.deb"
 
-PKGURL=$(eget --list --latest https://github.com/mi-g/vdhcoapp/releases/ $mask) || fatal "Can't get package URL"
+PKGURL=$(eget --list --latest https://github.com/aclap-dev/vdhcoapp/releases/ $mask) || fatal "Can't get package URL"
 
 repack=''
 [ "$(epm print info -s)" = "alt" ] && repack='--repack'
--- play.d/vdhcoapp.sh
+++ play.d/vdhcoapp.sh
@@ -4,15 +4,15 @@ PKGNAME=net.downloadhelper.coapp.noffmpeg
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Video DownloadHelper Companion App 2"
-URL="https://www.downloadhelper.net/install-coapp-v2"
+URL="https://www.downloadhelper.net/w/CoApp-Installation"
 
 . $(dirname $0)/common.sh
 
 arch="$(epm print info -a)"
 
-mask="$PKGNAME-${VERSION}-linux-$arch.deb"
+mask="dhcoapp-noffmpeg-linux-$arch.deb"
 
-PKGURL=$(eget --list --latest https://github.com/mi-g/vdhcoapp/releases/ $mask) || fatal "Can't get package URL"
+PKGURL=$(eget --list --latest https://github.com/aclap-dev/vdhcoapp/releases/ $mask) || fatal "Can't get package URL"
 
 repack=''
 [ "$(epm print info -s)" = "alt" ] && repack='--repack'
