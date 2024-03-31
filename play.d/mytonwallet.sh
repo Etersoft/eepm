0e7c41d25d6eb2a3d77a9712667ccc30d398ce5b
Vitaly Lipatov <lav@etersoft.ru>
2024-03-31 05:44:22 +0300

    epm play: add mytonwallet

---
+++ play.d/mytonwallet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PKGNAME=MyTonWallet
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="MyTonWallet from the official site"
+URL="https://mytonwallet.app/"
+
+. $(dirname $0)/common.sh
+
+PKGURL=$(eget --list --latest https://github.com/mytonwalletorg/mytonwallet/releases "MyTonWallet-x86_64.AppImage") || fatal "Can't get package URL"
+
+epm install "$PKGURL"
