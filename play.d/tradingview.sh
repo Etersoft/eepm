12d32b16d5edaafdd509d511d13c412e26cfe704
Ivan Mazhukin <vanomj@etersoft.ru>
2024-04-01 22:20:33 +0300

    epm play: add tradingview

---
+++ play.d/tradingview.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PKGNAME=tradingview
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="TradingView from the snapcraft"
+URL="https://snapcraft.io/tradingview"
+
+. $(dirname $0)/common.sh
+
+PKGURL="$(snap_get_pkgurl $PKGNAME)"
+install_pkgurl
