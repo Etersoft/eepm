8f30be52b15115b978cdc85a9fa913ba8ddb0be0
Vitaly Lipatov <lav@etersoft.ru>
2024-04-01 14:33:32 +0300

    epm play myoffice: add direct download for known version

--- play.d/myoffice.sh
+++ play.d/myoffice.sh
@@ -14,9 +14,19 @@ pkgtype=$(epm print info -p)
 # /var/lib/dpkg/info/myoffice-standard-home-edition.postinst: line 62: xdg-desktop-menu: command not found
 epm assure xdg-desktop-menu xdg-utils
 
+delim="-" 
+if [ "$pkgtype" != "rpm" ] ; then
+    delim="_"
+    pkgtype="deb"
+fi
+
 # https://preset.myoffice-app.ru/myoffice-standard-home-edition-2.3.0-x86_64.rpm
 # https://preset.myoffice-app.ru/myoffice-standard-home-edition_2.3.0_amd64.deb
-PKGMASK="$(epm print constructname $PKGNAME "$VERSION" "" "" "" "[-_]")"
-PKGURL="$(eget --list --latest https://myoffice.ru/products/standard-home-edition/ "$PKGMASK")"
+PKGMASK="$(epm print constructname $PKGNAME "$VERSION" "" "$pkgtype" "" "$delim")"
+if [ "$VERSION" = "*" ] ; then
+    PKGURL="$(eget --list --latest https://myoffice.ru/products/standard-home-edition/ "$PKGMASK")"
+else
+    PKGURL="https://preset.myoffice-app.ru/$PKGMASK"
+fi
 
 epm install "$PKGURL"
