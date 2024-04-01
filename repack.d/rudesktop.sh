5f41d33acfad1ffe692c93af38deebd186114f86
Vitaly Lipatov <lav@etersoft.ru>
2024-04-01 19:01:29 +0300

    epm repack rudesktop: drop original requires

--- repack.d/rudesktop.sh
+++ repack.d/rudesktop.sh
@@ -8,8 +8,8 @@ ORIGINPACKAGE="$4"
 . $(dirname $0)/common.sh
 
 # follow original requires
-reqs="$(epm requires "$ORIGINPACKAGE")"
-[ -n "$reqs" ] && add_requires $reqs
+#reqs="$(epm requires "$ORIGINPACKAGE")"
+#[ -n "$reqs" ] && add_requires $reqs
 
 # ??
 # echo "root ALL=(ALL) NOPASSWD:SETENV:/usr/bin/rudesktop" > /etc/sudoers.d/rudesktop
@@ -20,3 +20,5 @@ install_file usr/share/rudesktop-client/files/rudesktop.desktop /usr/share/appli
 #xdg-mime default rudesktop.desktop x-scheme-handler/rudesktop || true
 
 subst "s|^Summary:.*|Summary: A remote control software.|" $SPEC
+
+add_libs_requires
