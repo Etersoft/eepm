e026f806c44bb08563a25eb9d0eb5de57588a35a
Vitaly Lipatov <lav@etersoft.ru>
2024-04-02 22:33:54 +0300

    epm play telegram: add direct URL support

--- play.d/telegram.sh
+++ play.d/telegram.sh
@@ -18,12 +18,17 @@ if [ "$VERSION" = "*" ] ; then
     if ! is_glibc_enough 2.28 ; then
         fatal "glibc is too old, upgrade your system."
     fi
+
+    PKGURL=$(eget --list --latest https://github.com/telegramdesktop/tdesktop/releases "tsetup.$VERSION.tar.xz") #"
 else
-    VERSION="$VERSION*"
+    PKGBASEURL="https://github.com/telegramdesktop/tdesktop/releases/download/v$VERSION"
+    [ "$PKGNAME" = "$BASEPKGNAME" ] || VERSION="$VERSION.beta"
+    # version can be 1.2.3.beta or 1.2.3
+    PKGURL="$PKGBASEURL/tsetup.$VERSION.tar.xz"
+    #if [ "$PKGNAME" = "$BASEPKGNAME" ] && ! epm tool eget --check-url "$PKGURL" ; then
+    #    PKGURL="$PKGBASEURL/tsetup.$VERSION.beta.tar.xz"
+    #fi
 fi
-
-
-PKGURL=$(eget --list --latest https://github.com/telegramdesktop/tdesktop/releases "tsetup.$VERSION.tar.xz") #"
 
 # override PKGNAME for beta version
 echo "$PKGURL" | grep -q "beta.tar.xz" && override_pkgname "$BASEPKGNAME-beta"
