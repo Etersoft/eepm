b9a7c9ba37c1b03a10f7cd6037bfad1191ccc55c
Vitaly Lipatov <lav@etersoft.ru>
2024-04-03 00:28:45 +0300

    epm pack 64Gram: move icons/desktop from repack rules

--- pack.d/64Gram.sh
+++ pack.d/64Gram.sh
@@ -16,7 +16,32 @@ f=$FPRODUCT
 [ -f "$(echo */$FPRODUCT)" ] && f="$(echo */$FPRODUCT)"
 
 install -D -m755 $f opt/$PRODUCT/$PRODUCT || fatal
-erc pack $PKGNAME.tar opt/$PRODUCT
+
+# Icons
+iconname=$PRODUCT
+url=https://github.com/TDesktop-x64/tdesktop
+for i in 16 32 48 64 128 256 512 ; do
+    install_file $url/raw/master/Telegram/Resources/art/icon$i.png /usr/share/icons/hicolor/${i}x${i}/apps/$iconname.png
+done
+
+
+cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
+[Desktop Entry]
+Version=1.0
+Name=64Gram
+Comment=64Gram (unofficial Telegram Desktop)
+Exec=$PRODUCT -- %u
+Icon=$iconname
+StartupWMClass=64Gram
+Type=Application
+Categories=Chat;Network;InstantMessaging;Qt;
+MimeType=x-scheme-handler/tg;
+Keywords=tg;chat;im;messaging;messenger;sms;tdesktop;
+X-GNOME-UsesNotifications=true
+EOF
+
+
+erc pack $PKGNAME.tar opt/$PRODUCT usr
 
 cat <<EOF >$PKGNAME.tar.eepm.yaml
 name: $PRODUCT
