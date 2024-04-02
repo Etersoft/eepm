e646575dea36fe62d0ddfacacc88d9081c862c70
Vitaly Lipatov <lav@etersoft.ru>
2024-04-02 05:17:08 +0300

    epm repack tradingview: add missed --no-sandbox arg

--- repack.d/tradingview.sh
+++ repack.d/tradingview.sh
@@ -6,16 +6,19 @@ SPEC="$2"
 
 . $(dirname $0)/common.sh
 
-add_bin_link_command
+add_bin_exec_command
 
+# as in upstream's command.sh
+subst 's|"$@"|--no-sandbox "$@"|' usr/bin/$PRODUCT
 
 cd .$PRODUCTDIR || fatal
-
 
 for i in etc meta snap lib/dri usr/bin usr/lib usr/include usr/share/X11 usr/share/misc usr/share/doc usr/share/fonts ; do
     remove_dir $PRODUCTDIR/$i
 done
 
 cd >/dev/null
+
+#fix_chrome_sandbox
 
 add_libs_requires
