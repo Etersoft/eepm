8234594b5fbd667379abd1fc74b5f942ea777f54
Владислав Бабенко <65094576+vlad196@users.noreply.github.com>
2024-04-03 13:48:50 +0200

    Перевёл выводы строки на английский в switch-to-nvidia.sh

--- prescription.d/switch-to-nvidia.sh
+++ prescription.d/switch-to-nvidia.sh
@@ -4,7 +4,7 @@
 # https://www.altlinux.org/Nvidia#Смена_открытых_драйверов_на_проприетарные[1]
 # https://www.altlinux.org/Переход_на_драйверы_Nvidia_и_fglrx#Установка_проприетарных_драйверов_nvidia_и_fglrx_:
 
-[ "$1" != "--run" ] && echo "Переход на использование проприетарных драйверов nvidia" && exit
+[ "$1" != "--run" ] && echo "Switch to using NVidia proprietary driver" && exit
 
 . $(dirname $0)/common.sh
 
@@ -22,7 +22,7 @@ epm assure lspci pciutils || exit
 # проверяем работоспособность драйвера на текущий момент
 # TODO: добавить аргумент --force для принудительной переустановки
 if [ -z "$force" ] && a= lspci -k | grep -A 2 -i "VGA" | grep "Kernel driver in use" | grep -q "nvidia" ; then
-	echo "Драйвера nvidia уже установлены и запущены."
+	echo "Nvidia driver is already installed and used"
 	exit
 fi
 
@@ -40,10 +40,10 @@ check_run_kernel () {
 }
 
 if check_run_kernel ; then
-	echo "Запущено самое свежее установленное ${USED_KFLAVOUR} ядро."
+	echo "The most recently installed ${USED_KFLAVOUR} kernel is running."
 else
-	echo "В системе есть ${USED_KFLAVOUR} ядро свежее запущенного."
-	echo "Перезагрузитесь со свежим ${USED_KFLAVOUR} ядром и перезапустите: epm play switch-to-nvidia"
+	echo "The system has a ${USED_KFLAVOUR} kernel that is more recent than the one that was launched."
+	echo "Reboot with a fresh ${USED_KFLAVOUR} kernel and restart: epm play switch-to-nvidia"
 	fatal
 fi
 
@@ -70,19 +70,19 @@ fi
 # Создаем список дополнительных пакетов и их описания. Все новые пакеты добавлять в этот список.
 declare -A packages
 packages=(
-    ["nvidia-settings"]=" nvidia-settings — это инструмент для настройки видеокарт NVIDIA."
-    ["nvidia-vaapi-driver"]="nvidia-vaapi-driver — это драйвер для аппаратного декодирования видео на видеокартах NVIDIA."
-    ["vulkan-tools"]="vulkan-tools — это набор инструментов для работы с Vulkan API."
-    ["nvidia-modprobe"]="nvidia-modprobe — это утилита для загрузки модулей ядра NVIDIA."
-    ["nvidia-xconfig"]="nvidia-xconfig — это инструмент для управления конфигурацией X server для видеокарт NVIDIA."
-    ["libvulkan1"]="libvulkan1 — это библиотека, которая предоставляет API Vulkan 1.x."
+    ["nvidia-settings"]=" nvidia-settings — this is a tool for configuring the NVIDIA driver."
+    ["nvidia-vaapi-driver"]="nvidia-vaapi-driver — this is a VA-API implementation that uses NVDEC as a backend"
+    ["vulkan-tools"]="vulkan-tools is a set of tools for working with the Vulkan API."
+    ["nvidia-modprobe"]="nvidia-modprobe — this is a elper to load kernel module and create device files."
+    ["nvidia-xconfig"]="nvidia-xconfig — this is a ommand line tool for setup X11 for the NVIDIA driver"
+    ["libvulkan1"]="libvulkan1 — these are the Vulkan loader libraries."
 )
 install_list=()
 
 # Для каждого пакета спрашиваем пользователя, хочет ли он его установить
 for package in "${!packages[@]}"; do
     echo "${packages[$package]}"
-    read -p "Установить $package? [Y/n] " answer
+    read -p "Install $package? [Y/n] " answer
     answer=${answer,,}  # Преобразуем ответ в нижний регистр
 
     # Если ответ 'y' или пустой (вариант по умолчанию), то добавляем пакет в список для установки. В противном случае пропускаем.
@@ -95,7 +95,7 @@ done
 
 # Отдельно спрашиваем про пакеты для поддержки Cuda. Аналогично установке пакетов nvidia_glx_libs_XXX.XX.
 #TODO необходимо разобраться с групами пакетов. Если Rosa и другие ветки репозиториев Alt позволяют, то придумать как применить nvidia_glx_libs_XXX.XX нужной версии.
-read -p "Вы хотите установить дополнительные пакеты для поддержки Cuda? [Y/n] " answer
+read -p "Do you want to install additional packages to support Cuda? [Y/n] " answer
 
 answer=${answer,,}
 
@@ -108,29 +108,29 @@ fi
 
 # Если список для установки не пуст, то устанавливаем пакеты
 if [ ${#install_list[@]} -ne 0 ]; then
-    echo "Устанавливаем пакеты: ${install_list[@]}"
+    echo "Installing packages: ${install_list[@]}"
     epm install --skip-installed ${install_list[@]}
 else
-    echo "Вы не выбрали ни одного дополнительного пакета для установки."
+    echo "You have not selected any additional packages to install."
 fi
 
 epm play i586-fix
 
 # У этого фикса есть плюсы и минусы. Лучше предложить его применение на этапе установки.
-echo "FIX: убираем «Неизвестный монитор» в настройках дисплеев в сессии Wayland"
-echo "Существует проблема, когда у некоторых пользователей появляется дополнительный «Неизвестный монитор»."
-echo  "Внимание! Данное решение приводит к невозможности входа в tty, к отсутствию вывода логов во время загрузки (Если не включён Plymouth)."
+echo "FIX: removing the "Unknown monitor" in the display settings in the Wayland session"
+echo "There is a problem when some users have an additional "Unknown Monitor"."
+echo  "Attention! This solution leads to the inability to log in to tty, to the absence of log output during boot (If Plymouth is not enabled)."
 
-read -p "Вы хотите применить FIX? [Y/n] " answer
+read -p "Do you want to apply FIX? [Y/n] " answer
 
 answer=${answer,,}
 
 if [[ $answer == 'y' || $answer == '' ]]; then
 	if ! grep "initcall_blacklist=simpledrm_platform_driver_init" /etc/sysconfig/grub2 &>/dev/null ; then 
-		echo "Создание копии /etc/sysconfig/grub2..."
+		echo "Creating a copy of /etc/sysconfig/grub2..."
 		cp /etc/sysconfig/grub2 /etc/sysconfig/grub2.epmbak
 
-		echo "Добавление initcall_blacklist=simpledrm_platform_driver_init в параметры ядра..."
+		echo "Adding initcall_blacklist=simpledrm_platform_driver_init to the kernel parameters..."
 		sed -i "s|^\(GRUB_CMDLINE_LINUX_DEFAULT='.*\)'\$|\1 initcall_blacklist=simpledrm_platform_driver_init'|" /etc/sysconfig/grub2
 
 		echo "FIX применён."
@@ -140,7 +140,7 @@ else
 fi
 
 # Без этих интерфейсов будет некоректно работать уход в сон
-echo "Активируем интерфесы питания NVIDIA."
+echo "Activating nvidia power management interfaces."
 systemctl enable nvidia-suspend.service nvidia-resume.service nvidia-hibernate.service
 # Добавляем выделение места в системе для видеопамяти. Иначе интерфейсы не будут работать.
 cat << _EOF_ > /etc/modprobe.d/nvidia_videomemory_allocation.conf
@@ -148,11 +148,11 @@ options nvidia NVreg_PreserveVideoMemoryAllocations=1
 options nvidia NVreg_TemporaryFilePath=/run
 _EOF_
 
-echo "Запускаем регенерацию initrd."
+echo "Starting the initrd."
 make-initrd
 
-echo "Обновляем grub..."
+echo "Updating grub..."
 update-grub
 
-echo "Выполнено. Перезагрузите систему для использования проприетарных драйверов nvidia."
+echo "Done. Just you need reboot your system to use nVidia proprietary drivers."
 
