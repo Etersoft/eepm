3e917e6a129150f08bd6c3a3d8cfc13b51412036
Ivan Mazhukin <vanomj@etersoft.ru>
2024-04-06 22:25:34 +0300

    epm pack, repack: add Davinci-resolve

--- pack.d/davinci-resolve.sh
+++ pack.d/davinci-resolve.sh
@@ -8,19 +8,15 @@ PRODUCT=davinci-resolve
 
 # DaVinci_Resolve_18.6.5_Linux.run
 BASENAME=$(basename $1 .run)
-VERSION=$(echo $BASENAME | sed -e 's|DaVinci_Resolve_||' | sed -e 's|_Linux||')
+VERSION=$(echo $BASENAME | sed -e 's|DaVinci_Resolve_||' -e 's|_Linux||')
 mkdir -p opt/davinci-resolve
 
 $1 --appimage-extract &> /dev/null
 
 mv -v squashfs-root/* opt/davinci-resolve/
 
-# fix for libpango.so error
-# mv opt/davinci-resolve/libs/libc++.so.1 opt/davinci-resolve/libs/libc++.so.1.orig
-# mv opt/davinci-resolve/libs/libglib-2.0.so.0 opt/davinci-resolve/libs/libglib-2.0.so.0.orig
-
 install -Dm0644 opt/davinci-resolve/share/default-config.dat -t opt/$PRODUCT/configs
-install -Dm0644 opt/davinci-resolve/share/log-conf.xml -t opt/$PRODUCT}/configs
+install -Dm0644 opt/davinci-resolve/share/log-conf.xml -t opt/$PRODUCT/configs
 install -Dm0644 opt/davinci-resolve/share/default_cm_config.bin -t opt/$PRODUCT/DolbyVision
 
 install -Dm0644 opt/davinci-resolve/share/*.desktop -t usr/share/applications
@@ -34,7 +30,13 @@ install -Dm0644 opt/davinci-resolve/share/etc/udev/rules.d/99-BlackmagicDevices.
 install -Dm0644 opt/davinci-resolve/share/etc/udev/rules.d/99-ResolveKeyboardHID.rules -t usr/lib/udev/rules.d
 # install -Dm0644 opt/davinci-resolve/share/etc/udev/rules.d/99-DavinciPanel.rules -t usr/lib/udev/rules.d
 
+echo "StartupWMClass=resolve" >> usr/share/DaVinciResolve.desktop
+
+subst "s|RESOLVE_INSTALL_LOCATION|/opt/davinci-resolve|" usr/share/applications/*.desktop 
+subst "s|RESOLVE_INSTALL_LOCATION|/opt/davinci-resolve|" usr/share/desktop-directories/*
 # fix for libpango.so error too
+
+rm -v opt/davinci-resolve/libs/libglib-2.0.so.0
 # ln -s /usr/lib/libglib-2.0.so.0 opt/davinci-resolve/libs/libglib-2.0.so.0
 
 PKGNAME=$PRODUCT-$VERSION
