8007f255b9db64760206f63088d6c58a10a03092
Boria138 <boriabloger@protonmail.com>
2024-04-08 18:27:04 +0500

    epm play: added penpot-desktop

---
+++ play.d/penpot-desktop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PKGNAME=penpot-desktop
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Penpot Desktop from the snapcraft"
+URL="https://snapcraft.io/penpot-desktop"
+
+. $(dirname $0)/common.sh
+
+PKGURL="$(snap_get_pkgurl $PKGNAME)"
+install_pkgurl
