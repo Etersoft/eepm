129dd8df3b88edac2a547d87f9dfbd7d594624de
Boria138 <boriabloger@protonmail.com>
2024-04-08 18:27:04 +0500

    epm play: added penpot-desktop

---
+++ repack.d/penpot-desktop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+
+SPEC="$2"
+
+. $(dirname $0)/common.sh
+
+add_bin_exec_command
+
+cd .$PRODUCTDIR || fatal
+
+for i in data-dir gnome-platform lib meta scripts usr ; do
+    remove_dir $PRODUCTDIR/$i
+done
+
+for i in *.sh ; do
+    remove_file $PRODUCTDIR/$i
+done
+
+cd >/dev/null
+
+add_libs_requires
+
+add_bin_link_command
