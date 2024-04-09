36a54de0dcd670696290f570db0f68d987e79cab
Boria138 <boriabloger@protonmail.com>
2024-04-09 19:51:55 +0500

    epm play: use universal script for repaking snaps

--- repack.d/plex-desktop.sh
+++ repack.d/plex-desktop.sh
@@ -8,21 +8,4 @@ SPEC="$2"
 
 add_bin_link_command $PRODUCT $PRODUCTDIR/Plex.sh
 
-
-cd .$PRODUCTDIR || fatal
-
-for i in usr/lib/x86_64-linux-gnu/libwebp* ; do
-    move_file $PRODUCTDIR/$i $PRODUCTDIR/lib/$(basename $i)
-done
-
-for i in etc meta snap lib/dri usr/bin usr/lib usr/include usr/share/X11 usr/share/misc usr/share/doc usr/share/fonts ; do
-    remove_dir $PRODUCTDIR/$i
-done
-
-for i in libEGL.so.1 libdrm.so.2 libdrm_amdgpu.so.1 libva-drm.so.2 libva-x11.so.2 libva.so.2 ; do
-    remove_file $PRODUCTDIR/lib/$i
-done
-
-cd >/dev/null
-
 add_libs_requires
