fb9cddd490fc797850b98d12d80700b70baca6bc
Boria138 <boriabloger@protonmail.com>
2024-04-11 17:04:22 +0500

    epm play: install xdg-desktop-portal backends for flatpak

--- prescription.d/flatpak.sh
+++ prescription.d/flatpak.sh
@@ -11,10 +11,32 @@ assure_root
 # Мсправляет ошибку "enabling unprivileged user namespaces" без перезагрузки
 a= sysctl -w kernel.unprivileged_userns_clone=1
 
-epm install flatpak flatpak-repo-flathub sysctl-conf-userns
+epm install --skip-installed flatpak flatpak-repo-flathub sysctl-conf-userns
 
 if epm installed plasma5-discover ; then
-    epm install plasma5-discover-flatpak
+    epm install --skip-installed plasma5-discover-flatpak
 fi
 
-echo "Flatpak successfully installed, but epm play is the preferred way to install the software."
+if epm installed plasma5-kwin ; then
+    epm install --skip-installed plasma5-xdg-desktop-portal-kde xdg-desktop-portal
+elif epm installed deepin-kwin2 ; then
+    epm install --skip-installed xdg-desktop-portal-dde xdg-desktop-portal
+elif epm installed gnome-shell ; then
+    epm install --skip-installed xdg-desktop-portal-gnome xdg-desktop-portal
+elif epm installed xfwm4 ; then
+    epm install --skip-installed xdg-desktop-portal-gtk xdg-desktop-portal
+elif epm installed mate-window-manager ; then
+    epm install --skip-installed xdg-desktop-portal-gtk xdg-desktop-portal
+elif epm installed sway ; then
+    epm install --skip-installed xdg-desktop-portal-wlr xdg-desktop-portal
+elif epm installed hyprland ; then
+    epm install --skip-installed xdg-desktop-portal-hyprland xdg-desktop-portal
+elif epm installed muffin ; then
+    epm install --skip-installed xdg-desktop-portal-xapp xdg-desktop-portal
+elif epm installed liblxqt ; then
+    epm install --skip-installed xdg-desktop-portal-lxqt xdg-desktop-portal
+elif epm installed lxde-common ; then
+    epm install --skip-installed xdg-desktop-portal-gtk xdg-desktop-portal
+fi
+
+echo "Flatpak successfully installed, but you need to reboot for xdg-desktop-portals to work"
