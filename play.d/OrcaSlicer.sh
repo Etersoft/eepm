6d99258ada989002e5937ee30c44f11ffe95e81e
Ivan Mazhukin <vanomj@etersoft.ru>
2024-04-16 22:41:36 +0300

    epm play: fix OrcaSlicer version download

--- play.d/OrcaSlicer.sh
+++ play.d/OrcaSlicer.sh
@@ -8,7 +8,8 @@ URL="https://github.com/SoftFever/OrcaSlicer"
 
 . $(dirname $0)/common.sh
 
-PKGURL="$(eget --list --latest "https://github.com/SoftFever/OrcaSlicer/releases/" "OrcaSlicer${VERSION}.AppImage")"
+[ "$VERSION" = "*" ] && VERSION="[0-9]*[0-9]"
+PKGURL="$(eget --list --latest "https://github.com/SoftFever/OrcaSlicer/releases/" "OrcaSlicer_Linux_V${VERSION}.AppImage")"
 
 install_pack_pkgurl
 
