82af859e1a5b79c3aef768e3dc29dc30903efa5e
Boria138 <boriabloger@protonmail.com>
2024-04-18 18:19:29 +0500

    epm play: install xdg-desktop-portal backends for flatpak (eterbug #17245)

--- prescription.d/flatpak.sh
+++ prescription.d/flatpak.sh
@@ -11,10 +11,34 @@ assure_root
 # Мсправляет ошибку "enabling unprivileged user namespaces" без перезагрузки
 a= sysctl -w kernel.unprivileged_userns_clone=1
 
+install_portals=""
+
+while read -r window_manager package ; do
+    if epm installed $window_manager </dev/null ; then
+        install_portals+=" $package"
+    fi
+done <<EOF
+    plasma5-kwin        plasma5-xdg-desktop-portal-kde
+    deepin-kwin2        xdg-desktop-portal-dde
+    gnome-shell         xdg-desktop-portal-gnome
+    xfwm4               xdg-desktop-portal-gtk
+    mate-window-manager xdg-desktop-portal-gtk
+    sway                xdg-desktop-portal-wlr
+    hyprland            xdg-desktop-portal-hyprland
+    muffin              xdg-desktop-portal-xapp
+    liblxqt             xdg-desktop-portal-lxqt
+    lxde-common         xdg-desktop-portal-gtk
+EOF
+
+epm install --skip-installed $install_portals xdg-desktop-portal
+
 epm install flatpak flatpak-repo-flathub sysctl-conf-userns
 
 if epm installed plasma5-discover ; then
     epm install plasma5-discover-flatpak
 fi
+
+# Без перезагрузки dbus, порталы не заработают
+a= systemctl try-reload-or-restart dbus
 
 echo "Flatpak successfully installed, but epm play is the preferred way to install the software."
