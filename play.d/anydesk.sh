fdeeda08f79adc667398ed684f499334e0d15379
Vitaly Lipatov <lav@etersoft.ru>
2024-04-26 01:56:28 +0300

    epm play anydesk: update for new upstream package placement

--- play.d/anydesk.sh
+++ play.d/anydesk.sh
@@ -1,44 +1,30 @@
 #!/bin/sh
 
 PKGNAME=anydesk
-SUPPORTEDARCHES="x86_64 x86"
+SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION="AnyDesk from the official site"
 
 . $(dirname $0)/common.sh
 
-arch=$(epm print info --distro-arch)
-case $arch in
-    x86_64|amd64)
-        arch=$arch ;;
-    i686|i386)
-        arch=$arch ;;
-    i586)
-        arch=i686 ;;
-    *)
-        fatal "Unsupported arch $arch for $(epm print info -d)"
-esac
-
-# rpm packages have a release in their names
-[ "$(epm print info -p)" = "rpm" ] && [ "$VERSION" != "*" ] && VERSION="$VERSION-1"
-
+warn_version_is_not_supported
 
 # current links:
-# https://download.anydesk.com/rpm/anydesk_6.0.1-1_x86_64.rpm
-# https://download.anydesk.com/os-specific/rhel8/anydesk-6.0.1-1.el8.x86_64.rpm
-# https://download.anydesk.com/deb/anydesk_6.0.1-1_amd64.deb
+# https://download.anydesk.com/linux/anydesk_6.3.2-1_x86_64.rpm
+# https://download.anydesk.com/linux/anydesk-6.3.2-1.el8.x86_64.rpm
+# https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb
 
-PKGMASK="$(epm print constructname $PKGNAME "$VERSION" $arch '' '_')"
+[ "$VERSION" = "*" ] || VERSION="$VERSION-1"
 
-# we miss obsoleted libpangox on ALT, so use RHEL8 build
-# lib.req: WARNING: /usr/bin/anydesk: library libpangox-1.0.so.0 not found
-#[ "$(epm print info -s)" = "alt" ] && PKGMASK="os-specific/rhel8/$(epm print constructname $PKGNAME "*" $arch)"
+#PKGMASK="$(epm print constructname $PKGNAME "$VERSION" '' '' '_')"
+
+#[ "$(epm print info -s)" = "alt" ] && 
+# use el8 build for all systems
+PKGMASK="$(epm print constructname $PKGNAME "$VERSION.el8")"
 
 if [ "$VERSION" = "*" ] ; then
-    PKGURL="$(eget --list --latest https://download.anydesk.com/linux/ "./$PKGMASK")"
+    PKGURL="$(eget --list --latest https://download.anydesk.com/linux/$PKGMASK)"
 else
-    # https://download.anydesk.com/linux/anydesk_6.3.0-1_amd64.deb
-    # https://download.anydesk.com/linux/anydesk_6.3.0-1_x86_64.rpm
     PKGURL="https://download.anydesk.com/linux/$PKGMASK"
 fi
 
--- repack.d/anydesk.sh
+++ repack.d/anydesk.sh
@@ -11,8 +11,10 @@ PRODUCT=anydesk
 remove_file /usr/share/anydesk/files/init/anydesk
 
 # put service file to the normal place
-install_file usr/share/anydesk/files/systemd/anydesk.service /etc/systemd/system/anydesk.service
-remove_file /usr/share/anydesk/files/systemd/anydesk.service
+if [ -f usr/share/anydesk/files/systemd/anydesk.service ] ; then
+    install_file usr/share/anydesk/files/systemd/anydesk.service /etc/systemd/system/anydesk.service
+    remove_file /usr/share/anydesk/files/systemd/anydesk.service
+fi
 
 fix_desktop_file /usr/bin/$PRODUCT
 
