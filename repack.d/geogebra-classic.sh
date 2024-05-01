2c965d4d92e7212bed46fb18a196da850c3524ae
Vitaly Lipatov <lav@etersoft.ru>
2024-05-01 16:09:46 +0300

    epm repack geogebra-classic: skip using ALT only macro when eepm-rpm-rebuild is used

--- repack.d/geogebra-classic.sh
+++ repack.d/geogebra-classic.sh
@@ -11,10 +11,13 @@ PREINSTALL_PACKAGES="python3 rpm-build-python3"
 
 . $(dirname $0)/common-chromium-browser.sh
 
-subst '1iBuildRequires:rpm-build-python3' $SPEC
-subst "1i%add_python3_path $PRODUCTDIR" $SPEC
-
 move_to_opt
+
+subst '1iBuildRequires:rpm-build-python3' $SPEC
+if [ -z "$EPM_RPMBUILD" ] ; then
+    subst "1i%add_python3_path $PRODUCTDIR" $SPEC
+fi
+
 subst "s|/usr/share/$PRODUCT|$PRODUCTDIR|" $BUILDROOT/usr/bin/$PRODUCT
 
 cleanup
