7dcf159bbbef65b16b2b7c4abebf47f2d771cc13
Vitaly Lipatov <lav@etersoft.ru>
2024-05-06 02:13:11 +0300

    epm repack freeplane: fix path

--- repack.d/freeplane.sh
+++ repack.d/freeplane.sh
@@ -6,8 +6,10 @@ SPEC="$2"
 . $(dirname $0)/common.sh
 
 move_to_opt
+
 ln -sf $PRODUCTDIR/$PRODUCT.sh usr/bin/$PRODUCT
-fix_desktop_file "/usr/bin/freeplane/$PRODUCT"
+
+fix_desktop_file "/usr/bin/$PRODUCT"
 
 remove_dir /usr/lib/mime
 
