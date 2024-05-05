5ddef69aa73941c87fdcc978b5a9c3da5c393031
Vitaly Lipatov <lav@etersoft.ru>
2024-05-06 02:12:25 +0300

    epm generic-default: improve link creation, desktop fixing, chrome-sandbox detection

--- repack.d/generic-default.sh
+++ repack.d/generic-default.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-# Default repack script (used if a special script for target product is mieed)
+# Default repack script (used if a special script for target product is missed)
 
 # It will run with two args: buildroot spec
 BUILDROOT="$1"
@@ -13,12 +13,26 @@ PKG="$4"
 # detect requires by libs
 add_libs_requires
 
-if [ -f "$BUILDROOT/$PRODUCTDIR/$PRODUCT" ] ; then
+# FIXME: hack for nonstandart name
+pd="$(echo $BUILDROOT/opt/*)"
+[ -d "$pd" ] && PRODUCTDIR="/opt/$(basename "$pd")"
+
+if [ -f "$BUILDROOT$PRODUCTDIR/$PRODUCT" ] ; then
     add_bin_exec_command
 fi
 
+if [ -f $BUILDROOT/usr/share/applications/*.desktop ] ; then
+    EXEC="$(cat $BUILDROOT/usr/share/applications/*.desktop | grep "^Exec=" | head -n1 | sed -e 's|Exec=||' -e 's| .*||')"
+    if [ "/usr/bin/$(basename "$EXEC")" = "/usr/bin/$PRODUCT" ] || [ "$EXEC" = "$PRODUCTDIR/$PRODUCT" ] ; then
+        if [ -x $BUILDROOT/usr/bin/$PRODUCT ] ; then
+            fix_desktop_file "$EXEC"
+        fi
+    fi
+fi
+
 # TODO: add product dir detection
-if [ -f $PRODUCTDIR/v8_context_snapshot.bin ] ; then
+if [ -f "$BUILDROOT$PRODUCTDIR/v8_context_snapshot.bin" ] ; then
     echo "electron based application detected, adding requires for it ..."
     add_electron_deps
+    fix_chrome_sandbox
 fi
