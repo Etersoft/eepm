3f6a603526ad814fb91a9141f8699805c27261fb
Ivan Mazhukin <vanomj@etersoft.ru>
2024-05-06 17:59:04 +0300

    epm play: fix far2l-portable install(eterbug #17322)

--- pack.d/far2l-portable.sh
+++ pack.d/far2l-portable.sh
@@ -7,7 +7,7 @@ RETURNTARNAME="$2"
 . $(dirname $0)/common.sh
 
 erc $TAR || fatal
-pkg="$(ls | grep -e "far2l_[0-9].*_x86_64.*.AppImage")"
+pkg="$(ls | grep -e "far2l_[0-9].*x86_64.*.AppImage")"
 PKGNAME="${pkg/far2l/far2l-portable}"
 mv $pkg $PKGNAME
 return_tar "$PKGNAME"
