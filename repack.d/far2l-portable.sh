72246f11e9ddae3f020521c5964344b4eacfdef8
Ivan Mazhukin <vanomj@etersoft.ru>
2024-05-06 20:21:41 +0300

    epm repack far2l-portable: fix desktop file (eterbug #17322)

--- repack.d/far2l-portable.sh
+++ repack.d/far2l-portable.sh
@@ -12,5 +12,6 @@ remove_file /opt/far2l-portable/usr/lib/x86_64-linux-gnu/samba/libsamba-net.cpyt
 remove_file /opt/far2l-portable/usr/lib/x86_64-linux-gnu/libsamba-policy.cpython-310-x86-64-linux-gnu.so.0.0.1
 
 add_conflicts far2l
+fix_desktop_file usr/bin/far2l $PRODUCT
 
 add_libs_requires
