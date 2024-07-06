13cfc2cf2895fbecfffc7527aba62bf7734cbb40
Boris Yumankulov <boria138@altlinux.org>
2024-07-06 19:54:20 +0500

    epm play nwjs-ffmpeg-prebuilt: add version support (eterbug #17494)

--- play.d/nwjs-ffmpeg-prebuilt.sh
+++ play.d/nwjs-ffmpeg-prebuilt.sh
@@ -8,7 +8,10 @@ URL="https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/"
 
 . $(dirname $0)/common.sh
 
-# https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/download/0.85.0/0.85.0-linux-x64.zip
-PKGURL=$(eget --list --latest https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases "${VERSION}-linux-x64.zip")
+if [ "$VERSION" = "*" ] ; then
+    PKGURL=$(eget --list --latest https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases "${VERSION}-linux-x64.zip")
+else
+    PKGURL="https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/download/$VERSION/$VERSION-linux-x64.zip"
+fi
 
 install_pack_pkgurl
