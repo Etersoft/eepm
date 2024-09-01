a8d2bf59f654ac032a42800171d366d0b581b7b8
Valentin Pechenov <pe4enov.valentin@yandex.ru>
2024-09-01 12:15:40 +0300

    epm play: nekoray: fix version troubles
    Изменения в play: исправлено скачивание beta версии, вместо стабильной
    Изменения в repack: добавлена поддержка версии 4.0, где в некоторых
    местах nekoray поменялся на nekobox (спасибо им за головную боль)

--- repack.d/nekoray.sh
+++ repack.d/nekoray.sh
@@ -3,8 +3,12 @@
 # It will be run with two args: buildroot spec
 BUILDROOT="$1"
 SPEC="$2"
-
-PRODUCT=nekoray
+if [ -f $BUILDROOT/opt/nekoray/nekoray ]
+then
+	PRODUCT=nekoray
+else
+	PRODUCT=nekobox
+fi
 PRODUCTDIR=/opt/nekoray
 
 . $(dirname $0)/common.sh
@@ -18,7 +22,7 @@ cat <<EOF |create_file /usr/share/applications/$PRODUCT.desktop
 Name=Nekoray
 Comment=Qt based cross-platform GUI proxy configuration manager (backend: Xray / sing-box)
 Exec=$PRODUCT -- %u -appdata
-Icon=/opt/nekoray/nekoray.png
+Icon=/opt/$PRODUCT/$PRODUCT.png
 Type=Application
 Terminal=false
 Categories=Network;
