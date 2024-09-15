55607ae1d3d509f4381a5e7bef7786d68a20c42d
Ivan Mazhukin <vanomj@altlinux.org>
2024-09-15 14:31:26 +0300

    epm play: add freetube (eterbug #17696)

---
+++ repack.d/freetube.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCTDIR=/opt/FreeTube
+
+. $(dirname $0)/common.sh
+
+remove_dir /usr/share/doc
+
+add_bin_exec_command $PRODUCT
+
+fix_chrome_sandbox
+
+ignore_lib_requires 'libffmpeg.so()(64bit)'
+
+add_libs_requires
