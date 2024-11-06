a8a30612135bc99ed5dd5b2a02e092d5c3775776
Boris Yumankulov <boria138@altlinux.org>
2024-11-06 16:06:20 +0500

    epm play teamspeak3: parse version from vendor (eterbug #17822)

--- play.d/teamspeak3.sh
+++ play.d/teamspeak3.sh
@@ -8,8 +8,9 @@ URL="https://www.teamspeak.com/"
 
 . $(dirname $0)/common.sh
 
-# TODO: check latest version here: https://www.teamspeak.com/en/downloads/#ts3client
-[ "$VERSION" = "*" ] && VERSION=3.6.1
+if [ "$VERSION" = "*" ] ; then
+    VERSION=$(eget -O- https://teamspeak.com/en/downloads/#ts3client | grep -oP 'https://files\.teamspeak-services\.com/releases/client/3\.\d+\.\d+/TeamSpeak3-Client-linux_amd64-3\.(\d+\.\d+)\.run' | head -n 1 | grep -oP '3\.(\d+\.\d+)' | head -n 1)
+fi
 
 PKGURL="https://files.teamspeak-services.com/releases/client/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run"
 
