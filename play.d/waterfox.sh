a97c0cabac166e515a9b607e69f48c8818dccd52
Boris Yumankulov <boria138@altlinux.org>
2024-11-11 00:10:50 +0500

    epm play waterfox: fix PKGURL and VERSION (eterbug #17830)

--- play.d/waterfox.sh
+++ play.d/waterfox.sh
@@ -8,9 +8,9 @@ URL="https://www.waterfox.net/"
 . $(dirname $0)/common.sh
 
 if [ "$VERSION" = "*" ] ; then
-	VERSION="$(curl -s https://api.github.com/repos/BrowserWorks/Waterfox/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")' | sed 's/G//g')"
+	VERSION="$(get_github_tag https://github.com/BrowserWorks/Waterfox/)"
 fi
 
-PKGURL="https://cdn1.waterfox.net/waterfox/releases/G$VERSION/Linux_x86_64/waterfox-G$VERSION.tar.bz2"
+PKGURL="https://cdn1.waterfox.net/waterfox/releases/$VERSION/Linux_x86_64/waterfox-$VERSION.tar.bz2"
 
 install_pack_pkgurl $VERSION
