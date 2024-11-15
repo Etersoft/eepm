296f25f3ba1cdcc73238034091bdb489a3a32f14
Boris Yumankulov <boria138@altlinux.org>
2024-11-15 15:13:06 +0500

    epm: play added svp4 (eterbug #17842)

---
+++ repack.d/svp4.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCTDIR=/opt/svp4
+
+. $(dirname $0)/common.sh
+
+
+# pack icons
+for i in  32 48 64 128 ; do
+    [ -r $BUILDROOT/$PRODUCTDIR/svp-manager4-$i.png ] || continue
+    mkdir -p $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/
+    cp $BUILDROOT/$PRODUCTDIR/svp-manager4-$i.png $BUILDROOT/usr/share/icons/hicolor/${i}x${i}/apps/svp-manager4.png
+done
+subst "s|%files|%files\n/usr/share/icons/hicolor/*x*/apps/svp-manager4.png|" $SPEC
+
+cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=SVP 4 Linux
+GenericName=Real time frame interpolation
+Type=Application
+Categories=Multimedia;AudioVideo;Player;Video;
+MimeType=video/x-msvideo;video/x-matroska;video/webm;video/mpeg;video/mp4;
+Terminal=false
+StartupNotify=true
+Exec=$PRODUCT %f
+Icon=svp-manager4.png
+EOF
+
+add_requires mpv libmediainfo python3-module-vapoursynth libqt5-concurrent
+add_libs_requires
+add_bin_link_command $PRODUCT $PRODUCTDIR/SVPManager
