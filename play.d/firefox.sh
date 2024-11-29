ae610f8dc23cbf83dda2a393665d80476d5e2128
Boris Yumankulov <boria138@altlinux.org>
2024-11-29 10:49:29 +0500

    epm play firefox: fix PKGURL for xz archive format (eterbug #17838)

--- play.d/firefox.sh
+++ play.d/firefox.sh
@@ -7,10 +7,13 @@ URL="https://www.mozilla.org/en-US/firefox"
 
 . $(dirname $0)/common.sh
 
-if [ "$VERSION" = "*" ] ; then
-	VERSION="$(eget -O- https://www.mozilla.org/en-US/firefox/releases/ | grep -oP '\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1)"
+if [ "$VERSION" = "*" ]; then
+    VERSION="$(eget -O- https://www.mozilla.org/en-US/firefox/releases/ | \
+        grep -oP '[0-9]+\.[0-9]+(\.[0-9]+)?(?=/releasenotes/)' | \
+        sort -V | tail -n1)"
 fi
 
-PKGURL="https://ftp.mozilla.org/pub/firefox/releases/$VERSION/linux-x86_64/en-US/firefox-$VERSION.tar.bz2"
+# --second-latest because firefox-133.0.tar.bz2.asc
+PKGURL="$(eget --list --second-latest https://ftp.mozilla.org/pub/firefox/releases/$VERSION/linux-x86_64/en-US/ "firefox-$VERSION.tar.*")"
 
 install_pack_pkgurl $VERSION
