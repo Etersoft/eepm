175e510aaa25d3e974c3604535b9269f391430b1
Ivan Mazhukin <vanomj@altlinux.org>
2024-12-09 14:14:33 +0300

    epm play yucca: fix getting version (eterbug #17865)

--- play.d/yucca.sh
+++ play.d/yucca.sh
@@ -20,7 +20,7 @@ case $(epm print info -a) in
 esac
 
 if [ "$VERSION" = "*" ] ; then
-    VERSION=$(eget --list --latest "https://docs.yucca.app/releases/" | grep -oP 'v\K[0-9]+\.[0-9]+\.[0-9]+')
+    VERSION=$(eget -O- https://releases.yucca.app/latest/VERSION.txt)
 fi
 
 PKGURL="https://releases.yucca.app/v${VERSION}/yucca_${VERSION}_linux_${arch}.tar.gz"
