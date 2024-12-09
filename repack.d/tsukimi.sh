1ce5d6349f4fb48a194e1c589f3b231575b87f84
Ivan Mazhukin <vanomj@altlinux.org>
2024-12-09 13:46:47 +0300

    epm repack tsukimi: fix png, xml download link  (eterbug #17865)

--- repack.d/tsukimi.sh
+++ repack.d/tsukimi.sh
@@ -10,8 +10,8 @@ add_bin_link_command $PRODUCT $PRODUCTDIR/$PRODUCT
 
 add_requires "/usr/bin/clapper"
 
-install_file https://raw.githubusercontent.com/tsukinaha/tsukimi/main/resources/ui/icons/$PRODUCT.png /usr/share/pixmaps/$PRODUCT.png
-install_file https://raw.githubusercontent.com/tsukinaha/tsukimi/main/moe.tsuna.tsukimi.gschema.xml /usr/share/glib-2.0/schemas/moe.tsuna.tsukimi.gschema.xml
+install_file https://raw.githubusercontent.com/tsukinaha/tsukimi/refs/heads/main/resources/icons/tsukimi.png /usr/share/pixmaps/$PRODUCT.png
+install_file https://raw.githubusercontent.com/tsukinaha/tsukimi/refs/heads/main/resources/moe.tsuna.tsukimi.gschema.xml /usr/share/glib-2.0/schemas/moe.tsuna.tsukimi.gschema.xml
 
 # create desktop file
 cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
