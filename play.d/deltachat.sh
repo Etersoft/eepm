51b445f9d767e3dbb41219b5967e1c9c57500ae8
Ivan Mazhukin <vanomj@altlinux.org>
2025-01-10 19:38:41 +0300

    epm play: add deltachat (eterbug #17964)

---
+++ play.d/deltachat.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+PKGNAME=deltachat-desktop
+SUPPORTEDARCHES="x86_64 aarch64"
+VERSION="$2"
+DESCRIPTION="Delta Chat is a decentralized and secure messenger app"
+
+. $(dirname $0)/common.sh
+
+warn_version_is_not_supported
+
+case "$(epm print info -p)" in
+    rpm)
+        pkgtype=rpm ;;
+    deb|*)
+        pkgtype=deb ;;
+esac
+
+if [ $pkgtype = 'deb' ]; then
+    case $(epm print info -a) in
+        x86_64)
+            arch=amd64 ;;
+        aarch64)
+            arch=arm64 ;;
+        *)
+            fatal "Unsupported arch $arch for $(epm print info -d)"
+    esac
+else
+    arch=$(epm print info -a)
+fi
+
+PKGURL=$(eget --list --latest https://delta.chat/ru/download "deltachat-desktop*$arch.$pkgtype")
+
+install_pkgurl
