e7eeb4c7230c7b24319da2a278b37f1817075488
Ivan Mazhukin <vanomj@altlinux.org>
2025-01-13 22:33:16 +0300

    epm repack geogebra-classic: remove python3 deps (eterbug #17865)

--- repack.d/geogebra-classic.sh
+++ repack.d/geogebra-classic.sh
@@ -7,16 +7,9 @@ SPEC="$2"
 PRODUCT=geogebra-classic
 PRODUCTDIR=/opt/$PRODUCT
 
-PREINSTALL_PACKAGES="python3 rpm-build-python3"
-
 . $(dirname $0)/common-chromium-browser.sh
 
 move_to_opt
-
-subst '1iBuildRequires:rpm-build-python3' $SPEC
-if [ -z "$EPM_RPMBUILD" ] ; then
-    subst "1i%add_python3_path $PRODUCTDIR" $SPEC
-fi
 
 subst "s|/usr/share/$PRODUCT|$PRODUCTDIR|" $BUILDROOT/usr/bin/$PRODUCT
 
