5d92c0c25ab039ac1779df19b025309fd250f39b
Ivan Mazhukin <vanomj@altlinux.org>
2025-01-22 14:20:55 +0300

    epm play skype: add forgotten pack (eterbug #17865)

---
+++ pack.d/skypeforlinux.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TAR="$1"
+RETURNTARNAME="$2"
+#VERSION="$3"
+
+. $(dirname $0)/common.sh
+
+epm assure unsquashfs
+unsquashfs $TAR || fatal
+
+VERSION=$(grep version squashfs-root/snapcraft.yaml | awk '{print $2}')
+PKGNAME=$PRODUCT-$VERSION
+
+mkdir -p opt/
+mkdir -p usr/share/applications/
+mkdir -p usr/share/icons/hicolor/
+
+mv squashfs-root/snap/gui/*.desktop usr/share/applications/
+mv squashfs-root/usr/share/skypeforlinux opt/
+for i in 16 32 256 512 1024; do
+    install -Dm644 squashfs-root/usr/share/icons/hicolor/${i}x${i}/apps/skypeforlinux.png usr/share/icons/hicolor/${i}x${i}/apps/skypeforlinux.png
+done
+
+erc pack $PKGNAME.tar opt usr
+return_tar $PKGNAME.tar
