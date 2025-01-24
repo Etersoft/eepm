96224a89a1185ef6208fedb0e5e8697935b14b9e
CatSema <catsemaofficial@gmail.com>
2025-01-25 02:27:08 +0300

    epm play: nekoray: fix icon
    https://bugzilla.altlinux.org/52228

--- repack.d/nekoray.sh
+++ repack.d/nekoray.sh
@@ -3,8 +3,7 @@
 # It will be run with two args: buildroot spec
 BUILDROOT="$1"
 SPEC="$2"
-if [ -f $BUILDROOT/opt/nekoray/nekoray ]
-then
+if [ -f $BUILDROOT/opt/nekoray/nekoray ]; then
 	PRODUCT=nekoray
 else
 	PRODUCT=nekobox
@@ -17,12 +16,12 @@ add_bin_link_command
 
 add_libs_requires
 
-cat <<EOF |create_file /usr/share/applications/$PRODUCT.desktop
+cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
 [Desktop Entry]
 Name=Nekoray
 Comment=Qt based cross-platform GUI proxy configuration manager (backend: Xray / sing-box)
 Exec=$PRODUCT -- %u -appdata
-Icon=/opt/$PRODUCT/$PRODUCT.png
+Icon=$PRODUCTDIR/$PRODUCT.png
 Type=Application
 Terminal=false
 Categories=Network;
