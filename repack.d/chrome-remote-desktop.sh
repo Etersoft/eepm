4c7943187292b1dccfee025e91a0622fb9d5fba0
Vitaly Lipatov <lav@etersoft.ru>
2025-02-01 13:05:41 +0300

    repack.d/chrome-remote-desktop.sh: update

--- repack.d/chrome-remote-desktop.sh
+++ repack.d/chrome-remote-desktop.sh
@@ -5,13 +5,15 @@ SPEC="$2"
 PRODUCT=chrome-remote-desktop
 
 # install all requires packages before packing ($ rpmreqs chrome-remote-desktop  | xargs echo)
-PREINSTALL_PACKAGES="coreutils glib2 libcairo libdbus libdrm libexpat libgbm libgio libgtk+3 libnspr libnss libpango libX11 libxcb libXdamage libXext libXfixes libXrandr libXtst libutempter \
-     python3-base python3-module-psutil python3 rpm-build-python3"
+#PREINSTALL_PACKAGES="coreutils glib2 libcairo libdbus libdrm libexpat libgbm libgio libgtk+3 libnspr libnss libpango libX11 libxcb libXdamage libXext libXfixes libXrandr libXtst libutempter \
+#     python3-base python3-module-psutil python3"
 
 . $(dirname $0)/common-chromium-browser.sh
 
 cleanup
 
-subst '1iBuildRequires:rpm-build-python3' $SPEC
+# eepm-rpm-build doesn't support BRs
+#subst '1iBuildRequires:rpm-build-python3' $SPEC
+#set_autoreq 'yes'
 
-set_autoreq 'yes'
+add_libs_requires
