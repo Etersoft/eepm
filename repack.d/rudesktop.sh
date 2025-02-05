0265e987b8d2f8df464c619b947ace0c8486abbd
Boris Yumankulov <boria138@altlinux.org>
2025-02-05 23:05:17 +0500

    epm play: rudesktop move to opt (eterbug #18004)

--- repack.d/rudesktop.sh
+++ repack.d/rudesktop.sh
@@ -19,6 +19,29 @@ install_file usr/share/rudesktop-client/files/rudesktop.desktop /usr/share/appli
 
 #xdg-mime default rudesktop.desktop x-scheme-handler/rudesktop || true
 
+install_file usr/lib64/libsciter-gtk.so /usr/share/rudesktop-client/files/libsciter-gtk.so
+install_file usr/bin/rudesktop /usr/share/rudesktop-client/files/rudesktop
+
+remove_file /usr/lib64/libsciter-gtk.so
+remove_file /usr/bin/rudesktop
+
+move_to_opt /usr/share/rudesktop-client/files
+
+mkdir -p $BUILDROOT/usr/bin
+cat <<EOF >$BUILDROOT/usr/bin/rudesktop
+#!/bin/sh
+cd /opt/rudesktop
+if [ "\$LD_LIBRARY_PATH" ]; then
+	export LD_LIBRARY_PATH=".:\$LD_LIBRARY_PATH"
+else
+	export LD_LIBRARY_PATH="."
+fi
+./rudesktop
+EOF
+chmod a+x $BUILDROOT/usr/bin/rudesktop
+chmod a+x $BUILDROOT/opt/rudesktop/rudesktop
+pack_file /usr/bin/rudesktop
+
 subst "s|^Summary:.*|Summary: A remote control software.|" $SPEC
 
 add_libs_requires
