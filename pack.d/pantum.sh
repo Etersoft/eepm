4d939bb1b535201cd4ef83a68c64101d36cdc842
Ivan Mazhukin <vanomj@altlinux.org>
2025-02-18 18:17:03 +0300

    epm pack pantum: fix package name pattern for Astra linux (eterbug #18054)

--- pack.d/pantum.sh
+++ pack.d/pantum.sh
@@ -16,6 +16,9 @@ elif echo "$TAR" | grep -q "linux_pantum.7z" ; then
 elif echo "$TAR" | grep -q "pantum.*astra.*_amd64.zip" ; then
     erc "$TAR" || fatal
     return_tar pantum_*_amd64.deb
+# from Astra disk: Pantum_Ubuntu_Driver_V1.1.5.tar.gz
+elif echo "$TAR" | grep -q "Pantum_Ubuntu_Driver_V.*.tar.gz" ; then
+    erc "$TAR" || fatal
 else
     fatal "We support only Pantum Ubuntu Driver V.*.zip"
 fi
@@ -24,13 +27,12 @@ fi
 mv Pantum* PantumDriver || fatal
 cd PantumDriver/Resources || fatal
 
-
 case "$(epm print info -a)" in
     x86_64)
-        PKG="pantum_*_amd64.deb"
+        PKG="pantum[-_]*[-_]amd64.deb"
         ;;
     x86)
-        PKG="pantum_*_i386.deb"
+        PKG="pantum[-_]*[-_]i386.deb"
         ;;
 esac
 
