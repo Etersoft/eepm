cf906a455222e3ec2fc9ca18dcbcec48db658801
Vitaly Lipatov <lav@etersoft.ru>
2025-02-24 16:08:58 +0300

    epm play t1client: update packages' URL

--- play.d/t1client.sh
+++ play.d/t1client.sh
@@ -12,16 +12,16 @@ warn_version_is_not_supported
 
 case "$(epm print info -p)" in
   rpm)
-      PKGURL="ipfs://QmarAX2ATvXaqFdar6t5ZYTi9yuVjmAKyS8AvBCJDWC92Z?filename=t1client-standalone-0.1.4.0.13209.rpm"
+      PKGURL="https://ncloud.dssl.ru/s/SF7LcjPXa6oLbAN/download/t1client-standalone-13209.rpm"
       ;;
   *)
-      PKGURL="ipfs://QmYU1SVJXdPDYxYzoPWSJ39Xmrx16YbTDztyM5nMCAiDZx?filename=t1client-standalone-4.5.5.0-1202308-Release.deb"
+      PKGURL="https://ncloud.dssl.ru/s/F8sqrXwmpnb8Bj5/download/t1client-standalone-4.5.28.0-1238402-Release.deb"
       ;;
 esac
 
 case "$(epm print info -s)" in
   alt)
-      PKGURL="ipfs://QmYU1SVJXdPDYxYzoPWSJ39Xmrx16YbTDztyM5nMCAiDZx?filename=t1client-standalone-4.5.5.0-1202308-Release.deb"
+      PKGURL="https://ncloud.dssl.ru/s/F8sqrXwmpnb8Bj5/download/t1client-standalone-4.5.28.0-1238402-Release.deb"
       ;;
 esac
 
