f671896d88eb441be5979f1e6d5133446340630c
Boris Yumankulov <boria138@altlinux.org>
2025-02-26 15:39:09 +0500

    epm play: added zen-browser (eterbug #18085)

---
+++ play.d/zen-browser.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME=zen-browser
+SUPPORTEDARCHES="x86_64 aarch64"
+VERSION="$2"
+DESCRIPTION="Performance oriented Firefox-based web browser"
+URL="https://github.com/zen-browser/desktop"
+
+. $(dirname $0)/common.sh
+
+arch=$(epm print info -a)
+
+PKGURL=$(get_github_url "https://github.com/zen-browser/desktop/" "zen-${arch}.AppImage")
+
+install_pkgurl
