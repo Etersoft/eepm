4a4574fc19bb964f5bca22a973352c14376cace7
Boris Yumankulov <boria138@altlinux.org>
2025-03-12 15:57:16 +0600

    epm play discord: disable autoupdate and add epm config file (eterbug #18120)

--- repack.d/discord.sh
+++ repack.d/discord.sh
@@ -17,8 +17,33 @@ fix_chrome_sandbox
 add_electron_deps
 
 rm usr/bin/$PRODUCT
-add_bin_link_command $PRODUCTCUR $PRODUCTDIR/$PRODUCTCUR
-add_bin_link_command $PRODUCT $PRODUCTCUR
+
+mkdir -p $BUILDROOT/usr/bin
+cat <<EOF >$BUILDROOT/usr/bin/$PRODUCT
+#!/bin/sh
+CONFIG_DIR="\$HOME"/.config/discord
+SETTINGS_FILE="\$CONFIG_DIR"/settings.json
+DISCORD_CONFIG_FILE="\$HOME/.config/eepm/discord"
+EXTRA_PARAMS=""
+
+if [ ! -f "\$DISCORD_CONFIG_FILE" ]; then
+    DISCORD_CONFIG_FILE="/etc/eepm/config/discord"
+fi
+
+if [ -f "\$DISCORD_CONFIG_FILE" ]; then
+    EXTRA_PARAMS=$(cat "\$DISCORD_CONFIG_FILE")
+fi
+
+if [ -f "\$SETTINGS_FILE" ] && grep -q '"SKIP_HOST_UPDATE": true' "\$SETTINGS_FILE"; then
+    exec $PRODUCTDIR/$PRODUCTCUR \$EXTRA_PARAMS "\$@"
+else
+    mkdir -p "\$CONFIG_DIR"
+    echo '{ "SKIP_HOST_UPDATE": true}' > "\$SETTINGS_FILE"
+    exec $PRODUCTDIR/$PRODUCTCUR \$EXTRA_PARAMS "\$@"
+fi
+EOF
+chmod a+x $BUILDROOT/usr/bin/$PRODUCT
+pack_file /usr/bin/$PRODUCT
 
 rm usr/share/applications/discord.desktop
 install_file $PRODUCTDIR/discord.desktop /usr/share/applications/discord.desktop
