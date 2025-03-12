bab5caa37f29ac9e3589e99a6b40f400fb09303e
Ivan Mazhukin <vanomj@altlinux.org>
2025-03-12 16:57:47 +0300

    epm play: add tixati (eterbug #18121)

---
+++ repack.d/tixati.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=tixati
+PRODUCTDIR=/opt/$PRODUCT
+
+. $(dirname $0)/common.sh
+
+move_to_opt /usr/bin
+
+add_bin_exec_command
+
+add_libs_requires
