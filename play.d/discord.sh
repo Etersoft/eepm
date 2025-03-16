827784b454fb8d0306a315cb0e1f469cf3b19ca5
Vitaly Lipatov <lav@etersoft.ru>
2025-03-16 23:42:14 +0300

    epm play discord: use url as written in IPFS db

--- play.d/discord.sh
+++ play.d/discord.sh
@@ -12,7 +12,7 @@ if [ "$VERSION" = "*" ] ; then
     # workaround against curl can't get filename: https://github.com/curl/curl/issues/8461
     PKGURL="$(eget --get-real-url "https://discord.com/api/download?platform=linux&format=deb")"
 else
-    PKGURL="https://dl.discordapp.net/apps/linux/$VERSION/discord-$VERSION.deb"
+    PKGURL="https://stable.dl2.discordapp.net/apps/linux/$VERSION/discord-$VERSION.deb"
 fi
 
 install_pkgurl
