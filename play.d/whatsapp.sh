32024a20a016ba67989d5b0b3901e55b9ee3f50b
Vitaly Lipatov <lav@etersoft.ru>
2025-03-18 16:29:20 +0300

    epm play whatsapp: move to WasIstLos upstream and wasistlos package name

--- play.d/whatsapp.sh
+++ play.d/whatsapp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PKGNAME=whatsapp-for-linux
+PKGNAME=wasistlos
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION='An unofficial WhatsApp desktop application (from the repository if the package is there, or from the official site)'
-URL="https://github.com/eneshecan/whatsapp-for-linux"
+URL="https://github.com/xeco23/WasIstLos"
 
 . $(dirname $0)/common.sh
 
@@ -12,11 +12,8 @@ if epm install $PKGNAME ; then
     exit 0
 fi
 
-[ "$(epm print info -s)" = "alt" ] && fatal "ALT is not supports $PKGNAME AppImage for now."
-
 arch=x86_64
-# sh: symbol lookup error: /tmp/.private/lav/.mount_whatsaxhRMDh/opt/libc/lib/x86_64-linux-gnu/libc.so.6: undefined symbol: __libc_enable_secure, version GLIBC_PRIVATE
-PKGURL=$(eget --list --latest https://github.com/eneshecan/whatsapp-for-linux/releases "$PKGNAME-$VERSION-$arch.AppImage")
+PKGURL=$(eget --list --latest https://github.com/xeco23/WasIstLos/releases/download "$PKGNAME-$VERSION-$arch.AppImage")
 
 install_pkgurl
 
