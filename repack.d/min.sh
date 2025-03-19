386b46bd81a18250e054f9a55c4393dc368a1846
Vitaly Lipatov <lav@etersoft.ru>
2025-03-19 11:47:00 +0300

    epm repack min.sh: hack for install on separated /opt

--- repack.d/min.sh
+++ repack.d/min.sh
@@ -21,3 +21,9 @@ fix_chrome_sandbox
 add_bin_exec_command $PRODUCT $PRODUCTDIR/$PRODUCT 
 
 fix_desktop_file
+
+# hack against error: unpacking of archive failed on file /usr/share/icons/hicolor/256x256/apps/min.png;67da644b: cpio: link
+# the file also placed in /opt and when /opt and /usr is not the same fs, cpio can't unpack?
+cd usr/share/icons/hicolor/256x256/apps
+cp min.png min.png.copy
+mv min.png.copy min.png
