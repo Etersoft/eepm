f498d5f82c71221f61764ea9b2d22f903c7f0afc
Vitaly Lipatov <lav@etersoft.ru>
2025-03-19 05:06:01 +0300

    epm repack generic-snap.sh: add hack for binaries in /opt/PRODUCT/usr/bin

--- repack.d/generic-snap.sh
+++ repack.d/generic-snap.sh
@@ -15,9 +15,19 @@ for i in usr/lib/x86_64-linux-gnu/libwebp* ; do
     move_file $PRODUCTDIR/$i $PRODUCTDIR/lib/$(basename $i)
 done
 
-for i in  data-dir gnome-platform scripts lib/dri usr etc meta snap ; do
+for i in data-dir gnome-platform scripts lib/dri etc bin meta snap ; do
     remove_dir $PRODUCTDIR/$i
 done
+
+# hack
+if [ -d usr/share/applications ] && [ -f usr/bin/$PRODUCT ] ; then
+    # like whatsie
+    add_bin_exec_command $PRODUCT "$PRODUCTDIR/usr/bin/$PRODUCT"
+else
+    for i in usr ; do
+        remove_dir $PRODUCTDIR/$i
+    done
+fi
 
 for i in libEGL.so.1 libdrm.so.2 libdrm_amdgpu.so.1 libva-drm.so.2 libva-x11.so.2 libva.so.2 libcom_err.so.2 libdbus-1.so.3 libexpat.so.1 libkeyutils.so.1 ; do
     remove_file $PRODUCTDIR/lib/$i
