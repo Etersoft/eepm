11c4249c3f2c2131902226c7f4fea5e9196e7af5
Vitaly Lipatov <lav@etersoft.ru>
2025-03-20 01:40:10 +0300

    epm play vinteo.desktop: fix version detection

--- play.d/vinteo.desktop.sh
+++ play.d/vinteo.desktop.sh
@@ -11,12 +11,13 @@ URL="https://vinteo.com"
 arch=amd64
 pkgtype=deb
 
-[ "$VERSION" = "*" ] && VERSION="$(eget -q -O- "https://vinteo.ru/download/vinteo-desktop/" | grep -o -m 1 "Версия продукта [0-9].[0-9].[0-9]" | awk '{print $3}' )"
+[ "$VERSION" = "*" ] && VERSION="$(eget -q -O- "https://vinteo.ru/download/vinteo-desktop/" | grep -o -m 1 -E "Версия продукта [0-9].[0-9]+.[0-9]+" | awk '{print $3}' )"
 [ -n "$VERSION" ] || fatal "Can't get version"
 
 # use rpm, but not for ALT
 [ "$(epm print info -p)" = "rpm" ] && [ "$(epm print info -s)" != "alt" ] && pkgtype=rpm
 
+# https://download.vinteo.com/VinteoClient/linux/4.10.0/vinteo-desktop-4.10.0-amd64.deb
 case "$(epm print info -d)" in
   AstraLinux*)
       PKGURL="https://download.vinteo.com/VinteoClient/linux/$VERSION/astralinux/vinteo-desktop-$VERSION-$arch.$pkgtype"
