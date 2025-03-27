d513101132a7f595c0c6010d6ccb65e26dadf41a
Ivan Mazhukin <vanomj@altlinux.org>
2025-03-27 17:17:56 +0300

    epm play: add tabby (eterbug #18217)

---
+++ play.d/tabby.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PKGNAME=tabby-terminal
+SUPPORTEDARCHES="x86_64 aarch64 armhf"
+VERSION="$2"
+DESCRIPTION='A terminal for a more modern age'
+URL="https://github.com/Eugeny/tabby"
+
+. $(dirname $0)/common.sh
+
+case "$(epm print info -a)" in
+    x86_64)
+        arch="x64" ;;
+    aarch64)
+        arch="arm64" ;;
+    armhf)
+        arch="armv7l" ;;
+esac
+
+case "$(epm print info -p)" in
+    rpm)
+        pkgtype=rpm ;;
+    deb)
+        pkgtype=deb ;;
+    *)
+        pkgtype=AppImage ;;
+esac
+
+
+if [ "$VERSION" = "*" ] ; then
+    PKGURL=$(get_github_url "https://github.com/Eugeny/tabby/" "tabby-.$VERSION-linux-$arch.$pkgtype")
+else
+    PKGURL="https://github.com/Eugeny/tabby/releases/download/v$VERSION/tabby-$VERSION-linux-$arch.$pkgtype"
+fi
+
+install_pkgurl
+
