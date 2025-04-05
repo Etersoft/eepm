31613117a6afb6cb53266a14e04963e881f027ff
Vitaly Lipatov <lav@etersoft.ru>
2025-04-05 10:38:01 +0300

    epm play: add cstcadnavigator

---
+++ play.d/cstcadnavigator.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PKGNAME=cstcadnavigator
+VERSION="$2"
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="CST CAD Navigator from the official site"
+URL="https://cadsofttools.ru/products/cst-cad-navigator/download/"
+
+. $(dirname $0)/common.sh
+
+# Используются одни и те же бинарники в deb и rpm
+# но файл rpm с версией, поэтому нам предпочтительнее для истории
+if [ "$VERSION" = "*" ] ; then
+    PKGURL="$(eget --list --latest https://cadsofttools.ru/products/cst-cad-navigator/download/ "$PKGNAME-*x86_64.rpm" )"
+else
+    PKGURL="https://cadsofttools.ru/download/cstcadnavigator-$VERSION-1.x86_64.rpm"
+fi
+
+install_pack_pkgurl
---
+++ repack.d/cstcadnavigator.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+# It will run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCTCUR=cstCadNavigator
+. $(dirname $0)/common.sh
+
+move_to_opt
+
+# TODO: fix bug in upstream
+remove_file /usr/bin/libfmux.so
+remove_file /usr/bin/libdynapdf.so
+
+find . -type f -exec chmod 0644 {} +
+
+add_bin_exec_command $PRODUCTCUR
+add_bin_link_command $PRODUCT $PRODUCTCUR
+
+cat <<EOF >>./usr/share/applications/$PRODUCT.desktop
+MimeType=image/vnd.dwg;model/stl;image/cgm;image/svg+xml;
+EOF
+
+add_libs_requires
