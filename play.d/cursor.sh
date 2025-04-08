6a9da201cc53dd0c3000d2bf37391bd9421e7dc5
CatSema <catsemaofficial@gmail.com>
2025-04-08 11:07:14 +0300

    Update: Cursor-0.48.7

--- play.d/cursor.sh
+++ play.d/cursor.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-PKGNAME=cursor
+PKGNAME=Cursor
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
-DESCRIPTION="The AI-first Code Editor"
-URL="https://cursor.sh/"
+DESCRIPTION="Built to make you extraordinarily productive, Cursor is the best way to code with AI."
+URL="https://www.cursor.com/"
 
 . $(dirname $0)/common.sh
 
 warn_version_is_not_supported
 
-PKGURL="https://downloader.cursor.sh/linux/appImage/x64"
+PKGURL="https://downloads.cursor.com/production/1d623c4cc1d3bb6e0fe4f1d5434b47b958b05876/linux/x64/Cursor-0.48.7-x86_64.AppImage"
 
 install_pkgurl
