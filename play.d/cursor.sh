2fe45c4eb534089a2b901edf0e19e026843850f0
CatSema <catsemaofficial@gmail.com>
2025-04-08 09:52:30 +0300

    Update: Cursor-0.48.7

--- play.d/cursor.sh
+++ play.d/cursor.sh
@@ -10,6 +10,6 @@ URL="https://cursor.sh/"
 
 warn_version_is_not_supported
 
-PKGURL="https://downloader.cursor.sh/linux/appImage/x64"
+PKGURL="https://downloads.cursor.com/production/1d623c4cc1d3bb6e0fe4f1d5434b47b958b05876/linux/x64/Cursor-0.48.7-x86_64.AppImage"
 
 install_pkgurl
