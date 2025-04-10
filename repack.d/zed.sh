62a996e483c6e28dc481f4f4937ef2b579025c1a
Vitaly Lipatov <lav@etersoft.ru>
2025-04-11 00:26:38 +0300

    epm repack zed.sh: fix desktop file name

--- repack.d/zed.sh
+++ repack.d/zed.sh
@@ -10,4 +10,7 @@ PRODUCTDIR=/opt/$PRODUCT
 
 add_bin_link_command $PRODUCT $PRODUCTDIR/bin/$PRODUCT
 
+# fix wmclass
+move_file /usr/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
+
 add_libs_requires
