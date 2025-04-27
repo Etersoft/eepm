6e6e60eaad92b6012524ec13eb2ed20f1ea9a562
CatSema <catsemaofficial@gmail.com>
2025-04-27 14:59:30 +0300

    epm repack: updated icon paths for Nekoray and added categories to the .desktop file

--- repack.d/nekoray.sh
+++ repack.d/nekoray.sh
@@ -3,26 +3,30 @@
 # It will be run with two args: buildroot spec
 BUILDROOT="$1"
 SPEC="$2"
+
 if [ -f $BUILDROOT/opt/nekoray/nekoray ]; then
 	PRODUCT=nekoray
 else
 	PRODUCT=nekobox
 fi
+
 PRODUCTDIR=/opt/nekoray
 
 . $(dirname $0)/common.sh
 
 add_bin_link_command
 
-add_libs_requires
-
 cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
 [Desktop Entry]
 Name=Nekoray
 Comment=Qt based cross-platform GUI proxy configuration manager (backend: Xray / sing-box)
 Exec=$PRODUCT -- %u -appdata
-Icon=$PRODUCTDIR/$PRODUCT.png
+Icon=$PRODUCT
 Type=Application
 Terminal=false
-Categories=Network;
+Categories=Network;Internet;Utility;
 EOF
+
+install_file $PRODUCTDIR/$PRODUCT.png /usr/share/pixmaps/$PRODUCT.png
+
+add_libs_requires
