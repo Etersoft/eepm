5af65925640ebe3c74f8a00e06c50291c9717ebd
Vitaly Lipatov <lav@etersoft.ru>
2025-05-01 01:55:48 +0300

    epm play: add clash-verge

---
+++ play.d/clash-verge.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PKGNAME=clash-verge
+VERSION="$2"
+SUPPORTEDARCHES="x86_64 aarch64"
+DESCRIPTION="CClash Verge from the official site"
+URL="https://www.clashverge.dev/"
+
+. $(dirname $0)/common.sh
+
+arch="$(epm print info --debian-arch)"
+
+#https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge-2.2.3-1.aarch64.rpm
+#https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge-2.2.3-1.x86_64.rpm
+# Clash.Verge_2.2.3_amd64.deb
+if [ "$VERSION" = "*" ] ; then
+    PKGURL=$(get_github_url "https://github.com/clash-verge-rev/clash-verge-rev/" "Clash.Verge_.${VERSION}_$arch.deb")
+else
+    PKGURL="https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v${VERSION}/Clash.Verge_${VERSION}_$arch.deb"
+fi
+
+install_pkgurl
