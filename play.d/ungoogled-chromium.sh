f168066f09aea16a3efa902632f7cd3b255675be
Vitaly Lipatov <lav@etersoft.ru>
2025-05-06 10:24:54 +0300

    epm play ungoogled-chromium: allow direct download, change to get_github_url

--- play.d/ungoogled-chromium.sh
+++ play.d/ungoogled-chromium.sh
@@ -13,9 +13,13 @@ if [ "$VERSION" = "*" ] && ! is_glibc_enough 2.35 ; then
     VERSION="113.0.5672.127"
 fi
 
-[ "$VERSION" != "*" ] && VERSION="$VERSION-1.1"
-
-# https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases/download/134.0.6998.88-1/ungoogled-chromium_134.0.6998.88-1_linux.tar.xz
-PKGURL=$(eget --list --latest https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases "ungoogled-chromium_${VERSION}_linux.tar.xz")
+if [ "$VERSION" = "*" ] ; then
+    # ungoogled-chromium_136.0.7103.59-1_linux.tar.xz
+    PKGURL=$(get_github_url "https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/" "ungoogled-chromium_${VERSION}_linux.tar.xz")
+else
+    # https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases/download/134.0.6998.88-1/ungoogled-chromium_134.0.6998.88-1_linux.tar.xz
+    VERSION="$VERSION-1"
+    PKGURL=$(eget --list --latest https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases "ungoogled-chromium_${VERSION}_linux.tar.xz")
+fi
 
 install_pack_pkgurl
