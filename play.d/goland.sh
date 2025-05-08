832c213095e96ee4e2dcfdebd78776c90bd38c77
Vitaly Lipatov <lav@etersoft.ru>
2025-05-08 15:54:15 +0300

    epm play: add Goland

---
+++ play.d/goland.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PKGNAME=goland
+SUPPORTEDARCHES="x86_64 aarch64"
+VERSION="$2"
+DESCRIPTION="GoLand CE — IDE for Go developers"
+URL="https://www.jetbrains.com/go/"
+
+. $(dirname $0)/common-jetbrains.sh
+
+PKGURL="$(get_jetbrains_pkgurl GO go)"
+
+install_pkgurl
