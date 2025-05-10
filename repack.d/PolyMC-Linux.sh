6a27c5dd2c626ea30d0fe99d36b2a807730f223e
Boris Yumankulov <boria138@altlinux.org>
2025-05-10 16:26:27 +0500

    epm play: added PolyMC (eterbug #18292)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ repack.d/PolyMC-Linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+PRODUCT=PolyMC-Linux
+PRODUCTDIR=/opt/$PRODUCT
+
+. $(dirname $0)/common.sh
+
+move_file /usr/share/pixmaps/PolyMC-Linux.svg /usr/share/icons/hicolor/scalable/apps/org.polymc.PolyMC.svg
+
+add_libs_requires
