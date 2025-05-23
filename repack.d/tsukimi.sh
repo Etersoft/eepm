e9ae29c02fbe54533cb72bb83a61cb5cd448e3f4
Vitaly Lipatov <lav@etersoft.ru>
2025-05-23 22:20:29 +0300

    epm repack tsukimi: fix file install

--- repack.d/tsukimi.sh
+++ repack.d/tsukimi.sh
@@ -11,7 +11,7 @@ add_bin_link_command $PRODUCT $PRODUCTDIR/$PRODUCT
 add_requires "/usr/bin/clapper"
 
 install_file https://github.com/tsukinaha/tsukimi/blob/main/resources/icons/moe.tsuna.tsukimi.png /usr/share/pixmaps/$PRODUCT.png
-install_file moe.tsuna.tsukimi.gschema.xml /usr/share/glib-2.0/schemas/moe.tsuna.tsukimi.gschema.xml
+install_file $PRODUCTDIR/moe.tsuna.tsukimi.gschema.xml /usr/share/glib-2.0/schemas/moe.tsuna.tsukimi.gschema.xml
 
 # create desktop file
 cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
