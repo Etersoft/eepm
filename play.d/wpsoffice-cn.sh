c72158b378500bba533da64a305d8fb3110c910e
Vitaly Lipatov <lav@etersoft.ru>
2025-05-23 23:57:10 +0300

    epm play wpsoffice-cn: fix version getting

--- play.d/wpsoffice-cn.sh
+++ play.d/wpsoffice-cn.sh
@@ -10,7 +10,9 @@ TIPS="Run epm play wpsoffice=<version> to install some specific version"
 . $(dirname $0)/common.sh
 
 if [ "$VERSION" = "*" ] ; then
-    VERSION=$(eget -O- https://archive2.kylinos.cn/DEB/KYLIN_DEB/pool/main/deb/wpsoffice/ | grep -oP '[^/]+_\K[\d.]+(?=_amd64\.deb)' | sort -V | tail -n1)
+    #VERSION=$(eget -O- https://archive2.kylinos.cn/DEB/KYLIN_DEB/pool/main/deb/wpsoffice/ | grep -oP '[^/]+_\K[\d.]+(?=_amd64\.deb)' | sort -V | tail -n1)
+    VERSION="$(eget -O- https://www.wps.cn/product/wpslinux | grep '"banner_txt"' | sed -e 's|.*banner_txt">||' -e "s|</p>||")"
+    [ -n "$VERSION" ] || fatal "Can't get version"
 fi
 
 PKGURL="https://archive2.kylinos.cn/DEB/KYLIN_DEB/pool/main/deb/wpsoffice/wpsoffice_${VERSION}_amd64.deb"
