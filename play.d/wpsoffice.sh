79c21706aad6644d594e7f2a1817f535063cf063
Vitaly Lipatov <lav@etersoft.ru>
2025-05-24 00:00:49 +0300

    epm play wpsoffice: fix version getting

--- play.d/wpsoffice.sh
+++ play.d/wpsoffice.sh
@@ -9,8 +9,14 @@ TIPS="Run epm play wpsoffice=<version> to install some specific version"
 
 . $(dirname $0)/common.sh
 
+if [ "$VERSION" = "*" ] ; then
+    VERSION=$(eget -O- https://www.wps.com/whatsnew/linux/ | grep -oP "\W(\d+\.\d+\.\d+\.\d+)\W" | grep -oP "\d+\.\d+\.\d+\.\d+" | head -n1)
+    VERSION="${VERSION}.XA"
+fi
+
 mversion=$(echo "$VERSION" | sed -e 's|\.XA$||' -e 's|.*\.||')
 pkgtype=$(epm print info -p)
+
 case $pkgtype in
     rpm)
         PKGURL="https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/$mversion/wps-office-${VERSION}-1.x86_64.rpm"
