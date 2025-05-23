99117bf2254db9bf5ba7d5a59a007243a03c83f0
Vitaly Lipatov <lav@etersoft.ru>
2025-05-23 23:45:24 +0300

    epm play freeplane: fix latest version

--- play.d/freeplane.sh
+++ play.d/freeplane.sh
@@ -8,8 +8,10 @@ URL="http://freeplane.sourceforge.net"
 
 . $(dirname $0)/common.sh
 
-[ "$VERSION" = "*" ] && VERSION="$(eget -O- https://sourceforge.net/projects/$PKGNAME/best_release.json | sed -e 's|.*freeplane_bin-||g' -e 's|\.zip.*||')"
-
+if [ "$VERSION" = "*" ] ; then
+    # PKGURL="$(get_json_value https://sourceforge.net/projects/$PKGNAME/best_release.json '["platform_releases","linux","url"]')"
+    VERSION="1.12.10"
+fi
 PKGURL="https://download.sourceforge.net/project/freeplane/freeplane%20stable/freeplane_$VERSION~upstream-1_all.deb"
 
 install_pkgurl
