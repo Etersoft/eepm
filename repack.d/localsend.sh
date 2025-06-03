39be71a33f985afd82afd15df2dae53d78ca7e8d
Vitaly Lipatov <lav@etersoft.ru>
2025-06-03 21:43:08 +0300

    git repack localsend: fix package version

--- repack.d/localsend.sh
+++ repack.d/localsend.sh
@@ -9,6 +9,9 @@ PRODUCTCUR=localsend_app
 
 . $(dirname $0)/common.sh
 
+# clean version
+subst "s|^\(Version: .*\)+.*|\1|" $SPEC
+
 move_to_opt /usr/share/localsend_app
 
 add_bin_link_command $PRODUCTCUR
