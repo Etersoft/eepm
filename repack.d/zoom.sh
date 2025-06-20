1ded7630f5c4ed53a3f920f045f62039d697dbf2
Vitaly Lipatov <lav@etersoft.ru>
2025-06-20 11:21:18 +0300

    epm repack zoom: fix Qt requires

--- repack.d/zoom.sh
+++ repack.d/zoom.sh
@@ -12,4 +12,22 @@ fix_desktop_file /usr/bin/zoom
 # https://bugzilla.altlinux.org/47427
 remove_file /opt/zoom/Qt/qml/Qt/labs/lottieqt/liblottieqtplugin.so
 
+remove_file /opt/zoom/Qt/qml/QtQuick/Scene3D/libqtquickscene3dplugin.so
+remove_file /opt/zoom/Qt/qml/QtQuick/Scene2D/libqtquickscene2dplugin.so
+remove_file /opt/zoom/Qt/plugins/platforms/libqeglfs.so
+remove_file /opt/zoom/Qt/plugins/egldeviceintegrations/libqeglfs-*.so
+# bug
+remove_file /opt/zoom/Qt/plugins/egldeviceintegrations/libqeglfs-kms-egldevice-integration.so
+remove_file /opt/zoom/Qt/plugins/egldeviceintegrations/libqeglfs-x11-integration.so
+remove_file /opt/zoom/Qt/plugins/egldeviceintegrations/libqeglfs-emu-integration.so
+
+remove_file /opt/zoom/Qt/plugins/audio/libqt*.so
+# bug?
+remove_file /opt/zoom/Qt/plugins/audio/libqtmedia_pulse.so
+remove_file /opt/zoom/Qt/plugins/audio/libqtmedia_alsa.so
+remove_file /opt/zoom/Qt/qml/QtQuick/Particles.2/libparticlesplugin.so
+remove_file /opt/zoom/Qt/qml/QtQuick/LocalStorage/libqmllocalstorageplugin.so
+remove_file /opt/zoom/Qt/qml/QtQuick/XmlListModel/libqmlxmllistmodelplugin.so
+remove_file /opt/zoom/Qt/qml/QtQml/RemoteObjects/libqtqmlremoteobjects.so
+
 add_libs_requires
