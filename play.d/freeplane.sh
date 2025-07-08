e18617077f62c01b4c676532c2e2576a4d1c3841
Ivan Mazhukin <vanomj@altlinux.org>
2025-07-08 15:17:23 +0300

    epm play freeplane: fix getting version (eterbug #17865)

--- play.d/freeplane.sh
+++ play.d/freeplane.sh
@@ -9,9 +9,9 @@ URL="https://freeplane.sourceforge.net"
 . $(dirname $0)/common.sh
 
 if [ "$VERSION" = "*" ] ; then
-    # PKGURL="$(get_json_value https://sourceforge.net/projects/$PKGNAME/best_release.json '["platform_releases","linux","url"]')"
-    VERSION="1.12.10"
+    VERSION="$(get_json_value https://sourceforge.net/projects/$PKGNAME/best_release.json '["platform_releases","linux","filename"]' | grep -oP '\d+\.\d+\.\d+')"
 fi
+
 PKGURL="https://download.sourceforge.net/project/freeplane/freeplane%20stable/freeplane_$VERSION~upstream-1_all.deb"
 
 install_pkgurl
