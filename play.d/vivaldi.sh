b727d9d401f8dcd92057395a414de74aebc1ebfa
Ivan Mazhukin <vanomj@altlinux.org>
2025-07-09 10:44:53 +0300

    epm play vivaldi: fix download (eterbug #17865)

--- play.d/vivaldi.sh
+++ play.d/vivaldi.sh
@@ -10,8 +10,10 @@ TIPS="Run 'epm play vivaldi=snapshot' to install snapshot version of the browser
 
 . $(dirname $0)/common.sh
 
-
+pkgtype="$(epm print info -p)"
+rpmarch="$(epm print info -a)"
 arch="$(epm print info --debian-arch)"
+
 case "$arch" in
     amd64|aarch64|i386|armhf)
         ;;
@@ -35,20 +37,39 @@ warn_version_is_not_supported
 # epm uses eget to download * names
 #epm install "https://repo.vivaldi.com/archive/deb/pool/main/$(epm print constructname $PKGNAME "*" $arch deb)"
 
-if [ "$PKGNAME" = "$BASEPKGNAME-snapshot" ] ; then
+__get_snapshot(){
     # copied from install-vivaldi.sh script
     # https://help.vivaldi.com/desktop/install-update/install-snapshots-on-non-deb-rpm-distros/
     DEBARCH=$arch
+
     if [ "$VERSION" = "*" ] ; then
         VERSION=$(eget -O- "https://repo.vivaldi.com/archive/deb/dists/stable/main/binary-$DEBARCH/Packages.gz" | gzip -d | grep -A6 -x "Package: $PKGNAME" | sed -n 's/^Version: \(\([0-9]\+\.\)\{3\}[0-9]\+-[0-9]\+\)/\1/p' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -n 1) #'
     fi
+
     PKGURL="https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_${VERSION}_$arch.deb"
-else
+}
+
+__get_stable(){
     if [ "$VERSION" = "*" ] ; then
-        PKGURL="$(eget --list --latest https://vivaldi.com/ru/download "")" #"
+       
+        case $pkgtype in
+            rpm)
+                mask="vivaldi-stable-*$rpmarch.$pkgtype" ;;
+            *)
+                mask="vivaldi-stable_*$arch.$pkgtype" ;;
+        esac
+
+        PKGURL="$(eget --list --latest https://vivaldi.com/ru/download "$mask")"
     else
-        PKGURL="https://downloads.vivaldi.com/stable/$(epm print constructname $PKGNAME "$VERSION" $arch deb)"
+        PKGURL="https://downloads.vivaldi.com/stable/$(epm print constructname $PKGNAME $VERSION $arch deb)"
     fi
+}
+
+
+if [ "$PKGNAME" = "$BASEPKGNAME-snapshot" ] ; then
+    __get_snapshot
+else
+    __get_stable
 fi
 
 install_pkgurl
