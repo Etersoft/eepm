9200e2c6f9ef303b936c08448f8ddaee64a2208b
Boris Yumankulov <boria138@altlinux.org>
2025-07-10 17:03:56 +0500

    epm play: added ElyPrismLauncher (eterbug #18405)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ play.d/elyprismlauncher.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PKGNAME=ElyPrismLauncher-Linux
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION='Fork of Prism Launcher adds integrated support for Ely.by accounts (MSA accounts can still be used)'
+URL="https://github.com/ElyPrismLauncher/ElyPrismLauncher"
+
+. $(dirname $0)/common.sh
+
+arch=x86_64
+if [ "$VERSION" = "*" ] ; then
+    PKGURL=$(get_github_url "https://github.com/ElyPrismLauncher/ElyPrismLauncher/" "ElyPrismLauncher-Linux-x86_64.AppImage")
+else
+    PKGURL="https://github.com/ElyPrismLauncher/ElyPrismLauncher/releases/download/$VERSION/ElyPrismLauncher-Linux-x86_64.AppImage"
+fi
+
+install_pkgurl
---
+++ repack.d/ElyPrismLauncher-Linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+PRODUCT=ElyPrismLauncher-Linux
+PRODUCTDIR=/opt/$PRODUCT
+
+. $(dirname $0)/common.sh
+
+move_file /usr/share/pixmaps/ElyPrismLauncher-Linux.svg /usr/share/icons/hicolor/scalable/apps/org.prismlauncher.PrismLauncher.svg
+add_conflicts PrismLauncher-Linux prismlauncher
+
+add_libs_requires
