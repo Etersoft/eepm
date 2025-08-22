b0e24456f55ca82be673203ecee4bad1be0110eb
CatSema <catsemaofficial@gmail.com>
2025-08-22 20:48:56 +0300

    epm play persepolis: fix download

--- play.d/persepolis.sh
+++ play.d/persepolis.sh
@@ -3,7 +3,7 @@
 PKGNAME=persepolis
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
-DESCRIPTION='is a download manager & a GUI for Aria2'
+DESCRIPTION='Persepolis is a download manager written in Python.'
 URL="https://persepolisdm.github.io/"
 
 . $(dirname $0)/common.sh
@@ -11,7 +11,9 @@ URL="https://persepolisdm.github.io/"
 if [ "$VERSION" = "*" ] ; then
     PKGURL=$(get_github_url https://github.com/persepolisdm/persepolis/ "${PKGNAME}_${VERSION}_all.deb")
 else
-    PKGURL="https://github.com/persepolisdm/persepolis/releases/download/$VERSION/${PKGNAME}_${VERSION}_all.deb"
+    # Отрезаем последнюю цифру из версии для получения правильного тега. Например: 5.1.1.0 -> 5.1.1
+    TAG_VERSION=$(echo "$VERSION" | sed 's/\.[0-9]*$//')
+    PKGURL="https://github.com/persepolisdm/persepolis/releases/download/$TAG_VERSION/${PKGNAME}_${VERSION}_all.deb"
 fi
 
 install_pkgurl
