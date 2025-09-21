bf821be4c85033f284bbdcc5f44e2a830a84d561
Vitaly Lipatov <lav@etersoft.ru>
2025-09-21 16:57:24 +0300

    epm play play yandex-browser-stable-codecs-ffmpeg-extra: use json parser

--- play.d/yandex-browser-stable-codecs-ffmpeg-extra.sh
+++ play.d/yandex-browser-stable-codecs-ffmpeg-extra.sh
@@ -8,15 +8,13 @@ URL="https://browser-resources.s3.yandex.net/linux/codecs_snap.json"
 . $(dirname $0)/common.sh
 
 CODECS_JSON="https://browser-resources.s3.yandex.net/linux/codecs_snap.json"
-JSON=$(curl -s "$CODECS_JSON")
+JSON="$(eget -O- "$CODECS_JSON")"
+
+# TODO: use needed version
+# use latest available version
 VERSION=$(echo "$JSON" | grep -o '"[0-9]\+":' | tr -d '"' | tr -d ':' | tail -n1)
-FFMPEG_PATH=$(echo "$JSON" | awk -v rev="$VERSION" '
-    $0 ~ "\""rev"\"" {found=1; next}
-    found && /"path"/ {gsub(/.*: *"|",?$/, ""); print; exit}
-')
-PKGURL=$(echo "$JSON" | awk -v rev="$VERSION" '
-    $0 ~ "\""rev"\"" {found=1; next}
-    found && /"url"/ {gsub(/.*: *"|",?$/, ""); print; exit}
-')
+
+FFMPEG_PATH="$(echo "$JSON" | parse_json_value "[\"$VERSION\",\"path\"]")"
+PKGURL="$(echo "$JSON" | parse_json_value "[\"$VERSION\",\"url\"]")"
 
 install_pack_pkgurl "$VERSION" "$FFMPEG_PATH"
