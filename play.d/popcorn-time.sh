dc6ef14ad7c30aa7d3d7b85b526756d8026d9bb0
Boris Yumankulov <boria138@altlinux.org>
2025-09-30 13:53:33 +0500

    epm play popcorn-time: fix PKGURL (eterbug #17865)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- play.d/popcorn-time.sh
+++ play.d/popcorn-time.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PKGNAME=Popcorn-Time
+PKGNAME=popcorn-time-nightly
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION='Popcorn Time is a multi-platform, free software BitTorrent client that includes an integrated media player'
@@ -8,8 +8,9 @@ URL="https://github.com/popcorn-official/popcorn-desktop"
 
 . $(dirname $0)/common.sh
 
-PKGURL=$(epm tool eget --list --latest https://github.com/popcorn-official/popcorn-desktop/releases "Popcorn-Time-$VERSION-amd64.deb")
+warn_version_is_not_supported
+
+PKGURL=$(eget --list https://popcorntime.app/download "*.deb")
 
 # repack always, ever for deb system (bad postinst script)
 install_pkgurl --repack
-
