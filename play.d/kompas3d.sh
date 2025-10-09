d7618f972c7e7ae33a47b6882896b2ee66eaaa29
Boris Yumankulov <boria138@altlinux.org>
2025-10-09 20:40:55 +0500

    epm play: added ascon-kompas3d (eterbug #18603)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ play.d/kompas3d.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+PKGNAME=ascon-kompas3d
+SKIPREPACK=1
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Kompas 3D beta from the official site"
+URL="https://repo.ascon.ru/beta"
+REPOURL="https://repo.ascon.ru/beta"
+
+. $(dirname $0)/common.sh
+
+warn_version_is_not_supported
+
+case $(epm print info -e) in
+    ALTLinux/p11|ALTLinux/Sisyphus)
+        epm repo addkey "$REPOURL/alt/ascon.gpg"
+        epm repo add "rpm [ascon] $REPOURL/alt/ p11/x86_64 main"
+        ;;
+    RedOS/8.0)
+TMPREPO=$(mktemp -d)
+cat > "$TMPREPO/ascon-beta.repo" << EOF
+[ascon-beta]
+name=Ascon Beta
+baseurl=https://repo.ascon.ru/beta/rpm/redos/8.0
+gpgkey=https://repo.ascon.ru/beta/rpm/ascon.gpg
+gpgcheck=1
+enabled=1
+EOF
+        epm repo add "file://$TMPREPO/ascon-beta.repo"
+        ;;
+    AstraLinuxSE/1.8)
+        # Copied from install script
+        # echo "deb [signed-by=/etc/apt/trusted.gpg.d/ascon.gpg] https://repo.ascon.ru/beta/deb $(lsb_release -cs) main" > /etc/apt/sources.list.d/ascon-beta.list
+        epm install lsb-release
+        epm repo addkey "$REPOURL/deb/ascon.gpg"
+        epm repo add "deb [signed-by=/etc/apt/trusted.gpg.d/ascon.gpg] https://repo.ascon.ru/beta/deb $(lsb_release -cs) main"
+        ;;
+    *)
+        fatal "Unsupported distro $(epm print info -e). Ask application vendor for a support."
+        ;;
+esac
+
+
+epm update
+epm install ascon-kompas3d-v24
