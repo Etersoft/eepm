30fde91f9234ccdaa69fd70bcdc75863c591e129
Boris Yumankulov <36843745+Boria138@users.noreply.github.com>
2025-10-13 19:38:25 +0500

    epm play popcorn-time: fix PKGURL (eterbug #17865) (#485)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ repack.d/popcorn-time-nightly.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+. $(dirname $0)/common.sh
+
+# drop dictionaries from lib folder
+move_to_opt "/usr/lib/Popcorn Time Nightly"
+
+fix_desktop_file "Categories=" "Categories=Network;Video;"
+
+# workaround for Nvidia
+fix_desktop_file "Exec=popcorntime-tauri" "Exec=WEBKIT_DISABLE_DMABUF_RENDERER=1 GDK_BACKEND=x11 popcorntime-tauri"
+
+
+add_libs_requires
