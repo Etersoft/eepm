fcc4bc344537e2f45be112238d5e836b6a759bfc
Boris Yumankulov <36843745+Boria138@users.noreply.github.com>
2025-10-13 19:37:40 +0500

    epm play: added stub for DaVinci Resolve (#487)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ play.d/davinci-resolve.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PKGNAME=davinci-resolve
+SUPPORTEDARCHES="x86_64"
+DESCRIPTION="Professional A/V post-production software suite from Blackmagic Design (stub for epm pack)"
+URL="https://www.blackmagicdesign.com"
+
+cat <<EOF
+Since Blackmagic provides the DaVinci Resolve download link only to registered users, we cannot create a complete play script. Therefore, to repackage DaVinci Resolve, run:
+
+epm install D****.run
+EOF
