c0865bbfcd0d899c83aa320e5b0053628e53145f
Vitaly Lipatov <lav@etersoft.ru>
2025-10-21 21:23:29 +0300

    epm play aksusbd: update to 10.14

--- play.d/aksusbd.sh
+++ play.d/aksusbd.sh
@@ -11,8 +11,8 @@ warn_version_is_not_supported
 
 # Site: https://supportportal.gemalto.com/csm?id=kb_category&kb_category=f9ae29a44fb2c304873b69d18110c764
 
-# Sentinel LDK Linux Runtime Installer Script 10.13
-PKGURL="ipfs://Qme9tqhRy2gkbu1qtj663n5UN5mvFgCPRLLg3uhTCS23pC?filename=Sentinel_LDK_Linux_Run-time_Installer_script.tar.gz"
+# Sentinel LDK Linux Runtime Installer Script 10.14
+PKGURL="ipfs://QmTu5yGU6tqNA7Sh8BMtCsW3pLW3AMAcYcBv6ZwkmpLe8w?filename=Sentinel_LDK_Linux_Run-time_Installer_script.tar.gz"
 
 install_pack_pkgurl || exit
 
