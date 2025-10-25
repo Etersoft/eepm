9174c4b8c038a7ce71bd1b62d86e1b1113013b4e
Ivan Mazhukin <vanomj@altlinux.org>
2025-10-25 09:58:47 +0300

    epm play freeplane: fix download (eterbug #17865)

--- play.d/freeplane.sh
+++ play.d/freeplane.sh
@@ -12,6 +12,14 @@ if [ "$VERSION" = "*" ] ; then
     VERSION="$(get_json_value https://sourceforge.net/projects/$PKGNAME/best_release.json '["platform_releases","linux","filename"]' | grep -oP '\d+\.\d+\.\d+')"
 fi
 
-PKGURL="https://download.sourceforge.net/project/freeplane/freeplane%20stable/freeplane_$VERSION~upstream-1_all.deb"
+# Yes, now with dot after version
+case  $(epm print compare package version "$VERSION" 1.12.13) in
+    1|0)
+    file=freeplane_$VERSION.upstream-1_all.deb ;;
+    -1)
+    file=freeplane_$VERSION~upstream-1_all.deb ;;
+esac 
+
+PKGURL="https://download.sourceforge.net/project/freeplane/freeplane%20stable/${file}"
 
 install_pkgurl
