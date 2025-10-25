d822676d0b56be9211a55ac689c81d2d41f31bf6
Ivan Mazhukin <vanomj@altlinux.org>
2025-10-25 11:40:58 +0300

    epm play kyodialog: use russian site to download driver (eterbug #17865)

--- play.d/kyodialog.sh
+++ play.d/kyodialog.sh
@@ -11,6 +11,8 @@ URL="https://www.kyoceradocumentsolutions.com/download/model_ru.html?r=92&s=23&m
 warn_version_is_not_supported
 
 #PKGURL="https://www.kyoceradocumentsolutions.eu/content/download-center/eu/drivers/all/Linux_Universal_Driver_zip.download.zip"
-PKGURL="https://dam.kyoceradocumentsolutions.com/content/dam/gdam_dc/dc_global/executables/web/KyoceraLinuxPackages-20230720.tar.gz"
+#PKGURL="https://dam.kyoceradocumentsolutions.com/content/dam/gdam_dc/dc_global/executables/web/KyoceraLinuxPackages-20230720.tar.gz"
+# use the russian site, because the global site gives 403 error
+PKGURL="https://www.kyoceradocumentsolutions.ru/content/download-center/ru/drivers/all/Linux_Universal_Driver_zip.download.zip"
 
 install_pack_pkgurl
