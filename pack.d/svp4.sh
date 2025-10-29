f1afbe2e6f7864c5727df9ebade49c494f9b8077
Boris Yumankulov <boria138@altlinux.org>
2025-10-29 10:18:18 +0500

    epm play svp4: fix install (eterbug #17865)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- pack.d/svp4.sh
+++ pack.d/svp4.sh
@@ -9,23 +9,38 @@ PRODUCT="svp4"
 # svp4-linux.4.6.263
 VERSION="$(basename "$TAR" .tar.bz2 | grep -oP '\d+\.\d+(\.\d+)?')"
 
+# mkdir -p installer
+# echo "Finding 7z archives in installer..."
+# LANG=C grep --only-matching --byte-offset --binary --text $'7z\xBC\xAF\x27\x1C' "svp4-linux-64.run" |
+#     cut -f1 -d: |
+#     while read ofs; do
+#         dd if="svp4-linux-64.run" bs=1M iflag=skip_bytes status=none skip="${ofs}" of="installer/bin-${ofs}.7z"
+#     done
+#
+# echo "Extracting 7z archives from installer..."
+# for f in "installer/"*.7z; do
+#     7z -bd -bb0 -y x -o"extracted/" "${f}" || true
+# done
+
 mkdir -p opt/svp4
 erc $TAR
 
-mkdir installer
-LANG=C grep --only-matching --byte-offset --binary --text $'7z\xBC\xAF\x27\x1C' "svp4-linux-64.run" |
-	cut -f1 -d: |
-	while read ofs; do
-		dd if="svp4-linux-64.run" bs=1M iflag=skip_bytes status=none skip="${ofs}" of="installer/bin-${ofs}.7z"
-	done
-for f in "installer/"*.7z; do
-		7z -bd -bb0 -y x -o"extracted/" "${f}" || true
-done
+chmod +x svp4-linux.run
+./svp4-linux.run --installDefault --targetDir "$(pwd)/opt/svp4"
 
 # Drop bundled pythonqt for avoid dependency on python 3.8
-rm extracted/extensions/libPythonQt.so
+rm opt/svp4/extensions/libPythonQt.so
 
-mv extracted/* opt/svp4/
+# Drop svptube for avoid dependency on python 3.8
+rm opt/svp4/extensions/libsvptube.so
+rm -r opt/svp4/extensions/tube
+
+# Drop installer source and bundled dependency
+rm -r opt/svp4/installerResources
+rm -r opt/svp4/mpv
+rm -r opt/svp4/python
+rm opt/svp4/qt6.conf
+find opt/svp4/libs/ -mindepth 1 -maxdepth 1 ! -name 'libmediainfo.so.0' -exec rm -rf {} +
 
 PKGNAME=$PRODUCT-$VERSION
 
--- repack.d/svp4.sh
+++ repack.d/svp4.sh
@@ -34,6 +34,6 @@ EOF
 
 add_requires mpv libqt5-concurrent
 add_unirequires libPythonQt.so.0 "python3(vapoursynth)" libmediainfo.so.0
-ignore_lib_requires libPythonQt.so.1
+ignore_lib_requires libPythonQt.so.1 libPythonQt.so.3 libnvinfer.so.10
 add_libs_requires
 add_bin_link_command $PRODUCT $PRODUCTDIR/SVPManager
