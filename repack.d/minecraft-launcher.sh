e476482926cb3ef8ae99b709ca1328becefad42e
Vitaly Lipatov <lav@etersoft.ru>
2025-11-01 23:25:57 +0300

    epm play mincraft-launcher: add libcurl4-openssl to fix start

--- repack.d/minecraft-launcher.sh
+++ repack.d/minecraft-launcher.sh
@@ -9,4 +9,15 @@ PRODUCT=minecraft-launcher
 
 add_requires java-openjdk at-spi2-atk file GConf glib2 grep libatk libat-spi2-core libalsa libcairo libcups libdbus libdrm libexpat libgbm libgdk-pixbuf libgio libgtk+3 libnspr libnss libpango libX11 libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libXrandr libXrender libXtst sed which xdg-utils xprop libsecret
 
+# https://packages.altlinux.org/ru/sisyphus/srpms/libcurl4-openssl/
+add_unirequires libcurl-openssl.so.4
+
+move_file /usr/bin/$PRODUCT $PRODUCTDIR/$PRODUCT
+
+cat <<EOF | create_exec_file /usr/bin/$PRODUCT
+#!/bin/sh
+export LD_LIBRARY_PATH=/usr/lib64/libcurl4-openssl
+$PRODUCTDIR/$PRODUCT
+EOF
+
 set_autoreq 'yes'
