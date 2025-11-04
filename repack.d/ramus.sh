e3b2a6bb3cef1720ab3d859d61de2fe3a52b2b0a
Boris Yumankulov <boria138@altlinux.org>
2025-11-04 19:21:42 +0500

    epm play: added ramus (eterbug #18659)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ repack.d/ramus.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+. $(dirname $0)/common.sh
+
+add_requires java-11-openjdk-devel gradle
+
+add_libs_requires
+
