7fb36c1d280cfb02fa9bee2aea388e610c999015
Vitaly Lipatov <lav@etersoft.ru>
2025-11-05 11:45:48 +0300

    epm play librewolf: use direct link

--- play.d/librewolf.sh
+++ play.d/librewolf.sh
@@ -19,21 +19,24 @@ if [ "$VERSION" = "*" ] ; then
     VERSION="$(eget --list --latest https://repo.librewolf.net/pool/ | grep -oE '[0-9]+\.[0-9]+(\.[0-9]+)?')"
 fi
 
+# hack: assure there are 1 in any way
+rel=1
+
 pkgtype=$(epm print info -p)
 case $pkgtype in
     rpm)
         # https://repo.librewolf.net/pool/librewolf-132.0-1-linux-x86_64-rpm.rpm
-        PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-[0-9]*-linux-$arch-rpm.rpm"
+        PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-$rel-linux-$arch-rpm.rpm"
         ;;
     *)
         # https://repo.librewolf.net/pool/librewolf-132.0-1-linux-x86_64-deb.deb
-        PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-[0-9]*-linux-$arch-deb.deb"
+        PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-$rel-linux-$arch-deb.deb"
         ;;
 esac
 
 if ! is_glibc_enough 2.35 ; then
     # use deb package for old glibc
-    PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-[0-9]*-linux-$arch-deb.deb"
+    PKGURL="https://repo.librewolf.net/pool/librewolf-$VERSION-$rel-linux-$arch-deb.deb"
 fi
 
 install_pkgurl
