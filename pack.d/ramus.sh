92fc88ec110980769b2a287e106ffb8be1837fde
Boris Yumankulov <boria138@altlinux.org>
2025-11-06 10:55:57 +0500

    epm play ramus: build it on pack (eterbug #18659)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- pack.d/ramus.sh
+++ pack.d/ramus.sh
@@ -3,53 +3,70 @@
 TAR="$1"
 RETURNTARNAME="$2"
 URL="$4"
+WORKDIR="$(pwd)"
 
 . $(dirname $0)/common.sh
 
 erc unpack $TAR || fatal
 
 mkdir -p  opt
-mv  ramus* opt/$PRODUCT
+mv  $PRODUCT* opt/$PRODUCT
 
 VERSION=$(echo "$URL" | grep -oE '[0-9]+(\.[0-9]+){1,2}')
 [ -n "$VERSION" ] || fatal "Can't get package version"
 
-install_file opt/ramus/gui-framework-common/src/main/resources/com/ramussoft/gui/application.png /usr/share/pixmaps/ramus.png
+epmi java-11-openjdk-devel gradle
 
-cat <<EOF | create_file /usr/share/applications/ramus.desktop
+# Could not resolve all files for configuration ':gui-framework-core:compileClasspath'.
+sed -i "s|^.*implementation 'org\.dockingframes:docking-frames-common:1\.1\.2-SNAPSHOT'|    implementation 'org.dockingframes:docking-frames-common:1.1.1'|" opt/$PRODUCT/gui-framework-core/build.gradle
+
+cd opt/$PRODUCT
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
+gradle build || fatal
+gradle copyFiles || fatal
+cd $WORKDIR
+install -d "usr/share/doc/$PRODUCT/"{ru,en}
+install -d "usr/share/java/$PRODUCT"
+install -d "usr/share/pixmaps"
+
+install -m644 "opt/$PRODUCT/dest/doc/ru/"* "usr/share/doc/$PRODUCT/ru/"
+install -m644 "opt/$PRODUCT/dest/doc/en/"* "usr/share/doc/$PRODUCT/en/"
+mv "opt/$PRODUCT/dest/full/lib/thirdparty/"{local-client-1.0-SNAPSHOT.jar,$PRODUCT-modeler.jar} || true
+install -m644 "opt/$PRODUCT/dest/full/lib/$PRODUCT/"*.jar "usr/share/java/$PRODUCT/"
+install -m644 "opt/$PRODUCT/dest/full/lib/thirdparty/"*.jar "usr/share/java/$PRODUCT/"
+install -m644 "opt/$PRODUCT/dest/izpack/icon.png" "usr/share/pixmaps/$PRODUCT.png"
+
+
+cat <<EOF | create_file /usr/bin/$PRODUCT
+#!/bin/sh
+# Run Ramus IDEF0, DFD Modeler - Visual editor
+
+for name in /usr/share/java/ramus/*.jar ; do
+  CP=\$CP:\$name
+done
+
+
+# Set the initial and maximum JVM heap size
+JAVA_HEAP_MAX_SIZE=192
+
+# Start jEdit
+exec java -cp \$CP -Xmx\${JAVA_HEAP_MAX_SIZE}M -Dawt.useSystemAAFontSettings=lcd com.ramussoft.local.Main "\$@"
+EOF
+
+
+cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
 [Desktop Entry]
 Version=1.0
 Name=Ramus
 Comment=Java-based IDEF0 & DFD Modeler
-Exec=ramus %u
-Icon=ramus
+Exec=$PRODUCT %u
+Icon=$PRODUCT
 Type=Application
 Categories=Development;IDE
 EOF
 
-cat <<EOF | create_file /usr/bin/ramus
-#!/bin/sh
-# Run Ramus IDEF0, DFD Modeler - Visual editor
-
-cd /opt/ramus
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
-export PATH=\$JAVA_HOME/bin:\$PATH
-gradle runLocal --no-daemon
-EOF
-
-# Cannot create directory '/opt/ramus/.gradle/8.14.3/fileHashes'
-cat <<EOF | create_file /opt/ramus/gradle.properties
-gradle.user.home=\$HOME/.gradle
-EOF
-
-# Failed to create parent directory '/opt/ramus/*/build' when creating directory '/opt/ramus/*/build/classes/java/main'
-find . -type f -name "build.gradle" -exec sed -i "/apply plugin: 'java'/a buildDir = \"\${System.env.HOME}/.ramus-build/\${project.name}\"" {} +
-
-# Could not resolve all files for configuration ':gui-framework-core:compileClasspath'.
-sed -i "s|^.*implementation 'org\.dockingframes:docking-frames-common:1\.1\.2-SNAPSHOT'|    implementation 'org.dockingframes:docking-frames-common:1.1.1'|" opt/ramus/gui-framework-core/build.gradle
-
-chmod 755 usr/bin/ramus
-
+chmod 755 usr/bin/$PRODUCT
 
 PKGNAME=$PRODUCT-$VERSION
 
