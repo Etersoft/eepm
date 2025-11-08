063b1f3ac187475ca637e202d3718523ea0e2845
Vitaly Lipatov <lav@etersoft.ru>
2025-11-09 00:14:47 +0300

    epm play zen-browser: add warn_version_is_not_supported

--- play.d/zen-browser.sh
+++ play.d/zen-browser.sh
@@ -10,6 +10,8 @@ URL="https://github.com/zen-browser/desktop"
 
 arch=$(epm print info -a)
 
+warn_version_is_not_supported
+
 PKGURL=$(get_github_url "https://github.com/zen-browser/desktop/" "zen-${arch}.AppImage")
 
 install_pack_pkgurl
