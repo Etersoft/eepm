79e31b58b2ed8f22de09145f227f5d5ed5d6fe6e
Boris Yumankulov <boria138@altlinux.org>
2025-11-14 21:08:55 +0500

    epm play max: replace appimage to deb and rpm (eterbug #18399)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- play.d/max.sh
+++ play.d/max.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-PKGNAME=MAX
+PKGNAME=max
 SUPPORTEDARCHES="x86_64"
 DESCRIPTION='Быстрое и лёгкое приложение для общения и решения повседневных задач'
 URL="https://max.ru/"
 
 . $(dirname $0)/common.sh
 
-PKGURL="https://download.max.ru/electron/MAX.AppImage"
+pkgtype=$(epm print info -p)
+case $pkgtype in
+    rpm)
+        PKGURL="https://download.max.ru/electron/MAX.rpm"
+        ;;
+    *)
+        PKGURL="https://download.max.ru/electron/MAX.deb"
+        ;;
+esac
 
 install_pkgurl
