84bf8dc412de77b3926274813642101b1789e73e
Vitaly Lipatov <lav@etersoft.ru>
2025-11-21 12:13:52 +0300

    epm play kyodialog: update URL, add support for install by URL

--- play.d/kyodialog.sh
+++ play.d/kyodialog.sh
@@ -13,6 +13,7 @@ warn_version_is_not_supported
 #PKGURL="https://www.kyoceradocumentsolutions.eu/content/download-center/eu/drivers/all/Linux_Universal_Driver_zip.download.zip"
 #PKGURL="https://dam.kyoceradocumentsolutions.com/content/dam/gdam_dc/dc_global/executables/web/KyoceraLinuxPackages-20230720.tar.gz"
 # use the russian site, because the global site gives 403 error
-PKGURL="https://www.kyoceradocumentsolutions.ru/content/download-center/ru/drivers/all/Linux_Universal_Driver_zip.download.zip"
+PKGURL="https://www.kyoceradocumentsolutions.ru/content/dam/download-center-cf/ru/drivers/all/Linux_Universal_Driver_zip.download.zip"
+#PKGURL="https://www.kyoceradocumentsolutions.ru/content/download-center/ru/drivers/all/Linux_Universal_Driver_zip.download.zip"
 
 install_pack_pkgurl
