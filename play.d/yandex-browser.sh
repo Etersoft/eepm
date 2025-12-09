ebbbb9153990ec359cfdb0c305e7d06fb5a97768
Boris Yumankulov <boria138@altlinux.org>
2025-12-09 12:01:52 +0500

    epm play yandex-browser: replace link (eterbug #18724)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- play.d/yandex-browser.sh
+++ play.d/yandex-browser.sh
@@ -2,7 +2,7 @@
 
 BASEPKGNAME=yandex-browser
 SUPPORTEDARCHES="x86_64"
-PRODUCTALT="stable beta corporate"
+PRODUCTALT="stable beta"
 VERSION="$2"
 DESCRIPTION="Yandex browser from the official site"
 URL="https://browser.yandex.ru/"
@@ -12,22 +12,19 @@ TIPS="Run 'epm play yandex-browser=beta' to install beta version of the browser.
 
 warn_version_is_not_supported
 
-# See also https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=yandex-browser-beta
+URL="https://browser.yandex.ru/download"
 
-URL="https://repo.yandex.ru/yandex-browser"
-
-if [ "$(epm print info -p)" = "rpm" ] ; then
-    # https://repo.yandex.ru/yandex-browser/rpm/stable/x86_64/yandex-browser-stable-23.1.1.1114-1.x86_64.rpm
-    [ "$BRANCH" = "corporate" ] && BRANCH="stable"
-    PKGURL="$URL/rpm/$BRANCH/x86_64/$(epm print constructname $PKGNAME "$VERSION*" x86_64 rpm)"
+if [ "$BRANCH" = "beta" ]; then
+    BETA_FLAG="&beta=1"
 else
-    # https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-beta/yandex-browser-beta_23.5.4.682-1_amd64.deb
-    PKGURL="$URL/deb/pool/main/y/$PKGNAME/$(epm print constructname $PKGNAME "$VERSION*" amd64 deb)"
+    BETA_FLAG=""
 fi
 
-if [ "$(epm print info -s)" = "redos" ] ; then
-    BRANCH="stable"
-    PKGURL="$URL/rpm/redos/x86_64/$(epm print constructname $PKGNAME "$VERSION*" x86_64 rpm)"
+if [ "$(epm print info -p)" = "rpm" ] ; then
+    [ "$BRANCH" = "corporate" ] && BRANCH="stable"
+    PKGURL="$URL?os=linux&package=rpm${BETA_FLAG}"
+else
+    PKGURL="$URL?os=linux&package=deb${BETA_FLAG}"
 fi
 
 install_pkgurl
