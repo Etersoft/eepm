fe70652578181726f3ab25a20b4df8595dc1b164
Vitaly Lipatov <lav@etersoft.ru>
2025-12-10 22:02:22 +0300

    epm repack r7-office: remove extra deps on libQt5*

--- repack.d/r7-office.sh
+++ repack.d/r7-office.sh
@@ -12,4 +12,27 @@ fix_desktop_file /usr/bin/r7-office-desktopeditors
 fix_desktop_file /usr/bin/r7-office-imageviewer
 fix_desktop_file /usr/bin/r7-office-videoplayer
 
+# libQt5EglFSDeviceIntegration.so.5()(64bit)
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqeglfs.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqeglfs.so
+
+# libQt5Qml.so.5()(64bit)
+# libQt5VirtualKeyboard.so.5()(64bit)
+remove_file $PRODUCTDIR/desktopeditors/platforminputcontexts/libqtvirtualkeyboardplugin.so
+remove_file $PRODUCTDIR/mediaviewer/platforminputcontexts/libqtvirtualkeyboardplugin.so
+
+# libQt5Quick.so.5()(64bit)
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqwebgl.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqwebgl.so
+
+# libQt5WaylandClient.so.5()(64bit)
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqwayland-egl.so
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqwayland-generic.so
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqwayland-xcomposite-egl.so
+remove_file $PRODUCTDIR/desktopeditors/platforms/libqwayland-xcomposite-glx.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqwayland-egl.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqwayland-generic.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqwayland-xcomposite-egl.so
+remove_file $PRODUCTDIR/mediaviewer/platforms/libqwayland-xcomposite-glx.so
+
 add_libs_requires
