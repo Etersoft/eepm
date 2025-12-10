cfdd43ff22c97250880bb2fe0147e1ece807b958
Vitaly Lipatov <lav@etersoft.ru>
2025-12-10 22:01:43 +0300

    epm play/repack synology-drive: update

--- play.d/synology-drive.sh
+++ play.d/synology-drive.sh
@@ -8,11 +8,17 @@ URL="https://www.synology.com/"
 
 . $(dirname $0)/common.sh
 
-is_stdcpp_enough "11.0" || VERSION="3.2.1" && info "libstdc++ version below 11.0, we'll stick with the old version $VERSION"
+is_stdcpp_enough "11.0" || VERSION="3.4.29" && info "libstdc++ version below 11.0, we'll stick with the old version $VERSION"
+
+if ! is_glibc_enough 2.34 ; then
+    fatal "Версия glibc слишком старая, требуется система с glibc 2.34 и выше."
+fi
+
+
+[ "$VERSION" = "*" ] && VERSION="[0-9]*"
 
 # it is so strange, package name contains 3.x.x, but package version is 7.x.x
-[ "$VERSION" = "*" ] && VERSION="[0-9]*" || VERSION="$(echo "$VERSION" | sed -e 's|^7|3|')"
-
+VERSION="$(echo "$VERSION" | sed -e 's|^7|3|' -e 's|^8|4')"
 
 urldir="$(eget --list https://archive.synology.com/download/Utility/SynologyDriveClient "/$VERSION-*" | head -n1)"
 [ -n "$urldir" ] || fatal "Can't get dir for $VERSION version on https://archive.synology.com/download/Utility/SynologyDriveClient"
--- repack.d/synology-drive.sh
+++ repack.d/synology-drive.sh
@@ -13,25 +13,37 @@ cd $BUILDROOT/$PRODUCTDIR || exit
 # disable autoupdate
 remove_file $PRODUCTDIR/package/cloudstation/bin/cloud-drive-auto-updater
 
-# Suggests
-# epm install --skip-installed libnautilus libnautilus-extension-compat
-filter_from_requires libnautilus
+# they miss libQt5Pdf.so.5
+remove_file $PRODUCTDIR/plugins/imageformats/libqpdf.so
+remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/imageformats/libqpdf.so
 
-# /usr/lib/nautilus/extensions-3.0/libnautilus-drive-extension.so: library libnautilus-extension.so.1 not found
-# but we have
-# libnautilus /usr/lib64/libnautilus-extension.so.4
+# libQt5Sql.so.5()(64bit)
+remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/sqldrivers/libqsqlite.so
+# libQt5QuickWidgets.so.5()(64bit)
+remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/designer/libqquickwidget.so
 
-remove_file /usr/lib/nautilus/extensions-4/libnautilus-drive-extension-4.so
+# libQt5Quick.so.5()(64bit)
+# libQt5Qml.so.5()(64bit)
+remove_dir $PRODUCTDIR/package/cloudstation/lib/plugins/qmltooling/
+#remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/qmltooling/libqmldbg_inspector.so
+#remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/qmltooling/libqmldbg_preview.so
+#remove_file $PRODUCTDIR/package/cloudstation/lib/plugins/qmltooling/libqmldbg_quickprofiler.so
+
+# support install on p10 or KDE based
+ignore_lib_requires libnautilus-extension.so.4
+
+move_file /usr/lib/nautilus/extensions-4/libnautilus-drive-extension-4.so /usr/lib64/nautilus/extensions-4/libnautilus-drive-extension-4.so
+# don't support legacy
 remove_file /usr/lib/nautilus/extensions-3.0/libnautilus-drive-extension.so
+# deb systems only
+remove_file /usr/lib/x86_64-linux-gnu/nautilus/extensions-4/libnautilus-drive-extension.so
 
-# fix ALT p10 install (TODO: restore for Sisyphus)
-#   synology-drive: Depends: libc.so.6(GLIBC_2.33)(64bit) but it is not installable
-#                  Depends: libc.so.6(GLIBC_2.34)(64bit) but it is not installable
-#                  Depends: libstdc++.so.6(GLIBCXX_3.4.29)(64bit) but it is not installable
-remove_file $PRODUCTDIR/package/cloudstation/icon-overlay/14/lib/plugin-cb-4.so || :
-# skip nautilus support
-remove_file $PRODUCTDIR/package/cloudstation/icon-overlay/14/lib/plugin-cb.so || :
+remove_dir /usr/lib
+remove_dir /usr/lib/x86_64-linux-gnu/
+
+# old nautilus
 remove_file $PRODUCTDIR/package/cloudstation/icon-overlay/15/lib/plugin-cb.so || :
+
 # TODO: parse
 # https://www.synology.com/api/support/findDownloadInfo?lang=ru-ru&product=DS2411%2B&major=6&minor=2
 
