47281de174d8061043253fcf9702a2dc3919a12e
Ivan Mazhukin <vanomj@altlinux.org>
2025-12-11 11:51:11 +0300

    epm play manuskript: fix repack (eterbug #17865)

--- repack.d/manuskript.sh
+++ repack.d/manuskript.sh
@@ -7,18 +7,19 @@ PRODUCT=manuskript
 
 . $(dirname $0)/common.sh
 
-move_to_opt
+for i in 16 32 64 128 256 512; do
+    install_file /usr/share/manuskript/icons/Manuskript/icon-${i}px.png /usr/share/icons/hicolor/${i}x${i}/apps/$PRODUCT.png
+done
 
+install_file /usr/share/manuskript/icons/Manuskript/$PRODUCT.svg /usr/share/icons/hicolor/scalable/apps/$PRODUCT.svg 
+
+move_to_opt
 
 subst "s|/usr/share/manuskript/|$PRODUCTDIR/|" usr/bin/manuskript
 
 fix_desktop_file /usr/bin/manuskript
-fix_desktop_file Icon=/usr/share/manuskript/icons/Manuskript/icon-512px.png $PRODUCT
+fix_desktop_file /usr/share/manuskript/icons/Manuskript/icon-512px.png $PRODUCT
 
-install_file /usr/share/manuskript/icons/Manuskript/icon-512px.png /usr/share/icons/hicolor/256x256/apps/$PRODUCT.png
-
-add_unirequires python3(enchant) python3(lxml) python3(markdown)
-add_unirequires python3(qt5) python3(qt5-webkit)
-add_unirequires libQt5Svg.so.5
+add_unirequires 'python3(PyQt5)' 'libQt5Svg.so.5'
 
 add_libs_requires
