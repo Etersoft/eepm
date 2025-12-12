4681071344d5ce03dcdfe1dd6924606244c9e841
Vitaly Lipatov <lav@etersoft.ru>
2025-12-12 05:49:36 +0300

    epm play davinci-resolve: fix args handling

--- play.d/davinci-resolve.sh
+++ play.d/davinci-resolve.sh
@@ -5,6 +5,8 @@ SUPPORTEDARCHES="x86_64"
 DESCRIPTION="Professional A/V post-production software suite from Blackmagic Design (stub for epm pack)"
 URL="https://www.blackmagicdesign.com"
 
+. $(dirname $0)/common.sh
+
 cat <<EOF
 Since Blackmagic provides the DaVinci Resolve download link only to registered users, we cannot create a complete play script. Therefore, to repackage DaVinci Resolve, run:
 
