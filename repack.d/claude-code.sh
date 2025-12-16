c6da71c9bdc89ea1117afe67b94af7de52a07b19
Vitaly Lipatov <lav@etersoft.ru>
2025-12-17 00:40:41 +0300

    epm play: add claude-code

---
+++ repack.d/claude-code.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+PRODUCT=claude
+PRODUCTDIR=/opt/claude.ai
+
+. $(dirname $0)/common.sh
+
+add_bin_link_command
+
+add_libs_requires
