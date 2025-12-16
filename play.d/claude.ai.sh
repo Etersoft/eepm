b6241e2ae7ec681794842af61c65aca90d73e2dc
Vitaly Lipatov <lav@etersoft.ru>
2025-12-17 00:40:41 +0300

    epm play: add claude.ai

---
+++ play.d/claude.ai.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+PKGNAME=claude
+SUPPORTEDARCHES="x86_64 aarch64"
+VERSION="$2"
+DESCRIPTION="Claude is a next generation AI assistant built by Anthropic"
+URL="https://claude.ai/"
+
+. $(dirname $0)/common.sh
+
+arch="$(epm print info -a)"
+case "$arch" in
+    x86_64)
+        arch=x64
+        ;;
+    aarch64)
+        arch=arm64
+        ;;
+esac
+
+# TODO: Darwin support
+os="linux"
+
+platform="${os}-${arch}"
+
+GCS_BUCKET="https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases"
+#GCS_BUCKET="$(eget -O- https://claude.ai/install.sh | grep "^GCS_BUCKET=" | sed -e 's|^GCS_BUCKET=||' -e 's|^"||' -e 's|"$||')"
+#[ -n "$GCS_BUCKET" ] || fatal "Can't download https://claude.ai/install.sh"
+
+if [ "$VERSION" = "*" ] ; then
+    VERSION="$(eget -O- "$GCS_BUCKET/stable")" || fatal "Can't get version from $GCS_BUCKET/stable"
+fi
+
+# ["platforms","linux-x64","checksum"]
+checksum="$(get_json_value "$GCS_BUCKET/$version/manifest.json" '["platforms","'$platform'","checksum"]')"
+
+PKGURL="$GCS_BUCKET/$VERSION/$platform/claude"
+
+# TODO: compare checksum
+
+install_pack_pkgurl $VERSION $checksum
