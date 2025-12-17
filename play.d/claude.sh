5db5aa0cc63ed88d598be2daf55898c820c570c9
Vitaly Lipatov <lav@etersoft.ru>
2025-12-17 21:36:09 +0300

    epm play claude: get GCS_BUCKET dynamically, fix typos

--- play.d/claude.sh
+++ play.d/claude.sh
@@ -23,16 +23,17 @@ os="linux"
 
 platform="${os}-${arch}"
 
-GCS_BUCKET="https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases"
-#GCS_BUCKET="$(eget -O- https://claude.ai/install.sh | grep "^GCS_BUCKET=" | sed -e 's|^GCS_BUCKET=||' -e 's|^"||' -e 's|"$||')"
-#[ -n "$GCS_BUCKET" ] || fatal "Can't download https://claude.ai/install.sh"
+#GCS_BUCKET="https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases"
+GCS_BUCKET="$(fetch_url -O- https://claude.ai/install.sh | grep "^GCS_BUCKET=" | sed -e 's|^GCS_BUCKET=||' -e 's|^"||' -e 's|"$||')"
+[ -n "$GCS_BUCKET" ] || fatal "Can't download https://claude.ai/install.sh"
 
 if [ "$VERSION" = "*" ] ; then
-    VERSION="$(eget -O- "$GCS_BUCKET/stable")" || fatal "Can't get version from $GCS_BUCKET/stable"
+    VERSION="$(fetch_url -O- "$GCS_BUCKET/stable")" || fatal "Can't get version from $GCS_BUCKET/stable"
+    [ -n "$VERSION" ] || fatal "Got empty version from $GCS_BUCKET/stable"
 fi
 
 # ["platforms","linux-x64","checksum"]
-checksum="$(get_json_value "$GCS_BUCKET/$version/manifest.json" '["platforms","'$platform'","checksum"]')"
+checksum="$(get_json_value "$GCS_BUCKET/$VERSION/manifest.json" '["platforms","'$platform'","checksum"]')" || fatal "Can't get checksum"
 
 PKGURL="$GCS_BUCKET/$VERSION/$platform/claude"
 
