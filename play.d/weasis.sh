8f43a08b22573e8e5022e67a97c6430fdb0d685c
Vitaly Lipatov <lav@etersoft.ru>
2025-12-24 20:24:38 +0300

    play.d/weasis: use native rpm package on rpm-based systems

--- play.d/weasis.sh
+++ play.d/weasis.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 PKGNAME=weasis
-SUPPORTEDARCHES="x86_64 aarch64 armhf"
+SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Weasis DICOM medical viewer"
 URL="https://github.com/nroduit/Weasis"
 
 . $(dirname $0)/common.sh
 
-arch="$(epm print info --debian-arch)"
-file="weasis_$VERSION-1_$arch.deb"
+arch="$(epm print info -a)"
+case "$(epm print info -p)-$arch" in
+    rpm-x86_64)
+        file="weasis-$VERSION-1.$arch.rpm"
+        ;;
+    *)
+        arch="$(epm print info --debian-arch)"
+        file="weasis_$VERSION-1_$arch.deb"
+        ;;
+esac
 
 PKGURL=$(eget --list --latest https://github.com/nroduit/Weasis/releases "$file")
 
