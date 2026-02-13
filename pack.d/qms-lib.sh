634b4de8b01520a009f18b39c328249f41f0df3f
Vitaly Lipatov <lav@etersoft.ru>
2026-02-14 01:29:39 +0300

    epm play: add qms-lib (internet speed and quality testing tool)

---
+++ pack.d/qms-lib.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+TAR="$1"
+RETURNTARNAME="$2"
+
+. $(dirname $0)/common.sh
+
+erc unpack "$TAR" || fatal
+
+mkdir -p opt/$PRODUCT
+mv qms_lib opt/$PRODUCT/qms_lib
+chmod 755 opt/$PRODUCT/qms_lib
+
+VERSION=$(opt/$PRODUCT/qms_lib --version 2>&1 | grep -oP '[0-9]+\.[0-9.]+' | head -1)
+[ -n "$VERSION" ] || VERSION="1.0"
+
+PKGNAME=$PRODUCT-$VERSION
+
+mkdir -p usr/bin
+ln -s /opt/$PRODUCT/qms_lib usr/bin/qms_lib
+
+cat <<EOF >$PKGNAME.tar.eepm.yaml
+name: $PRODUCT
+group: Networking/Other
+license: Proprietary
+url: https://www.qms.ru/applications/linux
+summary: QMS Lib - internet connection speed and quality testing tool
+description: Command-line tool for testing internet connection speed and quality from qms.ru.
+EOF
+
+erc pack $PKGNAME.tar opt usr || fatal
+
+return_tar $PKGNAME.tar
---
+++ play.d/qms-lib.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME=qms-lib
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="QMS Lib - internet connection speed and quality testing tool"
+URL="https://www.qms.ru/applications/linux"
+
+. $(dirname $0)/common.sh
+
+warn_version_is_not_supported
+
+PKGURL="https://lib.qms.ru/bin/linux/qms_lib.zip"
+
+install_pack_pkgurl
