c5acf4320553c89c61dc38c4b491151fcb48f606
Boris Yumankulov <boria138@altlinux.org>
2026-02-20 16:01:32 +0500

    epm play: added cli-proxy-api-plus (eterbug #18903)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ pack.d/cli-proxy-api-plus.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+TAR="$1"
+RETURNTARNAME="$2"
+VERSION="$3"
+URL="$4"
+
+. $(dirname $0)/common.sh
+
+[ -n "$VERSION" ] || VERSION="$(echo "$URL" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z.]+)?' | head -n1)"
+[ -n "$VERSION" ] || fatal "Can't get package version"
+
+PKGNAME=$PRODUCT-$VERSION
+
+mkdir -p usr/bin
+mkdir -p usr/share/doc/$PRODUCT
+erc unpack $TAR || fatal
+
+BASENAME=$(basename $1 .tar.gz)
+ARCH=$(echo "$BASENAME" | sed -E 's/^.*_linux_//')
+
+mv -v CLIProxyAPIPlus_${VERSION}_linux_${ARCH}/$PRODUCT usr/bin/$PRODUCT
+
+mv -v CLIProxyAPIPlus_${VERSION}_linux_${ARCH}/config.example.yaml usr/share/doc/$PRODUCT/config.example.yaml
+
+chmod 755 usr/bin/$PRODUCT
+
+cat <<EOF | create_file /usr/lib/systemd/user/$PRODUCT.service
+[Unit]
+Description=CLIProxyAPI Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/$PRODUCT --config %h/.cli-proxy-api/config.yaml
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=default.target
+EOF
+
+erc pack $PKGNAME.tar usr
+
+return_tar $PKGNAME.tar
---
+++ play.d/cli-proxy-api-plus.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PKGNAME=cli-proxy-api-plus
+SUPPORTEDARCHES="x86_64 aarch64"
+VERSION="$2"
+DESCRIPTION="Proxy server providing OpenAI/Gemini/Claude compatible API interfaces"
+URL="https://github.com/router-for-me/CLIProxyAPIPlus"
+
+. $(dirname $0)/common.sh
+
+arch=$(epm print info --debian-arch)
+
+PKGURL=$(get_github_url https://github.com/router-for-me/CLIProxyAPIPlus "CLIProxyAPIPlus_${VERSION}_linux_$arch.tar.gz" )
+
+install_pack_pkgurl
