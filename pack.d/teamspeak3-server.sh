351a8267d46291154d44b10b7b09f2fb448040d8
Vitaly Lipatov <lav@etersoft.ru>
2026-02-20 17:46:53 +0300

    epm pack.d/teamspeak3-server.sh: add missing pack script for server

---
+++ pack.d/teamspeak3-server.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+TAR="$1"
+RETURNTARNAME="$2"
+
+. $(dirname $0)/common.sh
+
+erc unpack $TAR || fatal
+
+mkdir -p opt/
+mv teamspeak3-server_linux_amd64 opt/$PRODUCT
+
+VERSION="$(echo $TAR | sed -e 's|.*amd64-||' -e 's|\.tar\.bz2||')"
+PKGNAME=$PRODUCT-$VERSION
+
+erc pack $PKGNAME.tar opt || fatal
+
+cat <<EOF >$PKGNAME.tar.eepm.yaml
+name: $PRODUCT
+group: Networking
+license: Proprietary
+url: https://www.teamspeak.com
+summary: TeamSpeak3 Server for Linux
+description: TeamSpeak is software for quality voice communication via the Internet.
+EOF
+
+return_tar $PKGNAME.tar
