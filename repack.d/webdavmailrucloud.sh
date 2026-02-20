48a2861f220672f955957757235e9206ff9ae0b4
Vitaly Lipatov <lav@etersoft.ru>
2026-02-20 17:08:35 +0300

    epm repack.d/webdavmailrucloud.sh: fix create_bin_exec_command renamed to create_exec_file

--- repack.d/webdavmailrucloud.sh
+++ repack.d/webdavmailrucloud.sh
@@ -7,9 +7,9 @@ PRODUCTDIR=/opt/WebDAVCloudMailRu
 
 . $(dirname $0)/common.sh
 
-cat <<EOF | create_bin_exec_command /usr/bin/wdmrc
+cat <<EOF | create_exec_file /usr/bin/wdmrc
 #!/bin/sh
-dotnet $PRODUCTDIR/wdmrc.dll "\$@"
+exec dotnet $PRODUCTDIR/wdmrc.dll "\$@"
 EOF
 
 add_requires dotnet-6.0
