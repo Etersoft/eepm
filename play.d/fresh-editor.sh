6430107e572a4ff12d9a2707bf9d4d24fbe07b21
Vitaly Lipatov <lav@etersoft.ru>
2026-02-22 17:35:38 +0300

    epm play fresh-editor: use constructname, add RELEASE, remove duplicate fresh.sh (fix #569)

--- play.d/fresh-editor.sh
+++ play.d/fresh-editor.sh
@@ -3,25 +3,28 @@
 PKGNAME=fresh-editor
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
+RELEASE="$3"
 DESCRIPTION="A lightweight, fast terminal-based text editor with LSP support and TypeScript plugins"
 URL="https://sinelaw.github.io/fresh/"
 
 . $(dirname $0)/common.sh
 
-if [ "$VERSION" = "*" ] ; then
-    VERSION="$(get_github_tag https://github.com/sinelaw/fresh)"
-fi
+pkgtype="$(epm print info -p)"
 
-pkgtype=$(epm print info -p)
-rpm_arch=$(epm print info -a)
-deb_arch=$(epm print info --debian-arch)
-case $pkgtype in
-    rpm)
-        PKGURL="https://github.com/sinelaw/fresh/releases/download/v${VERSION}/fresh-editor-${VERSION}-1.${rpm_arch}.rpm"
+case "$pkgtype" in
+    rpm|deb)
         ;;
     *)
-        PKGURL="https://github.com/sinelaw/fresh/releases/download/v${VERSION}/fresh-editor-${VERSION}-1.${deb_arch}.deb"
+        pkgtype=rpm
         ;;
 esac
+
+# https://github.com/sinelaw/fresh/releases/download/v0.1.88/fresh-editor-0.1.88-1.x86_64.rpm
+# https://github.com/sinelaw/fresh/releases/download/v0.1.88/fresh-editor_0.1.88-1_amd64.deb
+if [ "$VERSION" = "*" ] || [ "$RELEASE" = "*" ] ; then
+    PKGURL="$(get_github_url sinelaw/fresh "$(epm print constructname $PKGNAME "$VERSION-*")")"
+else
+    PKGURL="https://github.com/sinelaw/fresh/releases/download/v$VERSION/$(epm print constructname $PKGNAME "$VERSION-$RELEASE")"
+fi
 
 install_pkgurl
