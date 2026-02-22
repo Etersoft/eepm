5ee46fa8cb2d252668bc8c0228930f35fb8d9b1e
Vitaly Lipatov <lav@etersoft.ru>
2026-02-22 19:31:49 +0300

    epm play openide: add EAP edition support via epm play openide=eap (fix #458)

--- play.d/openide.sh
+++ play.d/openide.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-PKGNAME=openIDE
+BASEPKGNAME=openIDE
+PRODUCTALT="'' eap"
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="openIDE - Free IDE based on IntelliJ IDEA Community Edition"
 URL="https://openide.ru/"
+TIPS="Run 'epm play openide=eap' to install EAP version."
 
 . $(dirname $0)/common.sh
 
@@ -18,15 +20,26 @@ case "$arch" in
         ;;
 esac
 
+if [ "$BRANCH" = "eap" ] ; then
+    eap="-eap"
+    download_page="https://openide.ru/download-eap/"
+    # tarball always extracts as openIDE-VERSION/, so repack creates openIDE package
+    override_pkgname "$BASEPKGNAME"
+else
+    eap=""
+    download_page="https://openide.ru/download/"
+fi
+
 if [ "$VERSION" = "*" ]; then
-    # stable version
-    VERSION="$(eget -q -O- https://openide.ru/download/ | grep "Сборка:" | sed -e "s|.*Сборка:</span>[[:space:]]||" -e "s|</p>.*||")"
+    VERSION="$(eget -q -O- "$download_page" | grep "Сборка:" | sed -e "s|.*Сборка:</span>[[:space:]]||" -e "s|</p>.*||")"
     if [ -z "$VERSION" ] ; then
-        # latest may be unstable
-        VERSION="$(eget -q -O- https://download.openide.ru/ | grep -B1 "openIDE.*tar\.gz" | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" | tail -n1)"
+        VERSION="$(eget -q -O- https://download.openide.ru/ | grep -o "openIDE-[0-9.]*${eap}\.tar\.gz" | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]*" | sort -V | tail -n1)"
     fi
 fi
 
-PKGURL="https://download.openide.ru/$VERSION/openIDE-$VERSION${arch}.tar.gz"
+# https://download.openide.ru/252.27397.103.1/openIDE-252.27397.103.1.tar.gz
+# https://download.openide.ru/253.28294.334.1/openIDE-253.28294.334.1-eap.tar.gz
+# https://download.openide.ru/253.28294.334.1/openIDE-253.28294.334.1-eap-aarch64.tar.gz
+PKGURL="https://download.openide.ru/$VERSION/openIDE-$VERSION${eap}${arch}.tar.gz"
 
 install_pkgurl
