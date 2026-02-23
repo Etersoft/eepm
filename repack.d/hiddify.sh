d50264422cfd19abe0c2bc19f8597d46a185dbe8
Vitaly Lipatov <lav@etersoft.ru>
2026-02-23 23:37:08 +0300

    epm repack hiddify: fix CURL_OPENSSL_4 version mismatch with libcurl4-openssl wrapper

--- repack.d/hiddify.sh
+++ repack.d/hiddify.sh
@@ -10,5 +10,13 @@ SPEC="$2"
 sed -i -e 's/^\(Version: [^+]*\)+.*/\1/' $SPEC
 
 move_to_opt
-add_bin_link_command
+
+# libsentry.so requires CURL_OPENSSL_4, but system libcurl provides CURL_GNUTLS_4
+ignore_lib_requires libcurl.so.4
+add_requires libcurl4-openssl
+cat <<'EOF' | create_exec_file "/usr/bin/hiddify"
+#!/bin/sh
+export LD_LIBRARY_PATH=/usr/lib64/libcurl4-openssl${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+exec /opt/hiddify/hiddify "$@"
+EOF
 
