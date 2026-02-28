871f9bcf94eb7bd4ed982f16c91e9afbfcf0d7ce
Vitaly Lipatov <lav@etersoft.ru>
2026-02-28 21:28:47 +0300

    epm repack aksusbd: add aarch64, armhf, x86 support

--- repack.d/aksusbd.sh
+++ repack.d/aksusbd.sh
@@ -7,21 +7,51 @@ SPEC="$2"
 
 add_unirequires udev
 
-install_file /var/hasplm/init/hasplmd.service /etc/systemd/system/hasplmd.service
-install_file /var/hasplm/init/aksusbd.service /etc/systemd/system/aksusbd.service
+arch="$(epm print info -a)"
 
-chmod -v u+w $BUILDROOT/usr/sbin/*
+case "$arch" in
+    x86_64)
+        install_file /var/hasplm/init/hasplmd.service /etc/systemd/system/hasplmd.service
+        install_file /var/hasplm/init/aksusbd.service /etc/systemd/system/aksusbd.service
 
-move_file /usr/sbin/aksusbd_x86_64 /usr/sbin/aksusbd
-move_file /usr/sbin/hasplmd_x86_64 /usr/sbin/hasplmd
+        chmod -v u+w $BUILDROOT/usr/sbin/*
 
-subst "s|aksusbd_x86_64|aksusbd|g" $BUILDROOT/etc/udev/rules.d/80-hasp.rules
+        move_file /usr/sbin/aksusbd_x86_64 /usr/sbin/aksusbd
+        move_file /usr/sbin/hasplmd_x86_64 /usr/sbin/hasplmd
 
-mkdir -p $BUILDROOT/etc/init.d/
-remove_dir /etc/init.d
+        subst "s|aksusbd_x86_64|aksusbd|g" $BUILDROOT/etc/udev/rules.d/80-hasp.rules
+
+        mkdir -p $BUILDROOT/etc/init.d/
+        remove_dir /etc/init.d
+        ;;
+    x86)
+        install_file /var/hasplm/init/hasplmd.service /etc/systemd/system/hasplmd.service
+        install_file /var/hasplm/init/aksusbd.service /etc/systemd/system/aksusbd.service
+
+        chmod -v u+w $BUILDROOT/usr/sbin/*
+
+        mkdir -p $BUILDROOT/etc/init.d/
+        remove_dir /etc/init.d
+        ;;
+    aarch64)
+        install_file /var/hasplm/init/hasplmd_arm64.service /etc/systemd/system/hasplmd.service
+        subst "s|hasplmd_arm64|hasplmd|g" $BUILDROOT/etc/systemd/system/hasplmd.service
+
+        chmod -v u+w $BUILDROOT/usr/sbin/*
+
+        move_file /usr/sbin/hasplmd_arm64 /usr/sbin/hasplmd
+        ;;
+    armhf)
+        install_file /var/hasplm/init/hasplmd_armhf.service /etc/systemd/system/hasplmd.service
+        subst "s|hasplmd_armhf|hasplmd|g" $BUILDROOT/etc/systemd/system/hasplmd.service
+
+        chmod -v u+w $BUILDROOT/usr/sbin/*
+
+        move_file /usr/sbin/hasplmd_armhf /usr/sbin/hasplmd
+        ;;
+esac
 
 touch $BUILDROOT/etc/hasplm/nethasp.ini
 pack_file /etc/hasplm/nethasp.ini
 
 remove_dir /var/hasplm/init
-
