d65fc42a243dcb92194e1b63cfb6449bc838b643
Vitaly Lipatov <lav@etersoft.ru>
2026-02-28 14:57:15 +0300

    epm play: add chitubox-basic (SLA/DLP/LCD slicer for 3D printing)

---
+++ play.d/chitubox-basic.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME=chitubox-basic
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="All-in-one SLA/DLP/LCD slicer for 3D printing"
+URL="https://www.chitubox.com/"
+
+. $(dirname $0)/common.sh
+
+warn_version_is_not_supported
+
+PKGURL="https://sac.chitubox.com/software/download.do?installerUrl=https%3A%2F%2Fdownload.chitubox.com%2F17839%2Fv${VERSION}%2FCHITUBOX_Basic_linux_Installer_${VERSION}&softwareId=17839&softwareVersionId=v${VERSION}"
+
+install_pack_pkgurl
