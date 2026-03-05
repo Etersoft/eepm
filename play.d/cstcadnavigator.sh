4f2a612930bffbfbb145b3407d2b14e663debb85
Vitaly Lipatov <lav@etersoft.ru>
2026-03-05 11:59:11 +0300

    epm play cstcadnavigator: fix doubled /download/ in URL

--- play.d/cstcadnavigator.sh
+++ play.d/cstcadnavigator.sh
@@ -12,7 +12,8 @@ URL="https://cadsofttools.ru/products/cst-cad-navigator/download/"
 # Используются одни и те же бинарники в deb и rpm
 # но файл rpm с версией, поэтому нам предпочтительнее для истории
 if [ "$VERSION" = "*" ] ; then
-    PKGURL="$(eget --list --latest https://cadsofttools.ru/products/cst-cad-navigator/download/ "$PKGNAME-*x86_64.rpm" )"
+    # eget resolves relative links from the page, which may produce doubled /download/download/
+    PKGURL="$(eget --list --latest https://cadsofttools.ru/products/cst-cad-navigator/download/ "$PKGNAME-*x86_64.rpm" | sed 's|.*/download/|https://cadsofttools.ru/download/|')"
 else
     PKGURL="https://cadsofttools.ru/download/cstcadnavigator-${VERSION}-${RELEASE}.x86_64.rpm"
 fi
