ce125dfd146a63dc4edf90e835597900b173731d
Vitaly Lipatov <lav@etersoft.ru>
2026-03-09 20:53:57 +0300

    epm play chitubox-basic: fix version

--- play.d/chitubox-basic.sh
+++ play.d/chitubox-basic.sh
@@ -8,6 +8,9 @@ URL="https://www.chitubox.com/"
 
 . $(dirname $0)/common.sh
 
+# See https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=chitubox-free-bin
+VERSION=2.3.1
+
 PKGURL="https://sac.chitubox.com/software/download.do?installerUrl=https%3A%2F%2Fdownload.chitubox.com%2F17839%2Fv${VERSION}%2FCHITUBOX_Basic_linux_Installer_${VERSION}&softwareId=17839&softwareVersionId=v${VERSION}"
 
 install_pack_pkgurl
