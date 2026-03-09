fadb88432de51de78430935b1093036ad1c1903f
Vitaly Lipatov <lav@etersoft.ru>
2026-03-09 14:45:55 +0300

    epm play neovide: use get_github_url, add direct download URL branch

--- play.d/neovide.sh
+++ play.d/neovide.sh
@@ -4,11 +4,15 @@ PKGNAME=neovide
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION="No Nonsense Neovim Client in Rust from the official site"
-URL="https://neovide.dev/"
+URL="https://github.com/neovide/neovide"
 
 . $(dirname $0)/common.sh
 
-PKGURL=$(eget --list --latest https://github.com/neovide/neovide/releases "$PKGNAME.AppImage")
+if [ "$VERSION" = "*" ] ; then
+    PKGURL=$(get_github_url "$URL" "$PKGNAME.AppImage")
+else
+    PKGURL="$URL/releases/download/$VERSION/$PKGNAME.AppImage"
+fi
 
 install_pkgurl
 
