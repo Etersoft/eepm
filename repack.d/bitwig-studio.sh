ad49705958d33e31b5b231ea675238572d4a16c2
Vitaly Lipatov <lav@etersoft.ru>
2026-03-13 02:18:39 +0300

    epm repack bitwig-studio: ignore 32-bit deps from BitwigPluginHost-X86-SSE41

--- repack.d/bitwig-studio.sh
+++ repack.d/bitwig-studio.sh
@@ -8,3 +8,8 @@ SPEC="$2"
 ignore_lib_requires 'libgif.so.7()(64bit)' 'libnet.so()(64bit)' 'libjvm.so()(64bit)' 'libjli.so()(64bit)' \
 'libjava.so()(64bit)' 'libawt_xawt.so()(64bit)' 'libawt.so()(64bit)'
 
+# BitwigPluginHost-X86-SSE41 is a 32-bit binary for running 32-bit VST plugins
+# ignore its deps to avoid pulling i586 packages
+ignore_lib_requires 'libm.so.6' 'libxcb-icccm.so.4' 'libxcb-util.so.1' 'libxcb.so.1' \
+'libxkbcommon.so.0' 'libX11.so.6' 'libatomic.so.1' 'libc.so.6' 'ld-linux.so.2'
+
