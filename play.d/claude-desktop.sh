60f60d3bb77d9a54cd6455a0b4db07ecbdc8f4e2
Boris Yumankulov <boria138@altlinux.org>
2026-03-14 15:00:10 +0500

    epm play: added native claude-code-desktop (eterbug #18971)
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- play.d/claude-desktop.sh
+++ play.d/claude-desktop.sh
@@ -3,25 +3,27 @@
 PKGNAME=claude-desktop
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
-DESCRIPTION="Claude Desktop (Wine based)"
-URL="https://claude.com/download"
+DESCRIPTION="Claude Desktop (Unofficial native client)"
+URL="https://github.com/aaddrick/claude-desktop-debian"
 
 . $(dirname $0)/common.sh
 
-if ! is_command wine ; then
-    epm play wine || fatal
-fi
+pkgtype="$(epm print info -p)"
+
+case $pkgtype in
+    rpm)
+        pkgformat="rpm"
+        arch=$(epm print info -a)
+        ;;
+    *)
+        pkgformat="deb"
+        arch=$(epm print info --debian-arch)
+        ;;
+esac
 
 warn_version_is_not_supported
 
-#https://claude.ai/redirect/claudedotcom.v1.1029bab3-186e-4985-87e7-b2a711d887b4/api/desktop/win32/arm64/exe/latest/redirect
-#https://claude.ai/redirect/claudedotcom.v1.1029bab3-186e-4985-87e7-b2a711d887b4/api/desktop/win32/x64/exe/latest/redirect
+# TAG v1.3.17%2Bclaude1.1.5749 create broken VERSION drop this
+PKGURL=$(get_github_url https://github.com/aaddrick/claude-desktop-debian "claude-desktop*$arch.$pkgformat")
 
-# https://claude.ai/api/desktop/win32/x64/exe/latest/redirect
-# https://claude.ai/api/desktop/win32/arm64/exe/latest/redirect
-
-VERSION="1.1029"
-#PKGURL="https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe"
-PKGURL="ipfs://Qme1jkcU95P5dCWTs5sMxsvUruBfaVQMrAhY2585AfsoVJ?filename=Claude-Setup-x64.exe"
-
-install_pack_pkgurl $VERSION
+install_pkgurl
--- repack.d/claude-desktop.sh
+++ repack.d/claude-desktop.sh
@@ -1,30 +1,15 @@
 #!/bin/sh -x
-# It will run with two args: buildroot spec
+
+# It will be run with two args: buildroot spec
 BUILDROOT="$1"
-
 SPEC="$2"
-
-PRODUCT=claude-desktop
-PRODUCTDIR=/opt/eepm-wine/$PRODUCT
 
 . $(dirname $0)/common.sh
 
-add_requires '/usr/bin/wine'
+move_to_opt /usr/lib/claude-desktop
 
-add_bin_link_command $PRODUCT $PRODUCTDIR/run.sh
+subst 's|/usr/lib/claude-desktop|/opt/claude-desktop|g' $BUILDROOT/usr/bin/$PRODUCT
 
-cat <<EOF | create_file /usr/share/applications/$PRODUCT.desktop
-[Desktop Entry]
-Name=Claude Desktop
-Exec=$PRODUCT %u
-Type=Application
-StartupNotify=true
-Icon=$PRODUCT
-StartupWMClass=claude.exe
-Categories=Wine;Network;InstantMessaging;
-MimeType=x-scheme-handler/claude;
-EOF
+fix_desktop_file /usr/bin/$PRODUCT $PRODUCT
 
-# copied from .local/share/icons/hicolor/256x256/apps/84BD_claude.0.png
-i=256
-install_file ipfs://QmbnRtSF3e5dENA6TGKADxpzNXMF5vV1UDaRGrKsQXu4xZ /usr/share/icons/hicolor/${i}x${i}/apps/$PRODUCT.png
+add_electron_deps
