264cdf187f26314ce057266375516ebbefb2a054
Vitaly Lipatov <lav@etersoft.ru>
2026-03-15 19:24:09 +0300

    epm play: add telega (Telega messenger)

---
+++ play.d/telega.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME=Telega
+SUPPORTEDARCHES="x86_64"
+VERSION="$2"
+DESCRIPTION="Telega messenger from the official site"
+URL="https://telega.me"
+
+. $(dirname $0)/common.sh
+
+warn_version_is_not_supported
+
+PKGURL=$(get_github_url "Telegru/tdesktop" "Telega_Linux.tar.xz")
+
+install_pack_pkgurl
