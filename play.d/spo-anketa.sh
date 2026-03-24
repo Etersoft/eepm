d3b718cb601f984dec96ab3d19be45022b21c68c
Vitaly Lipatov <lav@etersoft.ru>
2026-03-24 20:30:19 +0300

    epm play spo-anketa: update to version 1.0.2

--- play.d/spo-anketa.sh
+++ play.d/spo-anketa.sh
@@ -11,10 +11,10 @@ URL="https://gossluzhba.gov.ru/spo/"
 warn_version_is_not_supported
 
 # Download URL for Linux version from gossluzhba.gov.ru
-# Version 1.0.1 from 21.01.2026
-PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/4f447535-4219-46bf-8bc0-fc4fa18968e5"
+# Version 1.0.2 from 25.02.2026
+PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/94f919b3-eccc-4bc1-a838-4913bb92b1cc"
 
-# Checksum for version 1.0.1 (2026-01-21) - update when new version is released
-PKGSUM="sha256:12083b9c788772afde7d00872812daeb863fed4da0ac254e0da122e10b6e29c0"
+# Checksum for version 1.0.2 (2026-02-25) - update when new version is released
+PKGSUM="sha256:b2d4ff805429020a5640b8ee574e1dd6289704cded5729b942ddb1ee87ab97d2"
 
 install_pack_pkgurl "" "$PKGSUM"
