9ff39088ccb31e635372b0d341ad3ac489d6542b
Vitaly Lipatov <lav@etersoft.ru>
2026-03-31 22:48:03 +0300

    epm repack: fix pdfsam-basic (ignore bundled ffmpeg), lidarr (remap chromaprint dep)

---
+++ repack.d/lidarr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+# It will be run with two args: buildroot spec
+BUILDROOT="$1"
+SPEC="$2"
+
+. $(dirname $0)/common.sh
+
+# original deb depends on libchromaprint-tools, on ALT it's fpcalc
+filter_from_requires libchromaprint-tools
+add_unirequires fpcalc
--- repack.d/pdfsam-basic.sh
+++ repack.d/pdfsam-basic.sh
@@ -6,5 +6,10 @@ SPEC="$2"
 
 . $(dirname $0)/common.sh
 
+# bundled FFmpeg plugins link against all libavcodec/libavformat versions (54-61)
+# only one will be used at runtime depending on system ffmpeg version
+ignore_lib_requires 'libavcodec.*'
+ignore_lib_requires 'libavformat.*'
+
 chmod +x $BUILDROOT/opt/pdfsam-basic/runtime/bin/java
 
