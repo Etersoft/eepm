14c2014dcb048abaca08972ad4e05d47b2f5fb57
Vitaly Lipatov <lav@etersoft.ru>
2026-03-31 15:49:55 +0300

    epm play rider: fix PKGNAME to match actual package name JetBrains.Rider

--- play.d/rider.sh
+++ play.d/rider.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-PKGNAME=Rider
+PKGNAME=JetBrains.Rider
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Rider - A cross-platform IDE for .NET and game dev from the official site"
-URL="https://www.jetbrains.com/clion/"
+URL="https://www.jetbrains.com/rider/"
 
 . $(dirname $0)/common-jetbrains.sh
-
-# name for download
-# FIXME: by some reason alien makes Rider from JetBrains.Rider
-PKGNAME=JetBrains.Rider
 PKGURL="$(get_jetbrains_pkgurl RD rider)"
 
 install_pkgurl
