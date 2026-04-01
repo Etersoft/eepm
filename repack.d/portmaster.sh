ca10c09e650d7ab198d9270c3d8fb2030da4476a
Vitaly Lipatov <lav@etersoft.ru>
2026-04-01 22:51:45 +0300

    epm repack portmaster: move data/logs from /opt to /var/lib/portmaster

--- repack.d/portmaster.sh
+++ repack.d/portmaster.sh
@@ -15,17 +15,39 @@ remove_file $PRODUCTDIR/portmaster.desktop
 install -D -m644 .$PRODUCTDIR/portmaster_notifier.desktop ./usr/share/applications/portmaster_notifier.desktop
 remove_file $PRODUCTDIR/portmaster_notifier.desktop
 
+DATADIR=/var/lib/portmaster
+
 cat <<EOF | create_exec_file /usr/bin/$PRODUCT
 #!/bin/sh
-exec $PRODUCTDIR/portmaster-start app --data=$PRODUCTDIR "\$@"
+exec $PRODUCTDIR/portmaster-start app --data=$DATADIR "\$@"
 EOF
 
-echo "Dowloading ... "
-.$PRODUCTDIR/portmaster-start --data $BUILDROOT$PRODUCTDIR update || fatal
+# fix systemd service to use /var/lib/portmaster for data
+subst "s|--data /opt/safing/portmaster|--data $DATADIR|g" $BUILDROOT/lib/systemd/system/portmaster.service
+subst "s|PIDFile=/opt/safing/portmaster|PIDFile=$DATADIR|" $BUILDROOT/lib/systemd/system/portmaster.service
 
-p="$(basename $(dirname $PRODUCTDIR/updates/linux_amd64/app/portmaster-app_v*/chrome-sandbox))"
-fix_chrome_sandbox $PRODUCTDIR/updates/linux_amd64/app/$p/chrome-sandbox
+# download components at repack time
+echo "Downloading components ..."
+.$PRODUCTDIR/portmaster-start --data $BUILDROOT$DATADIR update || fatal
 
-pack_file $PRODUCTDIR/updates
+p="$(basename $(dirname $BUILDROOT$DATADIR/updates/linux_amd64/app/portmaster-app_v*/chrome-sandbox))"
+fix_chrome_sandbox $DATADIR/updates/linux_amd64/app/$p/chrome-sandbox
+
+# block runtime auto-updates: replace portmaster-start with wrapper
+mv $BUILDROOT$PRODUCTDIR/portmaster-start $BUILDROOT$PRODUCTDIR/portmaster-start.real
+cat <<'WRAPPER' | create_exec_file $PRODUCTDIR/portmaster-start
+#!/bin/sh
+case "$1" in
+    update)
+        echo "Auto-update is disabled in the packaged version. Update via: epm play portmaster"
+        exit 0
+        ;;
+esac
+exec /opt/safing/portmaster/portmaster-start.real "$@"
+WRAPPER
+
+# portmaster-start initializes logs dir even in app mode
+mkdir -p $BUILDROOT$DATADIR/logs
+pack_file $DATADIR
 
 add_electron_deps
