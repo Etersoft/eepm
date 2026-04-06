28444012900a2aa99956a9dc2fcaf83e33b54095
Vitaly Lipatov <lav@etersoft.ru>
2026-04-06 12:34:06 +0300

    Revert "epm play: add direct URLs for rocketchat, pstube, powershell to avoid API calls"
    This reverts commit 20575bcdfa06b8e0f61521db7f64383d2ec86e2e.

--- play.d/rocketchat.sh
+++ play.d/rocketchat.sh
@@ -11,10 +11,6 @@ URL="https://github.com/RocketChat/Rocket.Chat.Electron"
 arch=amd64
 pkgtype=deb
 
-if [ "$VERSION" = "*" ] ; then
-    PKGURL=$(eget --list --latest https://github.com/RocketChat/Rocket.Chat.Electron/releases/ "$PKGNAME*$arch.$pkgtype")
-else
-    PKGURL="https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$VERSION/${PKGNAME}_${VERSION}_${arch}.${pkgtype}"
-fi
+PKGURL=$(eget --list --latest https://github.com/RocketChat/Rocket.Chat.Electron/releases/ "$PKGNAME*$VERSION*$arch.$pkgtype")
 
 install_pkgurl
