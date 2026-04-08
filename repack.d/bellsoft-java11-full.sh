f850f3709ae0ed36d1021f6b61654157808ecdf5
Vitaly Lipatov <lav@etersoft.ru>
2026-04-08 23:03:06 +0300

    epm repack bellsoft-java11-full: ignore unavailable ffmpeg library versions

--- repack.d/bellsoft-java11-full.sh
+++ repack.d/bellsoft-java11-full.sh
@@ -6,4 +6,7 @@ SPEC="$2"
 
 . $(dirname $0)/common.sh
 
+# JavaFX media links against multiple ffmpeg versions (54-59), none available in Sisyphus
+ignore_lib_requires 'libavcodec.*' 'libavformat.*' 'libavcodec-ffmpeg.*' 'libavformat-ffmpeg.*'
+
 add_unirequires libX11.so.6 libXext.so.6 libXi.so.6 libXrender.so.1 libXtst.so.6 libasound.so.2 libfreetype.so.6
