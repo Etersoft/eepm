1c9b4246496fbe6fc4efc8019624673e28c42598
Vitaly Lipatov <lav@etersoft.ru>
2026-04-08 23:03:11 +0300

    epm repack plex-desktop: ignore bundled libedit, libwrap and libapparmor

--- repack.d/plex-desktop.sh
+++ repack.d/plex-desktop.sh
@@ -6,5 +6,8 @@ SPEC="$2"
 
 . $(dirname $0)/common.sh
 
+# bundled dependencies not available in Sisyphus
+ignore_lib_requires 'libedit.so.2' 'libwrap.so.0' 'libapparmor.so.1'
+
 add_bin_link_command $PRODUCT $PRODUCTDIR/Plex.sh
 
