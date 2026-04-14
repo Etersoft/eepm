c445fd74ab3b09724d9e4f4ae421defed70d6d56
Vitaly Lipatov <lav@etersoft.ru>
2026-04-15 00:37:47 +0300

    epm play synology-chat: use major version for directory search (fix ~ in version)

--- play.d/synology-chat.sh
+++ play.d/synology-chat.sh
@@ -8,6 +8,10 @@ URL="https://synology.com"
 
 . $(dirname $0)/common.sh
 
+if [ "$VERSION" != "*" ] ; then
+    # VERSION from app-versions: 1.2.3~232 -> use only major version for directory search
+    VERSION="$(echo "$VERSION" | sed 's|[~-].*||')"
+fi
 [ "$VERSION" = "*" ] && VERSION="[0-9]*"
 
 urldir="$(eget --list https://archive.synology.com/download/Utility/ChatClient "/$VERSION-*" | head -n1)"
