4d2cf4c03209e866bbc330f6e0d01bb2923fd9b1
Vitaly Lipatov <lav@etersoft.ru>
2026-04-17 23:22:32 +0300

    epm play metasploit-framework: build direct URL from VERSION+RELEASE, fallback to scraping

--- play.d/metasploit-framework.sh
+++ play.d/metasploit-framework.sh
@@ -3,6 +3,7 @@
 PKGNAME=metasploit-framework
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
+RELEASE="$3"
 DESCRIPTION="Penetration testing framework from Rapid7"
 URL="https://www.metasploit.com/"
 
@@ -10,6 +11,12 @@ URL="https://www.metasploit.com/"
 
 warn_version_is_not_supported
 
-PKGURL=$(eget --list --latest https://rpm.metasploit.com/ "metasploit-framework-*.x86_64.rpm")
+# file: metasploit-framework-VERSION-RELEASE.x86_64.rpm (e.g. 6.4.126~...~1rapid7-1.fedora30.x86_64.rpm)
+PKGURL=""
+if [ -n "$RELEASE" ] && [ "$VERSION" != "*" ] ; then
+    PKGURL="https://rpm.metasploit.com/metasploit-omnibus/pkg/metasploit-framework-${VERSION}-${RELEASE}.x86_64.rpm"
+    eget --check-url "$PKGURL" >/dev/null 2>&1 || PKGURL=""
+fi
+[ -n "$PKGURL" ] || PKGURL=$(eget --list --latest https://rpm.metasploit.com/ "metasploit-framework-*.x86_64.rpm")
 
 install_pkgurl
