604217696f422e58005ab2d028af06a069ce01e9
Vitaly Lipatov <lav@etersoft.ru>
2026-04-17 23:22:32 +0300

    epm play ridoclnx: fix URL mask to include build number between VERSION and distro suffix

--- play.d/ridoclnx.sh
+++ play.d/ridoclnx.sh
@@ -3,6 +3,7 @@
 PKGNAME=ridoclnx
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
+RELEASE="$3"
 DESCRIPTION="RiDocLNX - scanner software for Linux"
 URL="https://riman.ru/ridoclnx"
 
@@ -10,36 +11,34 @@ URL="https://riman.ru/ridoclnx"
 
 warn_version_is_not_supported
 
+# Filename: ridoclnx-VERSION-RELEASE.arch.rpm where RELEASE = build<distroSuffix> (e.g. 7.9P11ALT)
+# If we have RELEASE from app-versions and it matches the current distro suffix, use direct URL
+# Otherwise glob the build between VERSION and the suffix
 case $(epm print info -e) in
-    AstraLinuxSE/1.8)
-        mask="ridoclnx_${VERSION}astra18.deb"
-        ;;
+    AstraLinuxSE/1.8)            suffix=astra18; ext=deb; sep=_ ;;
     AstraLinuxSE/1.7|AstraLinuxSE/1.7.5|AstraLinuxCE/1.6)
-        mask="ridoclnx_${VERSION}astra17.deb"
-        ;;
-    RedOS/8*)
-        mask="ridoclnx-${VERSION}REDOS8.x86_64.rpm"
-        ;;
-    MSVSphere/*)
-        mask="ridoclnx-${VERSION}REDOS8.x86_64.rpm"
-        ;;
-    RedOS/7*)
-        mask="ridoclnx-${VERSION}REDOS7.x86_64.rpm"
-        ;;
+                                 suffix=astra17; ext=deb; sep=_ ;;
+    RedOS/8*|MSVSphere/*)        suffix=REDOS8;  ext=rpm; sep=- ;;
+    RedOS/7*)                    suffix=REDOS7;  ext=rpm; sep=- ;;
     ALTLinux/p11|ALTLinux/Sisyphus)
-        mask="ridoclnx-${VERSION}P11ALT.x86_64.rpm"
-        ;; 
-    ALTLinux/*|CentOS/*)
-        mask="ridoclnx-${VERSION}ALT.x86_64.rpm"
-        ;;
-    Ubuntu/*)
-        mask="ridoclnx_${VERSION}ubuntu22.deb"
-        ;;
-    Debian/*)
-        mask="ridoclnx_${VERSION}debian12.deb"
+                                 suffix=P11ALT;  ext=rpm; sep=- ;;
+    ALTLinux/*|CentOS/*)         suffix=ALT;     ext=rpm; sep=- ;;
+    Ubuntu/*)                    suffix=ubuntu22; ext=deb; sep=_ ;;
+    Debian/*)                    suffix=debian12; ext=deb; sep=_ ;;
+    *)                           suffix=ubuntu22; ext=deb; sep=_ ;;
+esac
+
+archpart=""
+[ "$ext" = "rpm" ] && archpart=".x86_64"
+
+case "$RELEASE" in
+    *${suffix})
+        # release matches our distro: build deterministic filename for IPFS lookup
+        mask="ridoclnx${sep}${VERSION}-${RELEASE}${archpart}.${ext}"
         ;;
     *)
-        mask="ridoclnx_${VERSION}ubuntu22.deb"
+        # glob the build number for scraping
+        mask="ridoclnx${sep}${VERSION}-*${suffix}${archpart}.${ext}"
         ;;
 esac
 
