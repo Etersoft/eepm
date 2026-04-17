68f1e089be70773d9f74d99e79b9bf6b8c95e20d
Vitaly Lipatov <lav@etersoft.ru>
2026-04-17 23:22:22 +0300

    epm play tailscale: use warn_version_is_not_supported, old versions not kept

--- play.d/tailscale.sh
+++ play.d/tailscale.sh
@@ -8,9 +8,11 @@ URL="https://tailscale.com/"
 
 . $(dirname $0)/common.sh
 
-if [ "$VERSION" = "*" ] ; then
-    VERSION=$(get_github_tag tailscale/tailscale)
-fi
+# pkgs.tailscale.com only keeps the latest version
+warn_version_is_not_supported
+
+VERSION=$(get_github_tag tailscale/tailscale)
+[ -n "$VERSION" ] || fatal "Can't get version"
 
 PKGURL="https://pkgs.tailscale.com/stable/fedora/x86_64/tailscale_${VERSION}_x86_64.rpm"
 
