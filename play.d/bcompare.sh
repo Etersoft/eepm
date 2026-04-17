a46b930b883c6d57f477a8a0a8ab7c6c69362f11
Vitaly Lipatov <lav@etersoft.ru>
2026-04-17 23:22:11 +0300

    epm play bcompare: use RELEASE for build number in URL

--- play.d/bcompare.sh
+++ play.d/bcompare.sh
@@ -3,18 +3,21 @@
 PKGNAME=bcompare
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
+RELEASE="$3"
 DESCRIPTION="Beyond Compare 5: Compare, sync, and merge files and folders for Linux from the official site"
 URL="https://www.scootersoftware.com/"
 
 . $(dirname $0)/common.sh
 
+# RELEASE is the build number, joined with VERSION via dot
+[ -n "$RELEASE" ] && [ "$VERSION" != "*" ] && FULLVERSION="${VERSION}.${RELEASE}" || FULLVERSION="$VERSION"
 
 case "$(epm print info -p)" in
   rpm)
-      PKGMASK="$PKGNAME-${VERSION}.x86_64.rpm"
+      PKGMASK="$PKGNAME-${FULLVERSION}.x86_64.rpm"
       ;;
   *)
-      PKGMASK="$PKGNAME-${VERSION}_amd64.deb"
+      PKGMASK="$PKGNAME-${FULLVERSION}_amd64.deb"
       ;;
 esac
 
