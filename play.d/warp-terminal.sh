fec4a1ae0958d16acfa15cad74c885fbdc47439e
Ivan Mazhukin <vanomj@altlinux.org>
2026-04-30 13:44:35 +0300

    epm play warp-terminal: don't use deb to get version on ALT (eterbug #17865)

--- play.d/warp-terminal.sh
+++ play.d/warp-terminal.sh
@@ -11,8 +11,21 @@ URL="https://www.warp.dev/"
 warn_version_is_not_supported
 
 arch="$(epm print info -a)"
-ARCHSUFF=""
-[ "$arch" = "aarch64" ] && ARCHSUFF="_arm64"
+case "$arch" in
+    x86_64)
+        deb_arch=amd64
+        appimage_arch=x86_64
+        ;;
+    aarch64)
+        deb_arch=arm64
+        appimage_arch=aarch64
+        ;;
+esac
+
+WARP_VERSION="$(get_json_value https://releases.warp.dev/channel_versions.json '["stable","version"]' | sed -e 's|^v||')"
+[ -n "$WARP_VERSION" ] || fatal "Can't get Warp version"
+WARP_PKG_VERSION="$(echo "$WARP_VERSION" | sed -e 's|\.stable_|.stable.|')"
+WARP_BASE_URL="https://releases.warp.dev/stable/v$WARP_VERSION"
 
 case $(epm print info -p) in
     # force repack for all rpm based (due scripts)
@@ -20,29 +33,15 @@ case $(epm print info -p) in
     #    PKGURL="https://app.warp.dev/download?package=rpm$ARCHSUFF"
     #    ;;
     *)
-        PKGURL="https://app.warp.dev/download?package=deb$ARCHSUFF"
+        PKGURL="$WARP_BASE_URL/warp-terminal_${WARP_PKG_VERSION}_${deb_arch}.deb"
         ;;
 esac
 
-# get version from deb package
-set_version()
-{
-    local URL="$1"
-    # use temp dir
-    PKGDIR="$(mktemp -d)"
-    trap "rm -frv $PKGDIR" EXIT
-    cd $PKGDIR || fatal
-    eget -O pkg.deb "$URL"
-    VERSION="$(epm print version of package pkg.deb)"
-}
-
 case "$(epm print info -d)" in
     ALTLinux)
-        set_version $PKGURL || fatal "Can't get version"
         # due warp-terminal: /lib64/libcurl.so.4: version `CURL_OPENSSL_4' not found (required by warp-terminal)
-        PKGURL="https://app.warp.dev/download?package=appimage$ARCHSUFF"
-        # TODO: eget can't --get-real-url or --get-filename for the url
-        install_pack_pkgurl $VERSION
+        PKGURL="$WARP_BASE_URL/Warp-$appimage_arch.AppImage"
+        install_pack_pkgurl "$WARP_PKG_VERSION"
         exit
         ;;
 esac
