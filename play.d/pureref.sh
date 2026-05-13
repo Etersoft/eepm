0c5541412f1672f8f21b2f2ddc8dd3bebaf84c71
Ivan Mazhukin <vanomj@altlinux.org>
2026-05-13 14:10:06 +0300

    epm play pureref: don't force curl anymore (eterbug #17865)

--- play.d/pureref.sh
+++ play.d/pureref.sh
@@ -14,6 +14,4 @@ key=$(eget -A -O- https://www.pureref.com/download.php | awk '/setupPaymentSyste
 VERSION=$(eget -O- "https://www.pureref.com/changelog.php" | grep -o -m 1 "Version [0-9].[0-9].[0-9]" | awk '{print $2}'| head -n 1)
 
 PKGURL="https://www.pureref.com/files/build.php?build=LINUX64.deb&version=${VERSION}&downloadKey=$key"
-export EGET_BACKEND=curl
 install_pkgurl
-
