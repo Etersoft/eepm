38a5844bbfe310e406de1e144efd175e4a4af638
Ivan Mazhukin <vanomj@altlinux.org>
2026-05-13 19:03:33 +0300

    epm repack netbeans: fix requires (eterbug #17865)

--- repack.d/apache-netbeans.sh
+++ repack.d/apache-netbeans.sh
@@ -9,6 +9,10 @@ SPEC="$2"
 # foreign binaries in /usr/lib/apache-netbeans/ide/bin/nativeexecution
 ignore_lib_requires libc.so.1 libjawt.so libpthread.1
 
+# The bundled ZuluFX runtime may ship optional JavaFX media plugins for
+# several ffmpeg ABIs at once. Supported systems don't provide this ABI
+# set simultaneously, and the IDE itself does not need these plugins.
+remove_file /usr/lib/apache-netbeans/jdk/lib/libavplugin-ffmpeg-*.so
+
 # need openjdk to run
 add_unirequires /usr/bin/javac
-
