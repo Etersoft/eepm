4ae560272f80d4a2ae9c2f52591f71f322ff9db2
Vitaly Lipatov <lav@etersoft.ru>
2026-05-15 23:27:24 +0300

    epm play xppen: scrape vendor page for the latest .deb instead of hardcoded id

--- play.d/xppen.sh
+++ play.d/xppen.sh
@@ -10,6 +10,21 @@ URL="https://www.xp-pen.com/"
 
 warn_version_is_not_supported
 
-PKGURL="https://www.xp-pen.com/download/file.html?id=3561&pid=1179&ext=deb"
+# the .deb installer is shared across all XP-Pen tablets,
+# scrape any product page for its current data-id/data-pid pair
+ids=$(eget -O- "https://www.xp-pen.com/download/deco-01-v2.html" 2>/dev/null | awk '
+    /name_text">XPPenLinux.*\.deb/ { found=1 }
+    found && /data-ext="deb"/ {
+        match($0, /data-id="[0-9]+"/);  id=substr($0,  RSTART+9,  RLENGTH-10)
+        match($0, /data-pid="[0-9]+"/); pid=substr($0, RSTART+10, RLENGTH-11)
+        print id, pid
+        exit
+    }')
+
+[ -n "$ids" ] || fatal "Cannot detect XPPen latest .deb URL from the vendor page"
+
+id=${ids% *}
+pid=${ids#* }
+PKGURL="https://www.xp-pen.com/download/file.html?id=$id&pid=$pid&ext=deb"
 
 install_pkgurl
