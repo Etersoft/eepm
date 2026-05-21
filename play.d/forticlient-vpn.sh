24659a661e691b2da3c38661d15d3d66f7c0b5ef
Ivan Mazhukin <vanomj@altlinux.org>
2026-05-21 16:34:11 +0300

    epm play forticlient-vpn: update version (eterbug #17865)

--- play.d/forticlient-vpn.sh
+++ play.d/forticlient-vpn.sh
@@ -10,10 +10,16 @@ URL="https://www.fortinet.com/support/product-downloads"
 
 warn_version_is_not_supported
 
-# https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=forticlient-vpn
-# TODO: get latest version
-VERSION="7.4.3.1736"
-arch="amd64"
-PKGURL="https://filestore.fortinet.com/forticlient/downloads/forticlient_vpn_${VERSION}_$arch.deb"
+# Fortinet page has only shortlinks, and filestore directory listing is closed.
+VERSION="7.4.3.5411"
+
+case "$(epm print info -p)" in
+    rpm)
+        PKGURL="https://filestore.fortinet.com/forticlient/downloads/forticlient_vpn_${VERSION}_x86_64.rpm"
+        ;;
+    *)
+        PKGURL="https://filestore.fortinet.com/forticlient/downloads/forticlient_vpn_${VERSION}_amd64.deb"
+        ;;
+esac
 
 install_pkgurl
