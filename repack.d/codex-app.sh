b923bfa0fc7c14351bc1448ef887c676aae29426
Boris Yumankulov <boria138@altlinux.org>
2026-06-05 14:37:18 +0500

    epm play codex-app: fix work on Wayland
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- repack.d/codex-app.sh
+++ repack.d/codex-app.sh
@@ -7,10 +7,3 @@ SPEC="$2"
 . $(dirname $0)/common-chromium-browser.sh
 
 move_to_opt
-
-rm -v usr/bin/$PRODUCT
-
-cat <<EOF | create_exec_file /usr/bin/$PRODUCT
-#!/bin/sh
-$PRODUCTDIR/$PRODUCT --ozone-platform=x11
-EOF
