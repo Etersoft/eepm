6c354979af56d58f1784d777286c466ce04f1c54
Ivan Mazhukin <vanomj@altlinux.org>
2026-06-09 21:55:28 +0300

    epm play teamspeak3: fix install (eterbug #17865)

--- play.d/teamspeak3.sh
+++ play.d/teamspeak3.sh
@@ -9,7 +9,9 @@ URL="https://www.teamspeak.com/"
 . $(dirname $0)/common.sh
 
 if [ "$VERSION" = "*" ] ; then
-    PKGURL=$(eget --list --latest https://teamspeak.com/en/downloads/#ts3client "TeamSpeak3-Client-linux_amd64-*.run")
+    PKGURL="$(fetch_url https://teamspeak.com/en/downloads/ \
+        | grep -oE 'https://files\.teamspeak-services\.com/releases/client/[0-9.]+/TeamSpeak3-Client-linux_amd64-[0-9.]+\.run' \
+        | head -n1)"
 else
     PKGURL="https://files.teamspeak-services.com/releases/client/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run"
 fi
