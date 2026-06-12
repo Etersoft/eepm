f939fbd866f0743bc13dff1371b57810c47bcfcf
Vitaly Lipatov <lav@etersoft.ru>
2026-06-12 14:58:57 +0300

    epm play spo-anketa: update to 1.3.2

--- play.d/spo-anketa.sh
+++ play.d/spo-anketa.sh
@@ -11,17 +11,17 @@ URL="https://gossluzhba.gov.ru/spo/"
 warn_version_is_not_supported
 
 # Native packages from gossluzhba.gov.ru
-# Version 1.3.1 from 29.04.2026
+# Version 1.3.2 from 06.2026
 pkgtype="$(epm print info -p)"
 
 case $pkgtype in
     deb)
-        # SPO_AstraLinux_1_3_1.deb
-        PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/3b381967-ea64-4a14-ad63-1689089ea4f9"
+        # SPO_AstraLinux_1_3_2.deb
+        PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/4cd9c732-ee4e-4dc6-9f44-83b2cf4e3a72"
         ;;
     *)
-        # SPO_RedOS_AltLinux_1_3_1.rpm
-        PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/049b2d5e-b1aa-4f79-913d-f9f562b4fe8e"
+        # SPO_RedOS_AltLinux_1_3_2.rpm
+        PKGURL="https://files.gossluzhba.gov.ru/49309a89-3c66-408c-805a-2d42b28e89c9/download/92b8266d-11c2-4972-8217-d2987d1b1377"
         ;;
 esac
 
