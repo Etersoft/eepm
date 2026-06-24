242727c8be370a36ed00e7d3518f3c84d2048985
Ivan Mazhukin <vanomj@altlinux.org>
2026-06-24 19:00:47 +0300

    epm play realvnc-viewer: fix download & install (eterbug #17865)

--- repack.d/realvnc-vnc-viewer.sh
+++ repack.d/realvnc-vnc-viewer.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -x
 
 # It will be run with two args: buildroot spec
+
+# Upstream renamed the Linux viewer package to "realvnc-rvncconnect-viewer",
+# but in EPM we keep the historical package name "realvnc-vnc-viewer".
 BUILDROOT="$1"
 SPEC="$2"
+PKGNAME=realvnc-vnc-viewer
 
 . $(dirname $0)/common.sh
 
-add_conflicts tigervnc
+subst "s|^Name:.*|Name: $PKGNAME|" "$SPEC"
 
+add_conflicts tigervnc
