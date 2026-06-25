46e95acfb1649eee2bba2f8315fb021f014a923b
Ivan Mazhukin <vanomj@altlinux.org>
2026-06-25 18:01:42 +0300

    epm play r7-office-organizer: fix download (eterbug #17865)

--- play.d/r7-office-organizer.sh
+++ play.d/r7-office-organizer.sh
@@ -5,7 +5,7 @@ SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 RELEASE="$3"
 DESCRIPTION="R7 Office Organizer for Linux from the official site"
-URL="https://r7-office.ru/downloadorganizer"
+URL="https://support.r7-office.ru/download/organizer/"
 
 # fixme: global epm is used
 case $(epm print info -p) in
@@ -23,8 +23,8 @@ esac
 
 case $(epm print info -p) in
     rpm)
+        # upstream now publishes generic rpm on the support page
         # https://download.r7-office.ru/organizer/centos/r7organizer-3.1.2-1.x86_64.rpm
-        # alt version has other deps, but we repack in anyway
         mask="centos/r7organizer-$VERSION.x86_64.rpm"
         ;;
     *)
@@ -33,9 +33,21 @@ case $(epm print info -p) in
         ;;
 esac
 
+case $(epm print info -e) in
+    AstraLinuxSE/*)
+        # https://download.r7-office.ru/organizer/astra/r7-organizer_3.1.2-1_astralinux-signed_amd64.deb
+        mask="astra/r7-organizer_${VERSION}_astralinux-signed_amd64.deb"
+        ;;
+    ALTLinux/*)
+        # https://download.r7-office.ru/organizer/alt/r7organizer-3.1.2-1_altlinux.x86_64.rpm
+        mask="alt/r7organizer-${VERSION}_altlinux.x86_64.rpm"
+        ;;
+esac
+
 
 if [ "$VERSION" = "*" ] ; then
-    PKGURL=$(eget -U --list --latest "https://r7-office.ru/downloadorganizer" "$mask")
+    # parse the support page, because the old vendor landing now redirects elsewhere
+    PKGURL=$(eget -U --list --latest "$URL" "$mask")
 else
     PKGURL="https://download.r7-office.ru/organizer/$mask"
 fi
