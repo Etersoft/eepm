54aba3e344155238d85f0c6c27538541f6b1ba10
Vitaly Lipatov <lav@etersoft.ru>
2026-06-29 11:13:42 +0300

    epm repack ipera-client: add libatomic.so.1 require (bundled libopenal needs it)

--- repack.d/ipera-client.sh
+++ repack.d/ipera-client.sh
@@ -7,7 +7,7 @@ PRODUCTDIR=/opt/ipera
 
 UNIREQUIRES="binutils coreutils
 libXcomposite.so.1 libXdamage.so.1 libXext.so.6 libXfixes.so.3 libXrandr.so.2 libXrender.so.1 libXtst.so.6
-libasound.so.2 libdrm.so.2 libexpat.so.1 libglib-2.0.so.0 libgmodule-2.0.so.0 libgthread-2.0.so.0
+libasound.so.2 libatomic.so.1 libdrm.so.2 libexpat.so.1 libglib-2.0.so.0 libgmodule-2.0.so.0 libgthread-2.0.so.0
 libnspr4.so libnss3.so libnssutil3.so libopenal.so.1 liborc-0.4.so.0 libplc4.so libplds4.so libpulse-mainloop-glib.so.0
 libpulse.so.0 libresolv.so.2 libsmime3.so
 libudev.so.1
