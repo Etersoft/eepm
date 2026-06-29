2267e1cfa49904599624e8048907c62c5593cf81
Vitaly Lipatov <lav@etersoft.ru>
2026-06-29 11:40:20 +0300

    epm play dbeaver: resolve release asset name via get_github_url (handle old -stable vs new -linux naming)

--- play.d/dbeaver.sh
+++ play.d/dbeaver.sh
@@ -9,15 +9,17 @@ URL="https://dbeaver.io/"
 . $(dirname $0)/common.sh
 
 pkgtype=$(epm print info -p)
-case $pkgtype in
-    rpm)
-        PKGURL="https://dbeaver.io/files/$VERSION/dbeaver-ce-$VERSION-linux-x86_64.rpm"
-        [ "$VERSION" = "*" ] && PKGURL="https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.rpm"
-        ;;
-    *)
-        PKGURL="https://dbeaver.io/files/$VERSION/dbeaver-ce-$VERSION-linux-x86_64.deb"
-        [ "$VERSION" = "*" ] && PKGURL="https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.deb"
-        ;;
-esac
+[ "$pkgtype" = "rpm" ] && ext=rpm || ext=deb
+
+if [ "$VERSION" = "*" ] ; then
+    PKGURL="https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.$ext"
+else
+    # DBeaver renamed its release assets over time (older 25.3.2-25.3.4: -stable.x86_64,
+    # newer: -linux-x86_64). Resolve the real file from the GitHub release by trying the
+    # current name, then the legacy one (dbeaver.io just redirects to GitHub).
+    DBEAVER_GH="https://github.com/dbeaver/dbeaver"
+    PKGURL="$(get_github_url "$DBEAVER_GH" "dbeaver-ce-$VERSION-linux-x86_64.$ext")"
+    [ -n "$PKGURL" ] || PKGURL="$(get_github_url "$DBEAVER_GH" "dbeaver-ce-$VERSION-stable.x86_64.$ext")"
+fi
 
 install_pkgurl
