66fcf2e988b3a45701d0c9ecf0782f98746d4eff
Vitaly Lipatov <lav@etersoft.ru>
2026-06-30 22:25:14 +0300

    epm play qsp-classic: drop eepm RELEASE from the download URL (fixes 404 on 5.9.5)

--- play.d/qsp-classic.sh
+++ play.d/qsp-classic.sh
@@ -13,9 +13,9 @@ arch=x86_64
 if [ "$VERSION" = "*" ] ; then
     PKGURL=$(get_github_url "https://github.com/QSPFoundation/qspgui/" "${PKGNAME}-${VERSION}-$arch.AppImage")
 else
-    # allow use 5.x.x-beta format
-    [ -z "$CHECKED_VERSION" ] && [ "$RELEASE" != "*" ] && VERSION="$VERSION-$RELEASE"
-    # replace ~ with - for URL (VERSION may contain ~b1 for pre-release)
+    # the upstream tag/asset carries only the version (with an optional ~bN
+    # pre-release marker), never the eepm RELEASE number. Build the URL from
+    # VERSION alone, replacing ~ with - (VERSION may contain ~b1 for pre-release).
     URLVERSION="$(echo "$VERSION" | tr '~' '-')"
     PKGURL="https://github.com/QSPFoundation/qspgui/releases/download/v$URLVERSION/${PKGNAME}-$URLVERSION-$arch.AppImage"
 fi
