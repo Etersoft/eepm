af6c7002b44f49ac08ffde998f569368b59ae84f
Vitaly Lipatov <lav@etersoft.ru>
2026-07-02 21:43:29 +0300

    epm play sunshine: match any fc* build (fc41 no longer built)

--- play.d/sunshine.sh
+++ play.d/sunshine.sh
@@ -5,7 +5,7 @@ SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 RELEASE="$3"
 DESCRIPTION="Self-hosted game stream host for Moonlight"
-TIPS="Run epm play Sunshine=<version> to install specific version (e.g. 2025.924.154138)."
+TIPS="Run epm play Sunshine=<version> to install specific version (e.g. 2026.516.143833)."
 URL="https://github.com/LizardByte/Sunshine"
 
 . $(dirname $0)/common.sh
@@ -14,11 +14,11 @@ is_openssl_enough 3 || fatal "There is no needed OpenSSL 3 in the system."
 
 arch=$(epm print info -a)
 
-# Use GitHub releases (Fedora 41 packages work for ALT via repack)
+# Use GitHub releases (Fedora packages work for ALT via repack)
 if [ "$VERSION" != "*" ] ; then
     PKGURL="https://github.com/LizardByte/Sunshine/releases/download/v${VERSION}/Sunshine-${VERSION}-${RELEASE}.$arch.rpm"
 else
-    PKGURL=$(eget --list --latest https://github.com/LizardByte/Sunshine/releases "Sunshine-*-*.fc41.$arch.rpm")
+    PKGURL=$(eget --list --latest https://github.com/LizardByte/Sunshine/releases "Sunshine-*-*.fc*.$arch.rpm")
 fi
 
 install_pkgurl
