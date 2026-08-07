b95fb446291653c7ad0a4273a957ac73b8fffc7c
Ivan Mazhukin <vanomj@altlinux.org>
2026-08-07 12:02:38 +0300

    epm play blender: fix repack for 5.2 (eterbug #17865)

--- repack.d/blender.sh
+++ repack.d/blender.sh
@@ -7,9 +7,8 @@ SPEC="$2"
 . $(dirname $0)/common.sh
 
 add_bin_link_command blender $PRODUCTDIR/blender-launcher
-add_bin_link_command blender-softwaregl $PRODUCTDIR/blender-softwaregl
+[ ! -e "$BUILDROOT$PRODUCTDIR/blender-softwaregl" ] || add_bin_link_command blender-softwaregl $PRODUCTDIR/blender-softwaregl
 add_bin_link_command blender-system-info $PRODUCTDIR/blender-system-info.sh
 add_bin_link_command blender-thumbnailer $PRODUCTDIR/blender-thumbnailer
 
-ignore_lib_requires libamdhip64.so.6 libze_loader.so.1
-
+ignore_lib_requires libamdhip64.so.6 libamdhip64.so.7 libcuda.so.1 libze_loader.so.1
