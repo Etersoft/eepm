75ca62b7b7d7e1d34b197ce14be5f7a848bc0679
Ivan Mazhukin <vanomj@altlinux.org>
2026-08-11 16:18:50 +0300

    epm play flclashx: fix repack package name (eterbug #17865)

--- repack.d/FlClashX.sh
+++ repack.d/FlClashX.sh
@@ -4,9 +4,14 @@
 BUILDROOT="$1"
 SPEC="$2"
 
+PRODUCT=flclashx
+PRODUCTCUR=FlClashX
+
 . $(dirname $0)/common.sh
 
 move_to_opt
 
+fix_desktop_file "$PRODUCTDIR/$PRODUCTCUR" $PRODUCT
 
-add_bin_link_command FlClashX /opt/$PRODUCT/FlClashX
+add_bin_link_command $PRODUCT "$PRODUCTDIR/$PRODUCTCUR"
+add_bin_link_command $PRODUCTCUR "$PRODUCTDIR/$PRODUCTCUR"
