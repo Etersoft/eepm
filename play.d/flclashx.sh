3178c241349e8332a56b56ca401d101de7b6e706
Ivan Mazhukin <vanomj@altlinux.org>
2026-08-11 16:18:50 +0300

    epm play flclashx: fix repack package name (eterbug #17865)

--- play.d/flclashx.sh
+++ play.d/flclashx.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-PKGNAME=FlClashX
+PKGNAME="FlClashX flclashx"
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Fork of FlClash | A multi-platform proxy client based on ClashMeta, simple and easy to use"
 URL="https://github.com/pluralplay/FlClashX"
 
 . $(dirname $0)/common.sh
+
+export EPM_REPACK_SCRIPT=FlClashX
 
 arch=$(epm print info --debian-arch)
 
