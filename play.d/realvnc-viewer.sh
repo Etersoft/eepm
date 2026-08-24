e93d597bfd75b5d862a36e310205dc232ec7cd90
Ivan Mazhukin <vanomj@altlinux.org>
2026-08-24 12:46:15 +0300

    epm play realvnc-viewer: fix upstream url (eterbug #17865)

--- play.d/realvnc-viewer.sh
+++ play.d/realvnc-viewer.sh
@@ -12,23 +12,12 @@ warn_version_is_not_supported
 
 pkgtype="$(epm print info -p)"
 
-
 case $pkgtype in
-    rpm)
-        mask='RealVNC-Connect-Viewer-[0-9.]*-Linux-x64\.rpm' ;;
-    *)
-        mask='RealVNC-Connect-Viewer-[0-9.]*-Linux-x64\.deb' ;;
+    rpm) suffix=rpm ;;
+    *) suffix=deb ;;
 esac
 
-# RealVNC no longer keeps classic Linux viewer packages under viewer.files and
-# now publishes current Linux builds on the download page as
-# RealVNC-Connect-Viewer-* files. Old pinned versions are not reliable there,
-# so always resolve the latest vendor-published x64 package from the page.
- 
-PKGFILE="$(fetch_url "$URL" | grep -Eo "$mask" | sort -Vu | tail -n1)"
-
-[ -n "$PKGFILE" ] || fatal "Can't get package file name from $URL."
-
-PKGURL="https://downloads.realvnc.com/download/file/realvnc-connect-viewer/$PKGFILE"
+[ "$VERSION" = "*" ] && VERSION=Latest
+PKGURL="https://downloads.realvnc.com/download/file/realvnc-connect-viewer/RealVNC-Connect-Viewer-$VERSION-Linux-x64.$suffix"
 
 install_pkgurl
