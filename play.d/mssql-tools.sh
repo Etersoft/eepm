057547f53e5b42221c3d932a379ab7d7710ebc49
Ivan Mazhukin <vanomj@altlinux.org>
2026-08-25 15:10:04 +0300

    epm play mssql-tools: add Fedora, ROSA and RedOS support (eterbug #17865)

--- play.d/mssql-tools.sh
+++ play.d/mssql-tools.sh
@@ -21,7 +21,7 @@ case "$(epm print info -e)" in
     epm install "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql17/msodbcsql17_1*_amd64.deb"
     epm install "https://packages.microsoft.com/debian/12/prod/pool/main/m/mssql-tools/mssql-tools_1*_amd64.deb"
     ;;
-  ALTLinux*)
+  ALTLinux*|ROSA/*|RedOS/*|Fedora/*)
     epm install --skip-installed unixODBC || fatal
     epm install --repack "https://packages.microsoft.com/rhel/8/prod/Packages/m/mssql-tools-1*.x86_64.rpm"
     epm install --repack "https://packages.microsoft.com/rhel/8/prod/Packages/m/msodbcsql17-1*.x86_64.rpm"
