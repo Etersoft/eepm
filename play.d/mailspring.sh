77cadd5d87a54f8b00f5b1156eedd1b111464bd6
Ivan Mazhukin <vanomj@altlinux.org>
2026-09-14 17:04:11 +0300

    epm play mailspring: fix package architecture (eterbug #17865)

--- play.d/mailspring.sh
+++ play.d/mailspring.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PKGNAME=mailspring
-SUPPORTEDARCHES="x86_64"
+SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Mailspring - a beautiful, fast and fully open source mail client"
 URL="https://www.getmailspring.com/"
@@ -11,11 +11,15 @@ URL="https://www.getmailspring.com/"
 pkgtype="$(epm print info -p)"
 case "$pkgtype" in
     rpm)
-        PKGURL=$(get_github_url https://github.com/Foundry376/Mailspring/ "$PKGNAME.*$VERSION.*$arch.$pkgtype")
+        arch="$(epm print info -a)"
+        mask="$PKGNAME-$VERSION-*.$arch.rpm"
         ;;
     *)
-        PKGURL=$(get_github_url https://github.com/Foundry376/Mailspring/ "$PKGNAME.*$VERSION.*$arch.deb")
+        arch="$(epm print info --debian-arch)"
+        mask="$PKGNAME-$VERSION-$arch.deb"
         ;;
 esac
+
+PKGURL=$(get_github_url https://github.com/Foundry376/Mailspring/ "$mask")
 
 install_pkgurl
